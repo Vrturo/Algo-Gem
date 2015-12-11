9dcd9be389db08552de9006794caa4abfc34e5c9
Arturo Perez <VrturoPerez@gmail.com>
2015-12-11 15:00:10 -0800

    add successful quicksort method to sort.rb

--- lib/algo-gem/sort.rb
+++ lib/algo-gem/sort.rb
@@ -13,37 +13,38 @@ class Array
   end
 
 
-  def quicksort #takes an array of integers as an argument
+  def quicksort
   # You need a base case, otherwise your recursive calls never terminate
 
-  if self.length <= 1
-    return self
-  # Now pick a pivot:
-  else
-    pivot = self.sample
-    self.delete_at(self.index(pivot)) # remove the pivot
-    # puts "Picked pivot of: #{pivot}"
-    less = []
-    greater = []
+    if self.length <= 1
+      return self
+    # Now pick a pivot:
+    else
+      pivot = self.sample
+      self.delete_at(self.index(pivot)) # remove the pivot
+      puts "Picked pivot of: #{pivot}"
+      less = []
+      greater = []
 
   # Loop through the array, comparing items to pivot and collecting them into less and greater arrays.
 
-    self.each do |x|
-      if x <= pivot
-        less << x
-      else
-        greater << x
-      end
-    end
+      self.each do |x|
+        if x <= pivot
+          less << x
+        else
+          greater << x
+        end
   # Now, recursively call self.quicksort on your less and greater arrays.
+      end
 
-    sorted_array = []
-    sorted_array << less.quicksort
-    sorted_array << pivot
-    sorted_array << greater.quicksort
-  # Return the sorted_array and you're done.
+      sorted_array = []
+      sorted_array << less.quicksort
+      sorted_array << pivot
+      sorted_array << greater.quicksort
+    # Return the sorted_array and you're done.
 
-    # using Array.flatten to remove subarrays
-    sorted_array.flatten!
+      # using Array.flatten to remove subarrays
+      sorted_array.flatten!
+    end
   end
 end
