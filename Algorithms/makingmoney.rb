e19871b8e54bcbc7f8092533129a533cb5228873
Arturo Perez <VrturoPerez@gmail.com>
2015-12-24 22:25:47 -0800

    start making change algorithm problem

---
+++ Algorithms/makingmoney.rb
@@ -0,0 +1,15 @@
+# Write a function that, given:
+#1. an amount of money
+#2. an array of coin denominations
+# computes the number of ways to make amount of money with coins of the available denominations.
+
+# Example:
+# 4 cents is equal to:
+# 1¢, 1¢, 1¢, 1¢
+# 1¢, 1¢, 2¢
+# 1¢, 3¢
+# 2¢, 2¢
+
+# for amount=44 (44¢) and denominations=[1,2,3][1,2,3] (11¢, 22¢ and 33¢),
+# your program would output 44—the number of ways to make 44¢ with those denominations:
+
