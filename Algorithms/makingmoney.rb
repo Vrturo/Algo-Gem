7e8b68fa496ecafef1fd9c90081215de4c984cc3
Arturo Perez <VrturoPerez@gmail.com>
2015-12-24 22:33:51 -0800

    makingmoney algo problem solution

--- Algorithms/makingmoney.rb
+++ Algorithms/makingmoney.rb
@@ -13,3 +13,21 @@
 # for amount=44 (44¢) and denominations=[1,2,3][1,2,3] (11¢, 22¢ and 33¢),
 # your program would output 44—the number of ways to make 44¢ with those denominations:
 
+def change_possibilities_bottom_up(amount, denominations)
+    ways_of_doing_n_cents = [0] * (amount + 1)
+    ways_of_doing_n_cents[0] = 1
+
+    denominations.each do |coin|
+        (coin..amount).each do |higher_amount|
+            higher_amount_remainder = higher_amount - coin
+            ways_of_doing_n_cents[higher_amount] += ways_of_doing_n_cents[higher_amount_remainder]
+        end
+    end
+
+    return ways_of_doing_n_cents[amount]
+end
+
+
+#Complexity
+# O(n∗m) time and O(n)O(n) additional space
+# where nn is the amount of money and mm is the number of potential denominations.
