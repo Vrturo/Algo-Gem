c6c538fdf7906b0c5b0f87f7e1c1a4ccd50760a1
Arturo Perez <VrturoPerez@gmail.com>
2016-01-18 15:37:43 -0800

    create integers directory && add isprime algo in direcory

---
+++ Algorithms/integers/isPrime.rb
@@ -0,0 +1,30 @@
+# Define a function isPrime that takes one integer argument and
+# returns true or false depending on if the integer is a prime.
+# A prime number is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+
+# Example
+# isPrime(5)
+# => true
+
+# def isPrime(num)
+#   return false if num <= 1
+#   Math.sqrt(num).to_i.downto(2).each {|i| return false if num % i == 0}
+#   true
+# end
+
+
+# require 'prime'
+
+# def isPrime(num)
+#   num.prime?
+# end
+
+
+# def isPrime(num)
+#   return false if num <= 1
+#   return true if num < 4
+#   (2...num).each do |i|
+#      return false if num % i == 0
+#    end
+#   true
+# end
