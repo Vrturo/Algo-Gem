f768b4774cf865cf3d74fcd32d83f3c141905e8b
Arturo Perez <VrturoPerez@gmail.com>
2016-01-18 15:49:17 -0800

    add new integer algorithm into integers directory

---
+++ Algorithms/integers/feymansquares.rb
@@ -0,0 +1,17 @@
+# How many different squares are there in a grid of NxN squares?
+# For example, when N=2, the answer is 5: the 2x2 square itself, plus the four 1x1 squares in its corners
+# You have to write a method
+
+# def count_squares(n)
+# that solves Feynman's question in general. The input to your function will always be a positive integer.
+
+# Examples
+# count_squares(1) =  1
+# count_squares(2) =  5
+# count_squares(3) = 14
+
+
+
+def count_squares(n)
+   (1..n).inject{|sum, e| sum+e*e}
+end
