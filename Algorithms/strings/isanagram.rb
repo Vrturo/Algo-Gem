2da93945cbcd4c797263cef33ce6de1776de2cf1
Arturo Perez <VrturoPerez@gmail.com>
2016-01-19 15:17:21 -0800

    add solutions to isanagram algo

--- Algorithms/strings/isanagram.rb
+++ Algorithms/strings/isanagram.rb
@@ -15,3 +15,37 @@
 # silent = listen
 # 12345 = 54321
 
+# ------------ two method solution ---------------
+# def isAnagram(test, original)
+#   reduce(test) == reduce(original)
+# end
+
+# def reduce(string)
+#   string.downcase.gsub(/\W+/, "").split("").sort
+# end
+# ------------------------------------------------
+
+# def isAnagram(test, original)
+#   f = ->(x) { x.downcase.delete("^a-z0-9").chars.sort }
+#   f[test] == f[original]
+# end
+
+# def isAnagram(test, original)
+#   t = test.downcase.gsub /\W/, ""
+#   o = original.downcase.gsub /\W/, ""
+#   return false unless t.length == o.length
+#   char_occurance = Hash.new(0)
+#   t.each_char {|char| char_occurance[char.to_sym]+=1}
+#   o.each_char {|char| char_occurance[char.to_sym]-=1}
+#   char_occurance.all? {|char, occurance| occurance.zero?}
+# end
+
+# ef isAnagram(test, original)
+#   t1 = test.upcase.scan(/\w/).sort
+#   t2 = original.upcase.scan(/\w/).sort
+#   return t1 == t2
+# end
+
+# def isAnagram(test, original)
+#   test.downcase.gsub(/[^0-9a-z ]/i, "").gsub(" ", "").split(//).sort == original.downcase.gsub(/[^0-9a-z ]/i, "").gsub(" ", "").split(//).sort
+# end
