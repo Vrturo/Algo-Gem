272c366f4f1a7bc08f8e1b112fc28e289b5c11c5
Arturo Perez <VrturoPerez@gmail.com>
2016-01-18 23:42:59 -0800

    added solutions

--- Algorithms/strings/ispalindrome.rb
+++ Algorithms/strings/ispalindrome.rb
@@ -1,6 +1,40 @@
 # Given an input string, determine whether any anagram of the string is a palindrome
 
 # def is_palindrome(string)
+#     front = 0
+#     back = string.length-1
+#     while back >= string.length/2
+#         if string[front] == string[back]
+#             front += 1
+#             back -= 1
+#         else
+#             return false
+#         end
+#     end
+#     return true
+# end
+
+# ----------------------
+# def reverse_string(string)
+#   start = 0
+#   finish = string.length-1
+#   while start < finish
+#     temp = string[start] #
+#     string[start] = string[finish] # ex 'hello' #oello
+#     string[finish] = temp #temp stores string start so its available when replaced
+#     start += 1 # ex 'hello' #oello, olllo
+#     finish -= 1
+#   end
+#   return string
+# end
+
+# def isPalindrome(string)
+#   reverse_string(string) == string ? true : false
+# end
+#--------------------------
+
+
+# def is_palindrome(string)
 #   counter = {}
 #   odd_count = 0
 #   string.each_char do |letter|
