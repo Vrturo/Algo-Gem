a97689968bb1d01c2d538782c2ad3f12dac2d24e
Arturo Perez <VrturoPerez@gmail.com>
2016-01-18 19:59:11 -0800

    add new algo problem and solutions

--- Algorithms/strings/replacestringinstring.rb
+++ Algorithms/strings/replacestringinstring.rb
@@ -49,3 +49,46 @@
 # end
 
 # ----------------------------
+
+
+# Our fruit guy has a bag of fruits (array of strings) where some fruits are rotten,
+# he wants to replace all the rotten fruits by good ones.
+# For example, given this array ["apple","rottenBanana","apple"] the replaced array should be
+# ["apple","banana","apple"]. Your task is to implement a method that will take as an argument
+# an array of strings containing fruits and should return an array of strings where all the rotten fruits
+# are replaced by good ones.
+
+# Note: If the array is null or empty you should return empty array ([]).
+# The rotten fruit name will be in this format rottenFruitname where is the 1st letter of the fruit name
+# is uppercase. NB: The returned array should be in LOWER case.
+
+
+# def remove_rotten(basket)
+#   basket ||= []
+#   basket.map { |fruit| fruit.gsub('rotten','').downcase }
+# end
+
+# def remove_rotten(fruitBasket)
+#   if fruitBasket == nil
+#      fruitBasket = []
+#   else
+#     fruitBasket.map {|fruit| fruit.gsub('rotten', '').downcase}
+#   end
+# end
+
+# def remove_rotten(fruitBasket)
+#   if fruitBasket == []
+#     []
+#   elsif fruitBasket == nil
+#     []
+#   else
+#     fruitBasket.each do |fruit|
+#       while fruit["rotten"]
+#         fruit.gsub!("rotten", "")
+#         fruit.downcase!
+#       end
+#      end
+#    end
+# end
+
+
