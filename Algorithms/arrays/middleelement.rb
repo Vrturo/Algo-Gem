eccfbe82b55be17acb9451e139cb8e46ccd67f33
Arturo Perez <VrturoPerez@gmail.com>
2016-01-19 22:47:54 -0800

    add descriptin

---
+++ Algorithms/arrays/middleelement.rb
@@ -0,0 +1,12 @@
+# Create a method that when provided with a triplet,
+# returns the index of the numerical element that lies between the other two elements.
+# The input to the method will be an array of three distinct numbers (Haskell: a tuple).
+
+# For example:
+
+# gimme([2, 3, 1]) => 0
+# 2 is the number that fits between 1 and 3 and the index of 2 in the input array is 0.
+
+# gimme([5, 10, 14]) => 1
+# 10 is the number that fits between 5 and 14 and the index of 10 in the input array is 1.
+
