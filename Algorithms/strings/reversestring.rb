34b01652e33e93d13b00ba8ed8030847b83777a7
Arturo Perez <VrturoPerez@gmail.com>
2016-01-19 18:46:39 -0800

    create emailvalidation file in strings directory

--- Algorithms/strings/reversestring.rb
+++ Algorithms/strings/reversestring.rb
@@ -1,21 +1,21 @@
-Reverse a string without the reverse method
+# Reverse a string without the reverse method
 
-def reverse_string(string)
-  new_string = []
-  last_index = string.length - 1
-  last_index.downto(0) { |i| new_string << string[i] }
-  p new_string.join('')
-end
+# def reverse_string(string)
+#   new_string = []
+#   last_index = string.length - 1
+#   last_index.downto(0) { |i| new_string << string[i] }
+#   p new_string.join('')
+# end
 
-def reverse_string(string)
-  start = 0
-  finish = string.length-1
-  while start < finish
-    temp = string[start] #
-    string[start] = string[finish] # ex 'hello' #oello
-    string[finish] = temp #temp stores string start so its available when replaced
-    start += 1 # ex 'hello' #oello, olllo
-    finish -= 1
-  end
-  return string
-end
+# def reverse_string(string)
+#   start = 0
+#   finish = string.length-1
+#   while start < finish
+#     temp = string[start] #
+#     string[start] = string[finish] # ex 'hello' #oello
+#     string[finish] = temp #temp stores string start so its available when replaced
+#     start += 1 # ex 'hello' #oello, olllo
+#     finish -= 1
+#   end
+#   return string
+# end
