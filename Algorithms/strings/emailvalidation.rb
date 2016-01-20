6a906e07c1e3934a54abeabe7dd2cd1d4081e3d3
Arturo Perez <VrturoPerez@gmail.com>
2016-01-19 18:47:16 -0800

    add description in emailvalidation

---
+++ Algorithms/strings/emailvalidation.rb
@@ -0,0 +1,11 @@
+# Write a method to test whether a given input is a valid email address.
+# Here is what makes a valid email:
+
+# At least one letter character at the beginning
+# All characters between the first character and the @ (if any) must be letters, numbers, or underscores
+# There must be an @ character (after the points listed just now)
+# After the @ character, there must be at least one word character (letter, number, or underscore) or hyphen
+# The email must end with at least one set of a dot followed by one or more word characters.
+# The input must NOT be case-sensitive
+# The function should return true or false.
+
