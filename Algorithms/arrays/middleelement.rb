596125bca8af97f75b7bb5185db3442699ff4e15
Arturo Perez <VrturoPerez@gmail.com>
2016-01-19 22:50:17 -0800

    add solutions

--- Algorithms/arrays/middleelement.rb
+++ Algorithms/arrays/middleelement.rb
@@ -10,3 +10,24 @@
 # gimme([5, 10, 14]) => 1
 # 10 is the number that fits between 5 and 14 and the index of 10 in the input array is 1.
 
+# def gimme(input)
+#   input.index(input.sort[1])
+# end
+
+# def gimme(input_array)
+#   mid_var = input_array.sort[1]
+#   input_array[mid_var]
+# end
+
+# def gimme(arr)
+#   arr.each { |num| return arr.index(num) if num < arr.max && num > arr.min }
+# end
+
+# def gimme(a)
+#   return 0 if a[1] <= a[0] && a[0] <= a[2]
+#   return 0 if a[2] <= a[0] && a[0] <= a[1]
+#   return 1 if a[0] <= a[1] && a[1] <= a[2]
+#   return 1 if a[2] <= a[1] && a[1] <= a[0]
+#   return 1 if a[2] <= a[1] && a[1] <= a[0]
+#   2
+# end
