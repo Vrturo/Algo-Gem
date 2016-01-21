9e93a6e42b2592ba0daa05ca3cc7364fb5c33286
Arturo Perez <VrturoPerez@gmail.com>
2016-01-20 17:09:08 -0800

    add solutions

--- Algorithms/arrays/howmanylikes.rb
+++ Algorithms/arrays/howmanylikes.rb
@@ -13,3 +13,47 @@
 
 # For more than 4 names, the number in and 2 others simply increases.
 
+# def likes(names)
+#   case names.size
+#   when 0
+#     "no one likes this"
+#   when 1
+#     "#{names[0]} likes this"
+#   when 2
+#     "#{names[0]} and #{names[1]} like this"
+#   when 3
+#     "#{names[0]}, #{names[1]} and #{names[2]} like this"
+#   else
+#     "#{names[0]}, #{names[1]} and #{names.size - 2} others like this"
+#   end
+# end
+
+# def likes(names)
+#   case names.size
+#   when 0
+#     "no one likes this"
+#   when 1
+#     "%s likes this" % names
+#   when 2
+#     "%s and %s like this" % names
+#   when 3
+#     "%s, %s and %s like this" % names
+#   else
+#     "%s, %s and %d others like this" % (names.first(2) << names.size - 2)
+#   end
+# end
+
+# def likes(names)
+#   case
+#   when names.length < 1
+#     'no one likes this'
+#   when names.length == 1
+#     "#{names[0]} likes this"
+#   when names.length == 2
+#     "#{names[0]} and #{names[1]} like this"
+#   when names.length == 3
+#     "#{names[0]}, #{names[1]} and #{names[2]} like this"
+#   when names.length > 3
+#     "#{names[0]}, #{names[1]} and #{names.length - 2} others like this"
+#   end
+# end
