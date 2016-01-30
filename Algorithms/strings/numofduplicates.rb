c6adbb6967406d6c34b4632a953d0232161395f8
Arturo Perez <VrturoPerez@gmail.com>
2016-01-30 15:45:01 -0800

    add description

---
+++ Algorithms/strings/numofduplicates.rb
@@ -0,0 +1,13 @@
+# Count the number of Duplicates
+
+# Write a method that will return the count of distinct case-insensitive alphabetic characters
+# that occur more than once in the given string.
+# The given string can be assumed to contain only uppercase and lowercase alphabets.
+
+# Example
+
+# "abcde" -> 0 # no characters repeats more than once
+# "aabbcde" -> 2 # 'a' and 'b'
+# "aabbcdeB" -> 2 # 'a' and 'b'
+# "indivisibility" -> 1 # 'i'
+# "Indivisibilities" -> 2 # 'i' and 's'
