c9316183d68fb0a7d591028ad182e4a1cff92a0a
Arturo Perez <VrturoPerez@gmail.com>
2016-01-30 16:38:34 -0800

    add solutions

--- Algorithms/strings/numofduplicates.rb
+++ Algorithms/strings/numofduplicates.rb
@@ -12,17 +12,28 @@
 # "indivisibility" -> 1 # 'i'
 # "Indivisibilities" -> 2 # 'i' and 's'
 
-def duplicate_count(text)
-    text.downcase!
-    counter = {}
-    text.each_char do |letter|
-      counter[letter] ? counter[letter] += 1 : counter[letter] = 1
-    end
-    dup_count = 0
-    counter.each do |k, v|
-      if v > 1
-        dup_count += 1
-      end
-    end
-    dup_count
-end
+
+# def duplicate_count(text)
+#   ('a'..'z').count { |c| text.downcase.count(c) > 1 }
+# end
+
+# def duplicate_count(text)
+#   hsh = Hash.new(0)
+#   text.downcase.chars.each { |c| hsh[c] += 1 }
+#   hsh.values.count { |k| k > 1 }
+# end
+
+# def duplicate_count(text)
+#     text.downcase!
+#     counter = {}
+#     text.each_char do |letter|
+#       counter[letter] ? counter[letter] += 1 : counter[letter] = 1
+#     end
+#     dup_count = 0
+#     counter.each do |k, v|
+#       if v > 1
+#         dup_count += 1
+#       end
+#     end
+#     dup_count
+# end
