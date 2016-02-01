44a80649d66ed17f5a941acfbb24b9490611eb80
Arturo Perez <VrturoPerez@gmail.com>
2016-01-31 23:25:35 -0800

    move stock algo to array folder

--- Algorithms/arrays/stockpricesyesterday1.rb
+++ Algorithms/arrays/stockpricesyesterday1.rb
@@ -1,10 +1,10 @@
-"Suppose we could access yesterday's stock prices as an array, where:
+# "Suppose we could access yesterday's stock prices as an array, where:
 
-The indices are the time in minutes past trade opening time, which was 9:30am local time.
-The values are the price in dollars of Apple stock at that time.
-So if the stock cost $500 at 10:30am, stock_prices_yesterday[60] = 500.
+# The indices are the time in minutes past trade opening time, which was 9:30am local time.
+# The values are the price in dollars of Apple stock at that time.
+# So if the stock cost $500 at 10:30am, stock_prices_yesterday[60] = 500.
 
-Write an efficient function that takes stock_prices_yesterday and returns the best profit I could have made from 1 purchase and 1 sale of 1 Apple stock yesterday."
+# Write an efficient function that takes stock_prices_yesterday and returns the best profit I could have made from 1 purchase and 1 sale of 1 Apple stock yesterday."
 
 
 array = [45, 2, 30, 19, 4, 40]
