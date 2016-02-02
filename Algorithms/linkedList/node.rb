113e6ca639df41b56135816bb1759ba15ddcce40
Arturo Perez <VrturoPerez@gmail.com>
2016-02-01 17:02:00 -0800

    add node class in node file

---
+++ Algorithms/linkedList/node.rb
@@ -0,0 +1,59 @@
+class Node
+    attr_accessor :value, :next
+
+    def initialize(value = nil)
+        @value = value
+    end
+
+    def to_s
+        @value
+    end
+end
+
+class SinglyLinkedList
+
+    attr_accessor :head
+
+    def initialize(first_value=nil)
+      @head = Node.new(first_value) if first_value
+    end
+
+    def add(value)
+      if head.nil?
+        head = Node.new(value)
+      else
+        current_node = @head
+        while current_node.next
+          current_node = current_node.next
+        end
+        current_node.next = Node.new(value)
+      end
+    end
+
+    def find(value)
+      current_node = head
+      while current_node != nil
+        return current_node if current_node.value == value
+        current_node = current_node.next
+      end
+      nil
+    end
+
+    def remove(value)
+      if head.value == value
+        head = head.next
+      else
+        current_node = head.next
+        prev_node = head
+        while current_node
+          if current_node.value == value
+            prev_node.next = current_node.next
+            return true
+          end
+          prev_node = current_node
+          current_node = current_node.next
+        end
+        nil
+      end
+    end
+end
