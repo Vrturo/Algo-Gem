3cb139dff28729659623a92e99352cdd94ed0b1d
Arturo Perez <VrturoPerez@gmail.com>
2016-02-03 16:22:18 -0800

    add Q class

---
+++ Algorithms/queue/queue.rb
@@ -0,0 +1,20 @@
+
+
+class Queue
+  def initialize
+    @store = Array.new
+  end
+
+  def dequeue
+    @store.pop
+  end
+
+  def enqueue(element)
+    @store.unshift(element)
+    self
+  end
+
+  def size
+    @store.size
+  end
+end
