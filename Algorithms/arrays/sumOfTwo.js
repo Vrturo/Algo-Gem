c87a900451b418aab64db060eea9ea3b00dd9233
Arturo Perez <VrturoPerez@gmail.com>
2016-02-11 23:03:11 -0800

    add solution

--- Algorithms/arrays/sumOfTwo.js
+++ Algorithms/arrays/sumOfTwo.js
@@ -1,5 +1,27 @@
 // From a unsorted array, check whether there are any two numbers that will sum up to a given number
 
+
+
+function sumFinder(arr, sum){
+      var differ = {},        // store the difference of sum and element. And then when new element comes
+                              // if difference is the object,
+                              // then i have a pair that sums up to the desired sum.
+      len = arr.length,
+      substract;
+
+  for(var i =0; i<len; i++){
+     substract = sum - arr[i];
+
+     if(differ[substract])
+       return true;
+     else
+       differ[arr[i]] = true;
+  }
+
+  return false;
+}
+
+
 function sumFinder(arr, sum){
   var len = arr.length;
 
