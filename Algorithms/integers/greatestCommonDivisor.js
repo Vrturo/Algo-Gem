e3d01672a293ea0533c6c412de8f5e10fa931325
Arturo Perez <VrturoPerez@gmail.com>
2016-02-11 19:35:23 -0800

    add solutions

--- Algorithms/integers/greatestCommonDivisor.js
+++ Algorithms/integers/greatestCommonDivisor.js
@@ -1,1 +1,26 @@
  // How would you find the greatest common divisor of two numbers?
+
+
+function greatestCommonDivisor(a, b){
+  var divisor = 2,
+      greatestDivisor = 1;
+
+  if (a < 2 || b < 2) { return 1 };
+
+  while(a >= divisor && b >= divisor){
+   if(a %divisor == 0 && b% divisor ==0){
+      greatestDivisor = divisor;
+    }
+   divisor++;
+  }
+  return greatestDivisor;
+}
+
+function greatestCommonDivisor(a, b){
+   if(b == 0) {
+      return a
+   }
+   else {
+     return greatestCommonDivisor(b, a%b);
+   }
+}
