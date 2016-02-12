5cd370aae0a704e9426d61f5b38232deaca98c57
Arturo Perez <VrturoPerez@gmail.com>
2016-02-11 19:20:21 -0800

    add recursive solution

--- Algorithms/integers/fibonacci.js
+++ Algorithms/integers/fibonacci.js
@@ -1,3 +1,28 @@
  // How do get nth Fibonacci number?
+
 // I create an array and start from iterate through.
+
+function fibonacci(n){
+  var fibo = [0, 1];
+
+  if (n <= 2) return 1;
+
+  for (var i = 2; i <=n; i++ ){
+   fibo[i] = fibo[i-1]+fibo[i-2];
+  }
+
+ return fibo[n];
+}
+
+
+# Recursion
+
+function fibonacci(n){
+  if(n<=1){
+    return n;
+  }
+  else {
+    return fibonacci(n-1) + fibonacci (n-2);
+  }
+}
 
