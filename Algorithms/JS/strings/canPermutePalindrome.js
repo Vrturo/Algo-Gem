7c94b2e1a9305c76358e6b6b06c97ad95ff7b8c9
Arturo Perez <VrturoPerez@gmail.com>
2016-02-12 23:31:37 -0800

    add solution

--- Algorithms/JS/strings/canPermutePalindrome.js
+++ Algorithms/JS/strings/canPermutePalindrome.js
@@ -2,3 +2,30 @@
 
 // For example,
 // "code" -> False, "aab" -> True, "carerac" -> True.
+
+
+var canPermutePalindrome = function(s) {
+    var freq = {},
+        splitStr = s.split(''),
+        odd = 0;
+    for(var i = 0; i < splitStr.length; i++){
+        var item = splitStr[i];
+        if(freq[item]){
+            freq[item] += 1;
+        }
+        else{
+            freq[item] = 1;
+        }
+    }
+    for (var key in freq){
+        if (freq[key]% 2 !== 0){
+            odd += 1;
+        }
+    }
+    if (odd<=1){
+        return true;
+    }
+    else{
+        return false;
+    }
+};
