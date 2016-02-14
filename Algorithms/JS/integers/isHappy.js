3f80824a37955c5340f439da6d93d88666836c1a
Arturo Perez <VrturoPerez@gmail.com>
2016-02-13 19:59:19 -0800

    add solution

--- Algorithms/JS/integers/isHappy.js
+++ Algorithms/JS/integers/isHappy.js
@@ -12,3 +12,36 @@
 // 82 + 22 = 68
 // 62 + 82 = 100
 // 12 + 02 + 02 = 1
+
+var isHappy = function(num) {
+    var m, n;
+    var c = [] ;
+
+    while(num != 1 && c[num] !== true)
+    {
+        c[num] = true ;
+        m = 0 ;
+        while (num > 0) {
+            n = num % 10 ;
+            m += n * n ;
+            num = (num  - n) / 10 ;
+        }
+        num = m ;
+    }
+    return (num == 1) ;
+};
+
+var isHappy = function(num){
+    function stringify(n){return String(n)}
+    var numStr = stringify(num),
+        total = 0,
+        done = false;
+    while(done === false){
+        for(var i = 0; i < numStr.length; i++){
+            total += Number(numStr[i]) * Number(numStr[i]);
+        }
+        (total === 1) ? done = true : numStr = stringify(total);
+        console.log(total);
+    }
+    return ((total === 1) ? true : false);
+};
