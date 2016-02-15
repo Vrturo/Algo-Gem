6b9cd9de7402c3e6fab8e5684a1afbe50dff246a
Arturo Perez <VrturoPerez@gmail.com>
2016-02-15 13:50:58 -0800

    add solution

--- Algorithms/JS/arrays/quickSort.js
+++ Algorithms/JS/arrays/quickSort.js
@@ -9,3 +9,29 @@
 //   *  Example: input = [3,9,1,4,7] , output = [1,3,4,7,9]               *
 
 
+var quickSort = function(input){
+  var pivot,
+      less = [],
+      greater = [],
+      sortedArr = [];
+  if (input.length <= 1){
+    return input;
+  }
+  else {
+    pivot = input[Math.floor(Math.random()*input.length)];
+    input.splice([input.indexOf(pivot)], 1);
+    for (var i = 0; i < input.length; i++){
+      if( input[i] <= pivot){
+        less.push(input[i]);
+      }
+      else{
+        greater.push(input[i]);
+      }
+    }
+  }
+  sortedArr.push(quickSort(less));
+  sortedArr.push(pivot);
+  sortedArr.push(quickSort(greater));
+  var merged = [].concat.apply([], sortedArr);
+  return merged;
+};
