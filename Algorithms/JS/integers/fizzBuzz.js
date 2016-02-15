b0faaf870f7d4503a65231f006e11cf748f30502
Arturo Perez <VrturoPerez@gmail.com>
2016-02-15 10:49:54 -0800

    add instructions

---
+++ Algorithms/JS/integers/fizzBuzz.js
@@ -0,0 +1,4 @@
+// Write a program that iterates the integers from 1 to 100, printing the numbers to the screen.
+// For each number that is a multiple of 3, however, print "Fizz" instead of the number.
+// For multiples of 5 print "Buzz" instead of the number.
+// For numbers that are multiples of both print "FizzBuzz."
