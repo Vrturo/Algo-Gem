92a39b35a8f0f7152ebb9cbde01aa21e848ebb95
Arturo Perez <VrturoPerez@gmail.com>
2016-02-15 13:50:35 -0800

    add instructions

---
+++ Algorithms/JS/arrays/quickSort.js
@@ -0,0 +1,11 @@
+// Problem: Quicksort                                                *
+//   *                                                                    *
+//   *  Prompt: Given an unsorted array of numbers,                       *
+//   *          return a sorted array using Quicksort sort.               *
+//   *                                                                    *
+//   *  Input:  An unsorted array                                         *
+//   *  Output: A sorted array                                            *
+//   *                                                                    *
+//   *  Example: input = [3,9,1,4,7] , output = [1,3,4,7,9]               *
+
+
