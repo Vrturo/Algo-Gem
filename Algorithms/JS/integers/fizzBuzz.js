c2485dc9773eebc2373dce1222b37e4d14e97070
Arturo Perez <VrturoPerez@gmail.com>
2016-02-15 10:51:18 -0800

    add solution

--- Algorithms/JS/integers/fizzBuzz.js
+++ Algorithms/JS/integers/fizzBuzz.js
@@ -2,3 +2,16 @@
 // For each number that is a multiple of 3, however, print "Fizz" instead of the number.
 // For multiples of 5 print "Buzz" instead of the number.
 // For numbers that are multiples of both print "FizzBuzz."
+
+function fizzBuzz() {
+    var res = "";
+    var resStr = ""
+    for (var i = 1; i < 100; i++) {
+        res = "";
+        if (i % 3 == 0) { res = "Fizz"; }
+        if (i % 5 == 0) { res += "Buzz"; }
+        if (!res) { res = i; }
+        resStr += res + " ";
+    }
+    return resStr;
+}
