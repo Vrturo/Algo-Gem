4dfde371000fc77f5ae2b171877c21326e77d629
Arturo Perez <VrturoPerez@gmail.com>
2016-02-15 17:40:56 -0800

    add solution

--- Algorithms/JS/arrays/mergeSort.js
+++ Algorithms/JS/arrays/mergeSort.js
@@ -13,3 +13,36 @@
 
    *                                                                    *
    **********************************************************************/
+
+
+function mergeSort(arr){
+    if (arr.length < 2)
+        return arr;
+
+    var mid = parseInt(arr.length / 2);
+    var left   = arr.slice(0, mid);
+    var right  = arr.slice(mid, arr.length);
+
+    return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(l, r)
+{
+    var result = [];
+
+    while (l.length && r.length) {
+        if (l[0] <= r[0]) {
+            result.push(l.shift());
+        } else {
+            result.push(r.shift());
+        }
+    }
+
+    while (l.length)
+        result.push(l.shift());
+
+    while (r.length)
+        result.push(r.shift());
+
+    return result;
+}
