5f9238522c5dfbc8efe06859eaccacf979ddf76c
Arturo Perez <VrturoPerez@gmail.com>
2016-02-15 17:40:21 -0800

    add description

---
+++ Algorithms/JS/arrays/mergeSort.js
@@ -0,0 +1,15 @@
+/**********************************************************************
+   *                                                                    *
+   *  Problem: Mergesort                                                *
+   *                                                                    *
+   *  Prompt: Given an unsorted array of numbers,                       *
+   *          return a sorted array using Mergesort sort.               *
+   *                                                                    *
+   *  Input:  An unsorted array                                         *
+   *  Output: A sorted array                                            *
+   *                                                                    *
+   *  Example: input = [3,9,1,4,7] , output = [1,3,4,7,9]               *
+   *                                                                    *
+
+   *                                                                    *
+   **********************************************************************/
