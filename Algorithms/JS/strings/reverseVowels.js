57d2f6be57d52807e10cb9443fb138ec306af8bd
Arturo Perez <VrturoPerez@gmail.com>
2016-02-17 23:47:30 -0800

    add solution

--- Algorithms/JS/strings/reverseVowels.js
+++ Algorithms/JS/strings/reverseVowels.js
@@ -5,3 +5,33 @@
 //ex. Input: "united states"
 //Output: "enated stitus"
 
+var reverseVowels = function(input){
+    var input = input.split('');
+    var i = 0;
+    var j = input.length-1;
+    var vowelLib = {
+        a: true,
+        e: true,
+        i: true,
+        o: true,
+        u: true
+    }
+
+    while (i < j){
+        if (vowelLib[input[i]]){
+            if (vowelLib[input[j]]){
+                var temp = input[i];
+                input[i] = input[j];
+                input[j] = temp;
+                i++;
+                j--;
+            } else {
+                j--;
+            }
+        } else {
+            i++;
+        }
+    }
+    input = input.join('');
+    return input;
+}
