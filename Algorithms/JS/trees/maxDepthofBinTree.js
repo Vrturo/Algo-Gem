e7d104a169e7a75bbee3a3d499456d0060c5db58
Arturo Perez <VrturoPerez@gmail.com>
2016-02-18 13:35:47 -0800

    add description

---
+++ Algorithms/JS/trees/maxDepthofBinTree.js
@@ -0,0 +1,15 @@
+// Given a binary tree, find its maximum depth.
+
+// The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
