0e453572719f95e9a13053bc87d97740b73a77dc
Arturo Perez <VrturoPerez@gmail.com>
2016-02-18 22:08:56 -0800

    add description

---
+++ Algorithms/JS/trees/binaryLvlOrderTraverse.js
@@ -0,0 +1,24 @@
+// Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+
+// For example:
+// Given binary tree {3,9,20,#,#,15,7},
+//     3
+//    / \
+//   9  20
+//     /  \
+//    15   7
+// return its level order traversal as:
+// [
+//   [3],
+//   [9,20],
+//   [15,7]
+// ]
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
