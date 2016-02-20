0d73484267eaefae4b3d828be3b1940a5bf74745
Arturo Perez <VrturoPerez@gmail.com>
2016-02-20 00:53:56 -0800

    add solution

--- Algorithms/JS/arrays/stockPrices.js
+++ Algorithms/JS/arrays/stockPrices.js
@@ -3,3 +3,17 @@
 // If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock),
 // design an algorithm to find the maximum profit.
 
+var maxProfit = function(prices) {
+    if(prices.length < 2){
+        return 0;
+    }
+    var minPrice = prices[0],
+        maxProfit = prices[1] - prices[0],
+        potentialProfit;
+    for(var i = 0; i < prices.length; i++){
+        potentialProfit = prices[i] - minPrice
+        maxProfit = maxProfit > potentialProfit ? maxProfit : potentialProfit
+        minPrice = minPrice < prices[i] ? minPrice : prices[i]
+    }
+    return maxProfit;
+};
