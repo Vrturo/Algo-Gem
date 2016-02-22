6f30da11033a4f20f9c37e8f362c8f8dcbd75fb8
Arturo Perez <VrturoPerez@gmail.com>
2016-02-22 13:14:34 -0800

    add solution

--- Algorithms/JS/arrays/groupAnagrams.js
+++ Algorithms/JS/arrays/groupAnagrams.js
@@ -12,3 +12,23 @@
 // For the return value, each inner list's elements must follow the lexicographic order.
 // All inputs will be in lower-case.
 
+
+
+
+var groupAnagrams = function(strs) {
+    var resultObj = {},
+        resultArr = [];
+    function addIndexToObj (e) {
+        var key = e.split("").sort().join("");
+        if (resultObj.hasOwnProperty(key)) {
+            resultObj[key].push(e);
+        } else {
+            resultObj[key] = [e];
+        }
+    }
+    strs.forEach(addIndexToObj);
+    for (var key in resultObj) {
+        resultArr.push(resultObj[key].sort());
+    }
+    return resultArr;
+};
