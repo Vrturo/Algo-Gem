e99dbff00cc43c75b5cf35b65781db2265e55e71
Arturo Perez <VrturoPerez@gmail.com>
2016-02-22 21:11:22 -0800

    add description

---
+++ Algorithms/JS/integers/coinChange.js
@@ -0,0 +1,26 @@
+/**********************************************************************
+ *  Problem : Coin Change                                             *
+ *                                                                    *
+ *  Prompt: US Currency has coins in circulation with the             *
+ *          following value in cents: [1, 5, 10, 25, 50, 100]         *
+ *                                                                    *
+ *          Given a value, find how many unique ways to make change   *
+ *          given the coin values in circulation.                     *
+ *                                                                    *
+ *  Input:  An integer N (value from which to make change)            *
+ *  Output: An integer (number of unique ways to make change)         *
+ *                                                                    *
+ *  Time Complexity: NA                                               *
+ *  Auxiliary Space Complexity: NA                                    *
+ *                                                                    *
+ *  Example: input = 11                                               *
+ *           output = 4 ([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],           *
+ *                       [5, 1, 1, 1, 1, 1, 1],                       *
+ *                       [5, 5, 1],                                   *
+ *                       [10, 1])                                     *
+ *                                                                    *
+ *  Challenge Mode: Solve coin change with you solution having:       *
+ *                  Time Complexity: O(nm)                            *
+ *                  Auxiliary Space Complexity: O(n)                  *
+ *  Hint: www.geeksforgeeks.org/dynamic-programming-set-7-coin-change *
+ **********************************************************************/
