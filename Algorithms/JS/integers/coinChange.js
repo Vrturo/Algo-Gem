d594bbffea97fe82e71b2927277a84b5b0c4ac50
Arturo Perez <VrturoPerez@gmail.com>
2016-02-22 21:11:49 -0800

    add one solution, 1 working on it

--- Algorithms/JS/integers/coinChange.js
+++ Algorithms/JS/integers/coinChange.js
@@ -24,3 +24,39 @@
  *                  Auxiliary Space Complexity: O(n)                  *
  *  Hint: www.geeksforgeeks.org/dynamic-programming-set-7-coin-change *
  **********************************************************************/
+
+
+function coinChange(n){
+  var CURRENCY = [1, 5, 10, 25, 50, 100];
+  var result = [];
+  function traverse(rem, arr){
+    if(rem === 0){
+      result.push(arr);
+      return;
+    }
+    for(var i = 0; i < CURRENCY.length; i++){
+      var temp = rem - CURRENCY[i]; // 10
+      if (temp >= 0 ){ // rem = 11
+        arr.push(CURRENCY[i]); // [1]
+        traverse(temp, arr); // traverse(10, [1])
+      }
+    }
+  }
+  traverse(n, [])
+  return result
+}
+
+function coinChange(total, coins){
+  if(total === 0){
+    return 1;
+  }
+  else if(total < 0){
+    return 0;
+  }
+  var ways = 0;
+  for (var i = 0; i < coins.length; i++){
+    avail = coins.slice(i);
+    ways += coinChange(total-coins[i], avail)
+  }
+  return ways.length;
+}
