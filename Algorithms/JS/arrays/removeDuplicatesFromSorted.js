e592efdc58aecd2aa929f886b3c32389973197c8
Arturo Perez <VrturoPerez@gmail.com>
2016-02-29 17:35:05 -0800

    add solution

--- Algorithms/JS/arrays/removeDuplicatesFromSorted.js
+++ Algorithms/JS/arrays/removeDuplicatesFromSorted.js
@@ -7,3 +7,18 @@
 
 // Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
 // It doesn't matter what you leave beyond the new length.
+
+
+
+
+
+
+var removeDuplicates = function(nums) {
+  var p1 = 0;
+    for (var p2 = 0; p2 < nums.length; p2++) {
+        if (nums[p1] !== nums[p2]) {
+            nums[++p1] = nums[p2];
+        }
+    }
+    return p1+1;
+};
