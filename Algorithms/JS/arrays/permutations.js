d48ca542f8bdab77ccd4843a070c40192175fdeb
Arturo Perez <VrturoPerez@gmail.com>
2016-03-04 21:14:12 -0800

    add solution

--- Algorithms/JS/arrays/permutations.js
+++ Algorithms/JS/arrays/permutations.js
@@ -3,3 +3,27 @@
 // For example,
 // [1,2,3] have the following permutations:
 // [1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], and [3,2,1].
+
+
+var permute = function(nums) {
+    var numsLen = nums.length,
+        result = [];
+
+    function getPermutations (currentArray, candidateArray) {
+        var candidateArrayLen = candidateArray.length, index;
+        if (candidateArrayLen < 1) {
+            result.push(currentArray.slice());
+        }
+        else {
+            for (index = 0; index < candidateArrayLen; index++) {
+                var nextCandidate = candidateArray.slice();
+                currentArray.push(candidateArray[index]);
+                nextCandidate.splice(index, 1);
+                getPermutations (currentArray, nextCandidate);
+                currentArray.pop();
+            }
+        }
+    }
+    getPermutations([], nums);
+    return result;
+};
