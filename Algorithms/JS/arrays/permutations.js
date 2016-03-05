9725aca520b98b17481fc9ddeddec7775f6917a8
Arturo Perez <VrturoPerez@gmail.com>
2016-03-05 00:34:29 -0800

    add logic to algo

--- Algorithms/JS/arrays/permutations.js
+++ Algorithms/JS/arrays/permutations.js
@@ -5,25 +5,43 @@
 // [1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], and [3,2,1].
 
 
-var permute = function(nums) {
-    var numsLen = nums.length,
-        result = [];
 
-    function getPermutations (currentArray, candidateArray) {
-        var candidateArrayLen = candidateArray.length, index;
-        if (candidateArrayLen < 1) {
-            result.push(currentArray.slice());
-        }
-        else {
-            for (index = 0; index < candidateArrayLen; index++) {
-                var nextCandidate = candidateArray.slice();
-                currentArray.push(candidateArray[index]);
-                nextCandidate.splice(index, 1);
-                getPermutations (currentArray, nextCandidate);
+
+// Logic
+// create a result array to push our permutations in and return
+// create a get getPermutations function that takes in an array, and our input
+  // create a base case, if input array is empty push copy of current array into result
+  // if input array is not empty, loop through input array
+  // else
+    // create a copy of our input array for our next input
+    // push the item of the inputArr that the for loop is on, into the currentArr
+    // splice the index of the nextInput array
+    // run the getPermutations function on the current array and nextInput array
+    // pop last array in current array
+
+  // run get permutations function on an empty arr and nums or our input
+  // return result
+
+
+var permute = function(nums) {
+    //nums are distinct numbers.
+    var result = [];
+
+    function getPermutations (currentArray, inputArr) {
+        if (inputArr.length < 1) {
+            result.push(currentArray.slice()); //Here we must do a copy. Because an array is an object,
+                                               // every time we're passing its reference into a new function.
+        } else {
+            for (var i = 0; i < inputArr.length; i++) {
+                var nextInput = inputArr.slice(); //Same reason, array is object, copy is needed.
+                currentArray.push(inputArr[i]);
+                nextInput.splice(i, 1);
+                getPermutations (currentArray, nextInput);
                 currentArray.pop();
             }
         }
     }
+
     getPermutations([], nums);
     return result;
 };
