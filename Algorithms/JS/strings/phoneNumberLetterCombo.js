e2dee810ae611d07ade9518ed0b0ed539a08b807
Arturo Perez <VrturoPerez@gmail.com>
2016-03-05 20:48:57 -0800

    add logic

--- Algorithms/JS/strings/phoneNumberLetterCombo.js
+++ Algorithms/JS/strings/phoneNumberLetterCombo.js
@@ -1,5 +1,4 @@
 // Given a digit string, return all possible letter combinations that the number could represent.
-
 // A mapping of digit to letters (just like on the telephone buttons) is given below.
 
 // 1    2     3
@@ -13,6 +12,30 @@
 
 // Input: Digit string "23"
 // Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+
+
+//Logic
+
+// create a library array to store letters in sub arrays the corresponding number as the index
+// if digits is null or empty return empty array
+// create a var temp to store a temp array
+// create a getString function that takes the digits string, temp array, result array, and our letter library
+  // set a base case of digits length is 0
+    //if digits length is 0
+      // create an array
+      // loop through temp array
+      // set arr = temp
+      // join array and push it into result
+    // grab first digit of substring and parse into integer and set it to currentDigit
+    // create a letters variable that check against the letter library returns the set of letters of that digit
+    // loop through the letters
+      // push the first letter of the set of letters into temp
+      // run the getString function digits, temp array, final result array, and the letter library
+      // remove whats in temp with pop
+// call getString function on remainding digits, temp array, final result array, and the letter library
+// return result
+
+
 
 var letterCombinations = function(digits) {
      var letterLib = [];
@@ -30,27 +53,28 @@ var letterCombinations = function(digits) {
         return result;
     }
     var temp = [];
+
+    function getString(digits, temp, result, letterLib){
+        if(digits.length === 0){
+            var arr = [];
+            for(var i=0; i<temp.length; i++){
+                arr[i] = temp[i];
+            }
+            result.push(arr.join(''));
+            return;
+        }
+        var currentDigit = parseInt(digits.substring(0,1));
+        var letters = letterLib[currentDigit];
+        for(var i=0; i<letters.length; i++){
+            temp.push(letters.charAt(i));
+            getString(digits.substring(1), temp, result, letterLib);
+            temp.pop();
+        }
+    }
     getString(digits, temp, result, letterLib);
     return result;
 };
 
-function getString(digits, temp, result, letterLib){
-    if(digits.length === 0){
-        var arr = [];
-        for(var i=0; i<temp.length; i++){
-            arr[i] = temp[i];
-        }
-        result.push(arr.join(''));
-        return;
-    }
 
-    var curr = parseInt(digits.substring(0,1));
-    var letters = letterLib[curr];
-    for(var i=0; i<letters.length; i++){
-        temp.push(letters.charAt(i));
-        getString(digits.substring(1), temp, result, letterLib);
-        temp.pop();
-    }
-}
 
 // -----------------------------
