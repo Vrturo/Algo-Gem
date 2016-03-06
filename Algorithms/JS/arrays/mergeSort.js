0b85e74a7bb00e97cf3bc9d423f9fcd5a1bc268a
Arturo Perez <VrturoPerez@gmail.com>
2016-03-05 22:56:35 -0800

    new file

--- Algorithms/JS/arrays/mergeSort.js
+++ Algorithms/JS/arrays/mergeSort.js
@@ -45,3 +45,44 @@ function merge(l, r){
 
     return result;
 }
+
+// ----------------------
+
+// using pointers
+
+function merge(l, r){
+
+    var result = [];
+    var p1 = 0;
+    var p2 = 0;
+
+    while (p1 < l.length && p2 < r.length) {
+        if (l[p1][1] >= r[p2][1]) {
+            result.push(l[p1]);
+            p1++;
+        } else {
+            result.push(r[p2]);
+            p2++;
+        }
+    }
+    while (p1 < l.length){
+        result.push(l[p1]);
+        p1++;
+    }
+    while (p2 < r.length){
+        result.push(r[p2]);
+        p2++;
+    }
+    return result;
+}
+
+function mergeSort(arr){
+    if (arr.length < 2){
+        return arr;
+     }
+    var mid = parseInt(arr.length / 2);
+    var left   = arr.slice(0, mid);
+    var right  = arr.slice(mid, arr.length);
+
+    return merge(mergeSort(left), mergeSort(right));
+}
