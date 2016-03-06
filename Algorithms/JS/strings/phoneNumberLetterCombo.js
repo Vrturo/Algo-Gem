452456bdf9c87130e850a256b0d18c60d30b2968
Arturo Perez <VrturoPerez@gmail.com>
2016-03-05 19:00:09 -0800

    add description:

---
+++ Algorithms/JS/strings/phoneNumberLetterCombo.js
@@ -0,0 +1,16 @@
+// Given a digit string, return all possible letter combinations that the number could represent.
+
+// A mapping of digit to letters (just like on the telephone buttons) is given below.
+
+// 1    2     3
+//     abc   def
+
+//  4    5    6
+// ghi  jkl  mno
+
+//  7    8    9
+// pqrs tuv  wxyz
+
+// Input: Digit string "23"
+// Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+
