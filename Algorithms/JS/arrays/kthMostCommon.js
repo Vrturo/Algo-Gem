fbfc243e5449d5a1d4214c12cb87eb655f3e0d2f
Arturo Perez <VrturoPerez@gmail.com>
2016-03-09 15:42:26 -0800

    add solution

--- Algorithms/JS/arrays/kthMostCommon.js
+++ Algorithms/JS/arrays/kthMostCommon.js
@@ -3,3 +3,24 @@
 // Example:
 // [1,2,2,3,3,4,4,4], 4 => 1
 
+
+
+
+function common(arr, k){
+  var freqHash = {},
+      storage = [];
+
+  for(var i = 0; i < arr.length; i++){
+    if (freqHash[arr[i]] === undefined){
+      freqHash[arr[i]] = 1;
+    }
+    else{
+      freqHash[arr[i]] += 1;
+    }
+  }
+  for(var key in freqHash){
+    storage.push(key);
+  }
+  storage = storage.sort(function(a,b){return b - a});
+  return storage[k-1];
+}
