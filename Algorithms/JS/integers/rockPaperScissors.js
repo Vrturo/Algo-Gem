5b7c1c6046a978cfe423f0000fa1d2b929e2574a
Arturo Perez <VrturoPerez@gmail.com>
2016-03-15 16:35:06 -0700

    add description

---
+++ Algorithms/JS/integers/rockPaperScissors.js
@@ -0,0 +1,22 @@
+/**
+ *
+ * Problem: paper rock scissors
+ *
+ * Write function paperRockScissors that takes in a number n
+ * and returns all the possibilities that exist for playing
+ * n rounds of paper rock scissors.
+ *
+ * For example, if n = 2, you would return:
+ *
+ * [ [ 'paper', 'paper' ],
+  [ 'paper', 'rock' ],
+  [ 'paper', 'scissors' ],
+  [ 'rock', 'paper' ],
+  [ 'rock', 'rock' ],
+  [ 'rock', 'scissors' ],
+  [ 'scissors', 'paper' ],
+  [ 'scissors', 'rock' ],
+  [ 'scissors', 'scissors' ] ]
+ *
+ *
+ */
