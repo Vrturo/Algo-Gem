c3507ac6f629f576781782d7143fe9ea09be822f
Arturo Perez <VrturoPerez@gmail.com>
2016-03-15 21:27:50 -0700

    add solution

--- Algorithms/JS/trees/invertBinaryTree.js
+++ Algorithms/JS/trees/invertBinaryTree.js
@@ -15,3 +15,16 @@
 // Input: [4, 2, 7, 1, 3, 6, 9]
 
 // Output: [4, 7, 2, 9, 6, 3, 1]
+
+
+var invertTree = function(root) {
+    if(!root || root.left === null && root.right === null){
+        return root;
+    }
+    var temp = root.left;
+    root.left = root.right;
+    root.right = temp;
+    invertTree(root.left);
+    invertTree(root.right);
+    return root;
+};
