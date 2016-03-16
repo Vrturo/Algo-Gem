94ae55ce95935964918bd0f030643b079adf78d9
Arturo Perez <VrturoPerez@gmail.com>
2016-03-15 21:32:50 -0700

    add logic

--- Algorithms/JS/trees/invertBinaryTree.js
+++ Algorithms/JS/trees/invertBinaryTree.js
@@ -17,6 +17,14 @@
 // Output: [4, 7, 2, 9, 6, 3, 1]
 
 
+// if root is null
+// or the root has no children return root
+// create a temp variable and set it to root.left to store for now
+// set root.left to root.right
+// set root.right to the temp variable we stored
+// invertTree on each child of the root
+// return root
+
 var invertTree = function(root) {
     if(!root || root.left === null && root.right === null){
         return root;
