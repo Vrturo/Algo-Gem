d5fe4cc2d7fcd6b3eb792d305a084b890de8460c
Arturo Perez <VrturoPerez@gmail.com>
2016-04-22 13:33:37 -0700

    add solution

--- Algorithms/JS/trees/serializeDeserializeTree.js
+++ Algorithms/JS/trees/serializeDeserializeTree.js
@@ -19,3 +19,29 @@
 // You do not necessarily need to follow this format, so please be creative and come up with different approaches yourself.
 
 
+var serializeHelper = function(root,lst) {
+    if (!root) { lst.push("#"); }
+    else {
+        lst.push(root.val);
+        serializeHelper(root.left,lst);
+        serializeHelper(root.right,lst);
+    }
+};
+var serialize = function(root) {
+    var lst=[];
+    serializeHelper(root,lst);
+    return lst.toString();
+};
+var deserializeHelper = function(lst) {
+    if (lst.length<1) { return null; }
+    var val=lst.splice(0,1);
+    if (val[0]==="#") { return null; }
+    var root=new TreeNode(parseInt(val));
+    root.left=deserializeHelper(lst);
+    root.right=deserializeHelper(lst);
+    return root;
+};
+var deserialize = function(data) {
+    if(!data||data.length===0) { return null; }
+    return deserializeHelper(data.split(","));
+};
