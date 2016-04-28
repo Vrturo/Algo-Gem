2d6e7316612d665d293473aa23419c4e2374e830
Arturo Perez <VrturoPerez@gmail.com>
2016-04-28 15:17:04 -0700

    add solution

--- Algorithms/JS/arrays/maxSubArray.js
+++ Algorithms/JS/arrays/maxSubArray.js
@@ -2,3 +2,17 @@
 
 // For example, given the array [−2,1,−3,4,−1,2,1,−5,4],
 // the contiguous subarray [4,−1,2,1] has the largest sum = 6.
+
+
+var maxSubArray = function(nums) {
+  var max = nums[0],
+      sum = 0;
+  nums.forEach((e, i) => {
+    sum += e;
+    if (sum > max)
+      max = sum;
+    if (sum < 0)
+      sum = 0;
+  });
+  return max;
+};
