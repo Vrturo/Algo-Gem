36ec036fcb7719788519df5ab982273ad9680a78
Arturo Perez <VrturoPerez@gmail.com>
2016-04-28 16:13:13 -0700

    add solution

--- Algorithms/JS/strings/isIsomorphic.js
+++ Algorithms/JS/strings/isIsomorphic.js
@@ -14,3 +14,22 @@
 
 // Note:
 // You may assume both s and t have the same length.
+
+
+
+var isIsomorphic = function(s, t) {
+    var obj = {};
+
+    for(var i = 0; i < s.length; i++){
+        if(obj['s' + s[i]] === undefined){
+          obj['s' + s[i]] = t[i];
+        }
+        if(obj['t' + t[i]] === undefined){
+          obj['t' + t[i]] = s[i];
+        }
+        if(t[i] !== obj['s' + s[i]] || s[i] !== obj['t' + t[i]]){
+          return false;
+        }
+    }
+    return true;
+};
