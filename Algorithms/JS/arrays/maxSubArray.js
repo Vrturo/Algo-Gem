dace7382bd3666a96d9222ef70cfbca083b65c3b
Arturo Perez <VrturoPerez@gmail.com>
2016-04-28 15:17:25 -0700

    add process

--- Algorithms/JS/arrays/maxSubArray.js
+++ Algorithms/JS/arrays/maxSubArray.js
@@ -3,6 +3,14 @@
 // For example, given the array [−2,1,−3,4,−1,2,1,−5,4],
 // the contiguous subarray [4,−1,2,1] has the largest sum = 6.
 
+// set variable max as the first subset of nums array
+// set sum to 0
+// loop through nums with a for each loop
+  // set or add sum to the first element
+  // if sum is greater than max set it
+  // if sum is less than 0 set to 0
+// return max
+
 
 var maxSubArray = function(nums) {
   var max = nums[0],
