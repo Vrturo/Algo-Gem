7993f72643fb0134e1717de6dee58778664e5cc9
Arturo Perez <VrturoPerez@gmail.com>
2016-05-23 20:43:46 -0700

    add solution

--- Algorithms/JS/integers/addDigits.js
+++ Algorithms/JS/integers/addDigits.js
@@ -1,1 +1,12 @@
-addDigits.js
+var addDigits = function(num) {
+    if(num<10){
+        return num;
+    }
+    var strArr = num.toString().split("");
+    var sum = strArr.reduce((a,b) => Number(a) + Number(b));
+    if(sum.toString().split("").length === 1){
+        return sum;
+    }else{
+        addDigits(sum);
+    }
+};
