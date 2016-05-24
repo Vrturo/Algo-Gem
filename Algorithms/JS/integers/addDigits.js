367e150a6902d6108a744bb445bdab3b0144f86f
Arturo Perez <VrturoPerez@gmail.com>
2016-05-23 21:07:25 -0700

    add desription

--- Algorithms/JS/integers/addDigits.js
+++ Algorithms/JS/integers/addDigits.js
@@ -1,4 +1,11 @@
+// Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
 
+// For example:
+
+// Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
+
+// Follow up:
+// Could you do it without any loop/recursion in O(1) runtime?
 
 var addDigits = function(num) {
    if(num === 0){
