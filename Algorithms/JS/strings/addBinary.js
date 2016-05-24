2ce6cf5bf6e001d592f822ef5c9cf06dbdb83fd5
Arturo Perez <VrturoPerez@gmail.com>
2016-05-23 22:07:13 -0700

    add unfinished solution

--- Algorithms/JS/strings/addBinary.js
+++ Algorithms/JS/strings/addBinary.js
@@ -1,1 +1,28 @@
-addBinary.js
+var addBinary = function(a, b) {
+    var aVal = 0;
+    var bVal = 0;
+    var counter = 0;
+    for(var i = 0; i < a.length; i++){
+        if(a[i] === "1"){
+            aVal += Math.pow(2, a.length-1 - i);
+        }
+    }
+    for(var j = 0; j < b.length; j++){
+        if(b[j] === "1"){
+            bVal += Math.pow(2, b.length-1 - j);
+        }
+    }
+    var sum = aVal + bVal;
+    var str = Array(Math.floor(sum/Math.floor(Math.log2(sum)))+ 3).join('x');
+
+    while(sum>1){
+        str[counter] = sum%2;
+        console.log(str);
+        // console.log(counter)
+        // console.log(sum%2)
+        counter++;
+        sum = Math.floor(sum/2);
+        // console.log(str);
+    }
+    return str;
+};
