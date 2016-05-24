ab719e0ea1a5855b7e36cffe4e3ec6a92a12e1fe
Arturo Perez <VrturoPerez@gmail.com>
2016-05-23 20:58:37 -0700

    edit solution

--- Algorithms/JS/integers/addDigits.js
+++ Algorithms/JS/integers/addDigits.js
@@ -3,10 +3,12 @@ var addDigits = function(num) {
         return num;
     }
     var strArr = num.toString().split("");
-    var sum = strArr.reduce((a,b) => Number(a) + Number(b));
+    var sum = strArr.reduce(function(a,b) {return Number(a) + Number(b)});
     if(sum.toString().split("").length === 1){
+        console.log(sum);
         return sum;
     }else{
         addDigits(sum);
     }
 };
+addDigits(20);
