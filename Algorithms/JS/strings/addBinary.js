eca3a99e992f408d1e8af16a94d84d41368021f0
Arturo Perez <VrturoPerez@gmail.com>
2016-05-23 22:07:38 -0700

    add description

--- Algorithms/JS/strings/addBinary.js
+++ Algorithms/JS/strings/addBinary.js
@@ -1,3 +1,11 @@
+// Given two binary strings, return their sum (also a binary string).
+
+// For example,
+// a = "11"
+// b = "1"
+// Return "100".
+
+
 var addBinary = function(a, b) {
     var aVal = 0;
     var bVal = 0;
