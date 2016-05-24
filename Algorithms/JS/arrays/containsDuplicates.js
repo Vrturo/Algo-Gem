004024fac7da813708fa0d21f31347d34e365d1b
Arturo Perez <VrturoPerez@gmail.com>
2016-05-24 14:19:10 -0700

    add solution

--- Algorithms/JS/arrays/containsDuplicates.js
+++ Algorithms/JS/arrays/containsDuplicates.js
@@ -2,3 +2,19 @@
 
 // Your function should return true if any value appears at least twice in the array,
 // and it should return false if every element is distinct.
+
+
+
+
+var containsDuplicate = function(nums) {
+    var map = {};
+    for(var i = nums.length; i--;){
+        if(map[nums[i]] !== undefined){
+            return true;
+        }
+        else{
+            map[nums[i]] = true;
+        }
+    }
+    return false;
+};
