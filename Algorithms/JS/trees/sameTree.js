6777812907121cf7b474aa9a90273c1c4166659a
Arturo Perez <VrturoPerez@gmail.com>
2016-05-25 12:26:11 -0700

    add solution

--- Algorithms/JS/trees/sameTree.js
+++ Algorithms/JS/trees/sameTree.js
@@ -1,3 +1,17 @@
 // Given two binary trees, write a function to check if they are equal or not.
 
 // Two binary trees are considered equal if they are structurally identical and the nodes have the same value.
+
+var isSameTree = function(p, q) {
+    if(p === null){
+         return q === null;
+    }
+    if(q === null){
+        return p === null;
+    }
+    if(p.val !== q.val){
+        return false;
+    }
+
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
