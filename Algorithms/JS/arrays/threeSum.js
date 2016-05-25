4004c63f78bb42358ff5fe9fb943809cb1ac81bf
Arturo Perez <VrturoPerez@gmail.com>
2016-05-25 11:40:08 -0700

    add solution:

--- Algorithms/JS/arrays/threeSum.js
+++ Algorithms/JS/arrays/threeSum.js
@@ -9,3 +9,26 @@
 //     A solution set is:
 //     (-1, 0, 1)
 //     (-1, -1, 2)
+
+
+var threeSum = function(nums) {
+    nums = nums.sort(function(a, b) { return a - b; });
+
+    var results = [];
+    var i, j, k, n = nums.length;
+
+    for (i = 0; i < n; ++i) {
+        if (i > 0 && nums[i] === nums[i-1]) continue;
+
+        for (j = i + 1; j < n; ++j) {
+            if (j > i + 1 && nums[j] === nums[j-1]) continue;
+
+            for (k = j + 1; k < n; ++k) {
+                if (k > j + 1 && nums[k] === nums[k-1]) continue;
+
+                if (nums[i] + nums[j] + nums[k] === 0) results.push([nums[i], nums[j], nums[k]]);
+            }
+        }
+    }
+    return results;
+};
