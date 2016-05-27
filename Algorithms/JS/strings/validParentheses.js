d476fcdcff29d4b26b5dbcf136e67dbbc3227328
Arturo Perez <VrturoPerez@gmail.com>
2016-05-27 13:32:24 -0700

    add description

--- Algorithms/JS/strings/validParentheses.js
+++ Algorithms/JS/strings/validParentheses.js
@@ -1,1 +1,3 @@
-validParentheses.js
+// Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+// The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
