a247ac11b1f07990d99edc390dfffda1b2859096
Arturo Perez <VrturoPerez@gmail.com>
2016-05-26 17:30:35 -0700

    add solution

--- Algorithms/JS/arrays/majorityElement.js
+++ Algorithms/JS/arrays/majorityElement.js
@@ -6,6 +6,19 @@
 var majorityElement = function(nums) {
     var map = {};
 
+    for (var i = 0; i < nums.length; i++) {
+        var times = map[nums[i]] || 0;
+        if ((times + 1) > parseInt(nums.length/2, 10)) {
+            return nums[i];
+        }
+        map[nums[i]] = times + 1;
+    }
+};
+
+
+var majorityElement = function(nums) {
+    var map = {};
+
     for(var i=0;i<nums.length;i++){
         map[nums[i]] = map[nums[i]] + 1 || 1;
         if(map[nums[i]] > nums.length / 2){
