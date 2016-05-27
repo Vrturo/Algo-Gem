c9d5774b38decf424ee1d3edc1419351936f5ca5
Arturo Perez <VrturoPerez@gmail.com>
2016-05-26 17:27:48 -0700

    add solution

--- Algorithms/JS/arrays/majorityElement.js
+++ Algorithms/JS/arrays/majorityElement.js
@@ -1,3 +1,15 @@
 // Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
 
 // You may assume that the array is non-empty and the majority element always exist in the array.
+
+
+var majorityElement = function(nums) {
+    var map = {};
+
+    for(var i=0;i<nums.length;i++){
+        map[nums[i]] = map[nums[i]] + 1 || 1;
+        if(map[nums[i]] > nums.length / 2){
+            return nums[i];
+        }
+    }
+};
