4e9b0e30574c3bf073cc5d776f6d5113640896cb
Arturo Perez <VrturoPerez@gmail.com>
2016-05-26 22:55:21 -0700

    add instructions

--- Algorithms/JS/strings/badWords.js
+++ Algorithms/JS/strings/badWords.js
@@ -1,1 +1,5 @@
-badWords.js
+/*
+   Implement the `hasBadwords` function in the code below to return a boolean if the
+   message contains a badword in it or not.  A badword is contained in the message
+   if the word appears in the sentence, ignoring adjacent punctuation and case.
+*/
