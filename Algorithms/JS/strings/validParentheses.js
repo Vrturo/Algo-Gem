074914626cc86d1d2c7392c603380cfe416368dd
Arturo Perez <VrturoPerez@gmail.com>
2016-05-28 14:59:05 -0700

    re do solution

--- Algorithms/JS/strings/validParentheses.js
+++ Algorithms/JS/strings/validParentheses.js
@@ -33,11 +33,14 @@ var isValid = function(s) {
 
 var isValid = function(s) {
     var st = [];
-    for(var l of s)
-        if ((i="({[]})".indexOf(l))>-1)
-            if (st[st.length-1]+i===5)
+    for(var l of s){
+        if ((i="({[]})".indexOf(l))>-1){
+            if (st[st.length-1]+i===5){
                 st.length--;
-            else
+              }else{
                 st.push(i);
+              }
+        }
+    }
     return st.length===0;
 }
