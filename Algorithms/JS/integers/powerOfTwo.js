3f9068aa636a38610d99cd92c515e535caed8a9e
Arturo Perez <VrturoPerez@gmail.com>
2016-05-29 22:40:47 -0700

    add solution

--- Algorithms/JS/integers/powerOfTwo.js
+++ Algorithms/JS/integers/powerOfTwo.js
@@ -1,1 +1,8 @@
 // Given an integer, write a function to determine if it is a power of two.
+
+var isPowerOfTwo = function(n) {
+    if(n < 1){
+        return false;
+    }
+        return (0 === ((n - 1) & n));
+};
