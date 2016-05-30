cefac5b1119c43fadb3f58b035685bfe2c72e011
Arturo Perez <VrturoPerez@gmail.com>
2016-05-29 22:47:20 -0700

    add steps

--- Algorithms/JS/integers/powerOfTwo.js
+++ Algorithms/JS/integers/powerOfTwo.js
@@ -15,6 +15,12 @@ var isPowerOfTwo = function(n) {
 };
 
 
+// Javascript has only natural log build-in function Math.log, so
+
+// log2(n) = ln(n)/ln(2)
+
+// Moreover, javascript works bad with huge numbers, thats why we divide n by 4, which is enough to pass all test cases.
+
 var isPowerOfTwo = function(n) {
     return Number.isInteger(Math.log(n/4)/Math.log(2));
 };
