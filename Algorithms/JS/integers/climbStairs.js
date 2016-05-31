1c714ad7c492524439a8779c1c5be059b571f405
Arturo Perez <VrturoPerez@gmail.com>
2016-05-31 12:31:46 -0700

    add solution

--- Algorithms/JS/integers/climbStairs.js
+++ Algorithms/JS/integers/climbStairs.js
@@ -3,3 +3,20 @@
 // Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 
 
+var climbStairs = function(n) {
+    if(n === 0)  {
+      return 0;
+    }
+    if(n === 1){
+      return 1;
+    }
+    if(n === 2){
+      return 2;
+    }
+
+    var arr = [1,2];
+    for(var i = 2; i < n; i++) {
+        arr[i] = arr[i-1] + arr[i-2];
+    }
+    return arr[n-1];
+};
