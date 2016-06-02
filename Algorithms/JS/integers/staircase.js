d3b04ad73e2e5830f9373588b1695822793747c3
Arturo Perez <VrturoPerez@gmail.com>
2016-06-02 15:00:03 -0700

    add desc

--- Algorithms/JS/integers/staircase.js
+++ Algorithms/JS/integers/staircase.js
@@ -1,1 +1,15 @@
-staircase.js
+// Your teacher has given you the task of drawing a staircase structure. Being an expert programmer, you decided to make a program to draw it for you instead. Given the required height, can you print a staircase as shown in the example?
+
+// Input
+// You are given an integer  depicting the height of the staircase.
+
+// Output
+// Print a staircase of height  that consists of # symbols and spaces. For example for , here's a staircase of that height:
+
+//      #
+//     ##
+//    ###
+//   ####
+//  #####
+// ######
+// Note: The last line has 0 spaces before it.
