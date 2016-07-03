7f00f69f00dfe9a89365a00324eec9285e908069
Arturo Perez <VrturoPerez@gmail.com>
2016-07-02 21:57:19 -0700

    add solution

--- Algorithms/JS/strings/pangram.js
+++ Algorithms/JS/strings/pangram.js
@@ -1,28 +1,39 @@
-Input Format
+// Input Format
 
-Input consists of a string .
+// Input consists of a string .
 
-Constraints
-It may contain spaces, lower case and upper case letters.
-Lower-case and upper-case instances of a letter are considered the same.
+// Constraints
+// It may contain spaces, lower case and upper case letters.
+// Lower-case and upper-case instances of a letter are considered the same.
 
-Output Format
+// Output Format
 
-Output a line containing pangram if  is a pangram, otherwise output not pangram.
+// Output a line containing pangram if  is a pangram, otherwise output not pangram.
 
-Sample Input
+// Sample Input
 
-Input #1
+// Input #1
 
-We promptly judged antique ivory buckles for the next prize
-Input #2
+// We promptly judged antique ivory buckles for the next prize
+// Input #2
 
-We promptly judged antique ivory buckles for the prize
-Sample Output
+// We promptly judged antique ivory buckles for the prize
+// Sample Output
 
-Output #1
+// Output #1
 
-pangram
-Output #2
+// pangram
 
-not pangram
+// Output #2
+
+// not pangram
+
+
+function isPangram(input) {
+    var letters = 'abcdefghijklmnopqrstuvwxyz';
+    input = input.toLowerCase().replace(/[^a-z]/g,'');
+    for (var i = 0; i < 26; i++){
+        if (input.indexOf(letters[i]) < 0)  return "not pangram"
+    }
+    return "pangram"
+}
