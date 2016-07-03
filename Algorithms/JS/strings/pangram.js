e0b9f170940c52e51f0c2be0fc543e77c875f576
Arturo Perez <VrturoPerez@gmail.com>
2016-07-02 20:54:05 -0700

    add description

--- Algorithms/JS/strings/pangram.js
+++ Algorithms/JS/strings/pangram.js
@@ -1,1 +1,28 @@
-pangram.js
+Input Format
+
+Input consists of a string .
+
+Constraints
+It may contain spaces, lower case and upper case letters.
+Lower-case and upper-case instances of a letter are considered the same.
+
+Output Format
+
+Output a line containing pangram if  is a pangram, otherwise output not pangram.
+
+Sample Input
+
+Input #1
+
+We promptly judged antique ivory buckles for the next prize
+Input #2
+
+We promptly judged antique ivory buckles for the prize
+Sample Output
+
+Output #1
+
+pangram
+Output #2
+
+not pangram
