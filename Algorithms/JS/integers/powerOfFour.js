4a260ab7365b2ef55b2b89ea17c916881cf85ffd
Arturo Perez <VrturoPerez@gmail.com>
2016-07-07 21:39:47 -0700

    add solution

--- Algorithms/JS/integers/powerOfFour.js
+++ Algorithms/JS/integers/powerOfFour.js
@@ -4,3 +4,8 @@
 // Given num = 16, return true. Given num = 5, return false.
 
 // Follow up: Could you solve it without loops/recursion?
+
+
+var isPowerOfFour = function(num) {
+    return Math.log10(num) / Math.log10(4) % 1 === 0;
+};
