9df5b3fd7f6b69a0007e8b51e049f5787cae0759
Arturo Perez <VrturoPerez@gmail.com>
2016-07-10 22:34:19 -0700

    add solution

--- Algorithms/JS/strings/isPalindrome.js
+++ Algorithms/JS/strings/isPalindrome.js
@@ -5,6 +5,30 @@
 
 // set a dictionary hash that gets checked, only if the index char of the str and the end pointer
 // match as part of the Dictionary
+
+
+function isPalindrome(str) {
+  // Step 1. Lowercase the string and use the RegExp to remove unwanted characters from it
+  var re = /[\W_]/g; // or var re = /[^A-Za-z0-9]/g;
+
+  var lowRegStr = str.toLowerCase().replace(re, '');
+  // str.toLowerCase() = "A man, a plan, a canal. Panama".toLowerCase() = "a man, a plan, a canal. panama"
+  // str.replace(/[\W_]/g, '') = "a man, a plan, a canal. panama".replace(/[\W_]/g, '') = "amanaplanacanalpanama"
+  // var lowRegStr = "amanaplanacanalpanama";
+
+  // Step 2. Use the same chaining methods with built-in functions from the previous article 'Three Ways to Reverse a String in JavaScript'
+  var reverseStr = lowRegStr.split('').reverse().join('');
+  // lowRegStr.split('') = "amanaplanacanalpanama".split('') = ["a", "m", "a", "n", "a", "p", "l", "a", "n", "a", "c", "a", "n", "a", "l", "p", "a", "n", "a", "m", "a"]
+  // ["a", "m", "a", "n", "a", "p", "l", "a", "n", "a", "c", "a", "n", "a", "l", "p", "a", "n", "a", "m", "a"].reverse() = ["a", "m", "a", "n", "a", "p", "l", "a", "n", "a", "c", "a", "n", "a", "l", "p", "a", "n", "a", "m", "a"]
+  // ["a", "m", "a", "n", "a", "p", "l", "a", "n", "a", "c", "a", "n", "a", "l", "p", "a", "n", "a", "m", "a"].join('') = "amanaplanacanalpanama"
+  // So, "amanaplanacanalpanama".split('').reverse().join('') = "amanaplanacanalpanama";
+  // And, var reverseStr = "amanaplanacanalpanama";
+
+  // Step 3. Check if reverseStr is strictly equals to lowRegStr and return a Boolean
+  return reverseStr === lowRegStr; // "amanaplanacanalpanama" === "amanaplanacanalpanama"? => true
+}
+
+
 function isPalindrome(str){
   var dict = {
     a: true,
@@ -95,3 +119,5 @@ var isPalindrome = function(str) {
        }
   return true;
 };
+
+
