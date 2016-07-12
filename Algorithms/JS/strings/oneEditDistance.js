794c4dd438adbb9e0552656c1fbc7234a76775a1
Arturo Perez <VrturoPerez@gmail.com>
2016-07-11 19:19:29 -0700

    add desc

--- Algorithms/JS/strings/oneEditDistance.js
+++ Algorithms/JS/strings/oneEditDistance.js
@@ -1,1 +1,20 @@
-oneEditDistance.js
+// Given two string s1 and s2, find if s1 can be converted to s2 with exactly one edit.
+// Expected time complexity is O(m+n) where m and n are lengths of two strings.
+
+// Examples:
+
+// Input:  s1 = "geeks", s2 = "geek"
+// Output: yes
+// Number of edits is 1
+
+// Input:  s1 = "geeks", s2 = "geeks"
+// Output: no
+// Number of edits is 0
+
+// Input:  s1 = "geaks", s2 = "geeks"
+// Output: yes
+// Number of edits is 1
+
+// Input:  s1 = "peaks", s2 = "geeks"
+// Output: no
+// Number of edits is 2
