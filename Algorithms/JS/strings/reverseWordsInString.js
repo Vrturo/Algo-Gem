0d346a7285aff8160909e20764f20f91b4093ca9
Arturo Perez <VrturoPerez@gmail.com>
2016-07-12 14:38:57 -0700

    edit solution, add new file

--- Algorithms/JS/strings/reverseWordsInString.js
+++ Algorithms/JS/strings/reverseWordsInString.js
@@ -5,13 +5,13 @@
 // return "blue is sky the".
 
 
-var reverseWords = function(str) {
-  var result = [],
-      strArr = str.split(" ");
+var reverseWords = function(str) { // 88ms runtime
+    var strArr = str.split(' '),
+        result = '';
 
-  for(var i=strArr.length; i>=0; i--){
-    result.push(strArr[i]);
-  }
-  return result.join(" ");
+    for ( i = strArr.length-1; i>=0 ; i-- ){
+        if  ( strArr[i] !== '' ) result += strArr[i] + ' '; // if word exists add the word plus space to result str
+    }
+
+    return result.trimRight(); // removes whitespace coming from the right
 };
-
