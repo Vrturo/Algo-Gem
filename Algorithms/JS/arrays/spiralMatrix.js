0366c9bfbaad56ed1e1e8b7cd638d9b94d126b0f
Arturo Perez <VrturoPerez@gmail.com>
2016-07-12 16:27:05 -0700

    add solution

--- Algorithms/JS/arrays/spiralMatrix.js
+++ Algorithms/JS/arrays/spiralMatrix.js
@@ -1,1 +1,40 @@
-spiralMatrix.js
+// Given a matrix of m x n elements (m rows, n columns), return all elements of the matrix in spiral order.
+
+// For example,
+// Given the following matrix:
+
+// [
+//  [ 1, 2, 3 ],
+//  [ 4, 5, 6 ],
+//  [ 7, 8, 9 ]
+// ]
+// You should return [1,2,3,6,9,8,7,4,5].
+
+
+var spiralOrder = function(matrix, shrink, arr) { // 132ms runtime
+  if(!arr) arr = [];
+  if(!matrix || !matrix[0]) return arr; // if matrix doesnt exist or is empty return arr
+  if(!shrink) shrink = 0;
+
+  var height = matrix.length - 2 * shrink, // -2 because of matrix arr and length
+      width = matrix[0].length - 2 * shrink;
+
+  if( height === 0 || width === 0 ) return arr;
+
+  if( width === 1) {
+    for( var i = shrink; i < shrink + height; i++ ) arr.push(matrix[i][shrink + width - 1]);
+    return arr;
+  }
+
+  if(height === 1){
+    for(var i = shrink; i < shrink + width; i++) arr.push(matrix[shrink][i]);
+    return arr;
+  }
+
+  for(var i = shrink; i < shrink + width - 1; i++) arr.push( matrix[shrink][i] );
+  for(var i = shrink; i < shrink + height - 1; i++) arr.push( matrix[i][shrink + width - 1] );
+  for(var i = shrink + width - 1; i > shrink; i--) arr.push( matrix[shrink + height - 1][i] );
+  for(var i = shrink + height - 1; i > shrink; i--) arr.push( matrix[i][shrink] );
+
+  return spiralOrder( matrix,shrink + 1,arr );
+};
