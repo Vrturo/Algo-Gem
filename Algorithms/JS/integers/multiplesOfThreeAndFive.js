9df53fc25a69d19d1585829df164b2cef5b49711
Arturo Perez <VrturoPerez@gmail.com>
2016-07-13 17:14:48 -0700

    add solution

--- Algorithms/JS/integers/multiplesOfThreeAndFive.js
+++ Algorithms/JS/integers/multiplesOfThreeAndFive.js
@@ -2,3 +2,15 @@
 // The sum of these multiples is 23.
 
 // Find the sum of all the multiples of 3 or 5 below 1000.
+
+function findMultiples(){
+
+  var sum = 0;
+  for( var i = 2; i < 1000; i++ ){
+    if( i % 3 === 0 || i % 5 === 0){
+      sum = sum += i
+    }
+  }
+  return sum;
+
+}
