46909a67a852cf392ca35a822cab2d187d7a827e
Arturo Perez <VrturoPerez@gmail.com>
2016-07-14 13:06:29 -0700

    add num palindrome

--- Algorithms/JS/integers/largestPalindromeProduct.js
+++ Algorithms/JS/integers/largestPalindromeProduct.js
@@ -2,3 +2,18 @@
 // The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 
 // Find the largest palindrome made from the product of two 3-digit numbers.
+
+
+function isPalindrome(num){
+  var numArr = num.toString().split(""),
+      len = numArr.length;
+
+  for(var i = 0; i < Math.floor(len/2); i++){
+    if ( numArr[i] !== numArr[len -1 -i] )
+       return false;
+  }
+
+  return true;
+}
+
+console.log( isPalindrome(969) )
