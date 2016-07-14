9d8d6756a1ef8a08e82daaed37b80aa63f65cf14
Arturo Perez <VrturoPerez@gmail.com>
2016-07-14 13:09:01 -0700

    add solution

--- Algorithms/JS/integers/isPalindrome.js
+++ Algorithms/JS/integers/isPalindrome.js
@@ -7,3 +7,16 @@
 // output
 
 // true
+
+
+function isPalindrome(num){
+  var numArr = num.toString().split(""),
+      len = numArr.length;
+
+  for(var i = 0; i < Math.floor(len/2); i++){
+    if ( numArr[i] !== numArr[len -1 -i] )
+       return false;
+  }
+
+  return true;
+}
