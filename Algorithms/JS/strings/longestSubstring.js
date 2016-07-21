36cb6815e9b08bdeb7f2079445e15a930a8ef2ad
Arturo Perez <VrturoPerez@gmail.com>
2016-07-20 23:34:45 -0700

    add solution

--- Algorithms/JS/strings/longestSubstring.js
+++ Algorithms/JS/strings/longestSubstring.js
@@ -8,3 +8,20 @@
 
 // Given "pwwkew", the answer is "wke", with the length of 3.
 // Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+
+var lengthOfLongestSubstring = function(s) {
+    if( s === "" ) return 0;
+    var maxLen = 0,
+        l,
+        r;
+
+    for ( l = 0, r = 1; r < s.length; r++ ) {
+        var i = s.lastIndexOf( s[r], r-1 );
+        if ( i >= 0 ) {
+            maxLen = Math.max( maxLen, r - l );
+            l = Math.max( l, i + 1 );
+        }
+    }
+    return Math.max( maxLen, r - l );
+};
