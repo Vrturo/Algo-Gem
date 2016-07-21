2afff7b1cdbf5d465602d6c001bf27ba780976f5
Arturo Perez <VrturoPerez@gmail.com>
2016-07-21 14:59:25 -0700

    add solution

--- Algorithms/JS/arrays/highestProduct.js
+++ Algorithms/JS/arrays/highestProduct.js
@@ -7,7 +7,59 @@
 // If arrayOfInts is [−10,−10,1,3,2]
 // we should return 300 (which we get by taking -10 * -10 * 3−10∗−10∗3).
 
-
 function highestProduct( arrayOfInts ){
+  if (arrayOfInts.length < 3) {
+        return arrayOfInts;
+  }
+    // We're going to start at the 3rd item (at index 2)
+    // so pre-populate highests and lowests based on the first 2 items.
+    // we could also start these as null and check below if they're set
+    // but this is arguably cleaner
+    var highest = Math.max(arrayOfInts[0], arrayOfInts[1]);
+    var lowest  = Math.min(arrayOfInts[0], arrayOfInts[1]);
 
+    var highestProductOf2 = arrayOfInts[0] * arrayOfInts[1];
+    var lowestProductOf2  = arrayOfInts[0] * arrayOfInts[1];
+
+    // except this one--we pre-populate it for the first /3/ items.
+    // this means in our first pass it'll check against itself, which is fine.
+    var highestProductOf3 = arrayOfInts[0] * arrayOfInts[1] * arrayOfInts[2];
+
+    // walk through items, starting at index 2
+    for (var i = 2; i < arrayOfInts.length; i++) {
+        var current = arrayOfInts[i];
+
+        // do we have a new highest product of 3?
+        // it's either the current highest,
+        // or the current times the highest product of two
+        // or the current times the lowest product of two
+        highestProductOf3 = Math.max(
+            highestProductOf3,
+            current * highestProductOf2,
+            current * lowestProductOf2
+        );
+
+        // do we have a new highest product of two?
+        highestProductOf2 = Math.max(
+            highestProductOf2,
+            current * highest,
+            current * lowest
+        );
+
+        // do we have a new lowest product of two?
+        lowestProductOf2 = Math.min(
+            lowestProductOf2,
+            current * highest,
+            current * lowest
+        );
+
+        // do we have a new highest?
+        highest = Math.max(highest, current);
+
+        // do we have a new lowest?
+        lowest = Math.min(lowest, current);
+    }
+
+    return highestProductOf3;
 }
+
