5fcc1586a264935ea2c9b68ff68389edaef23ff4
Arturo Perez <VrturoPerez@gmail.com>
2016-07-21 14:17:54 -0700

    add desc

--- Algorithms/JS/arrays/highestProduct.js
+++ Algorithms/JS/arrays/highestProduct.js
@@ -1,1 +1,13 @@
-highestProduct.js
+// Given an arrayOfInts, find the highestProduct you can get from three of the integers.
+
+// The input arrayOfInts will always have at least three integers.
+
+// Does your function work with negative numbers?
+
+// If arrayOfInts is [-10, -10, 1, 3, 2][−10,−10,1,3,2]
+// we should return 300 (which we get by taking -10 * -10 * 3−10∗−10∗3).
+
+
+function highestProduct( arrayOfInts ){
+
+}
