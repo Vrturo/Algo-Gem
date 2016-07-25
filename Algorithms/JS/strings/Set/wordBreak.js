7e5f7c68098adf2681c31a6dc8093a4755f1234b
Arturo Perez <VrturoPerez@gmail.com>
2016-07-25 13:37:10 -0700

    add notes

--- Algorithms/JS/strings/Set/wordBreak.js
+++ Algorithms/JS/strings/Set/wordBreak.js
@@ -14,3 +14,18 @@
  *   https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
  * @return {boolean}
  */
+
+ var wordBreak = function(s, wordDict) { // 136ms runtime
+    var result = [];
+
+    for ( var i = 0; i < s.length; i++ ){
+        for ( var j = 0; j <= i; j ++ ) {
+            var word = s.slice( j, i + 1 );
+            if( wordDict.has(word)&&( j === 0 || result[j-1] === true ) ) {
+                result[i] = true; // [, , , , true]
+            }
+        }// end j
+    }// end i
+
+    return result[s.length - 1] || false;
+};
