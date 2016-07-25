5e068bf474d2d577effc53c8130622f6bc0ba11b
Arturo Perez <VrturoPerez@gmail.com>
2016-07-24 17:33:41 -0700

    add desc

--- Algorithms/JS/arrays/matrices/diagonalLine.js
+++ Algorithms/JS/arrays/matrices/diagonalLine.js
@@ -1,1 +1,9 @@
-diagonalLine.js
+// return a matrix of size n * n with 1's covering the main diagonal and 0's covering the rest
+
+function diag( int ){
+  var result = Array.new(2);
+
+  return result;
+}
+
+console.log( diag( 2 ) )
