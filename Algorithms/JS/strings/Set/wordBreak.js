183b103092dbc72c2b845f24f90df7de50c5eeae
Arturo Perez <VrturoPerez@gmail.com>
2016-07-25 13:29:36 -0700

    add desc

--- Algorithms/JS/strings/Set/wordBreak.js
+++ Algorithms/JS/strings/Set/wordBreak.js
@@ -1,1 +1,16 @@
-wordBreak.js
+// Given a string s and a dictionary of words dict,
+// determine if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+// For example, given
+// s = "leetcode",
+// dict = ["leet", "code"].
+
+// Return true because "leetcode" can be segmented as "leet code".
+
+/**
+ * @param {string} s
+ * @param {set<string>} wordDict
+ *   Note: wordDict is a Set object, see:
+ *   https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
+ * @return {boolean}
+ */
