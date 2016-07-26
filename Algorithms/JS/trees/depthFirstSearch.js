b1020d6c7883b0b2538ce6b9f48239a242b3d192
Arturo Perez <VrturoPerez@gmail.com>
2016-07-26 13:27:29 -0700

    add solution

--- Algorithms/JS/trees/depthFirstSearch.js
+++ Algorithms/JS/trees/depthFirstSearch.js
@@ -1,1 +1,11 @@
-depthFirstSearch.js
+// How do you implement Breadth First Search
+
+
+
+function dfs(node){
+  if(node){
+    console.log(node.value);
+    dfs(node.left);
+    dfs(node.right);
+  }
+}
