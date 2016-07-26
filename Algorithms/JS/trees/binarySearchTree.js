34a05a4308dc48fa81b4ffde1b1aa71ce467b48d
Arturo Perez <VrturoPerez@gmail.com>
2016-07-26 13:22:35 -0700

    add node class

--- Algorithms/JS/trees/binarySearchTree.js
+++ Algorithms/JS/trees/binarySearchTree.js
@@ -1,1 +1,9 @@
 // How would you create a binary search tree?
+
+
+function Node( val ){ // to create a tree you need a node. a node in a tree looks like
+  this.value = val;
+  this.left = null;
+  this.right = null;
+}
+
