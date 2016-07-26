0ecc18254b4b199b4871ef9a139e4d7ae3abf2d4
Arturo Perez <VrturoPerez@gmail.com>
2016-07-26 13:27:01 -0700

    add solutiion; create new file

--- Algorithms/JS/trees/binarySearchTree.js
+++ Algorithms/JS/trees/binarySearchTree.js
@@ -7,3 +7,41 @@ function Node( val ){ // to create a tree you need a node. a node in a tree look
   this.right = null;
 }
 
+function BinarySearchTree(){
+  this.root = null;
+}
+
+
+BinarySearchTree.prototype.push = function( val ){
+   var root = this.root;
+
+   if( !root ){ // if root doesnt exist
+      this.root = new Node( val ); // create a new node from the val
+      return;
+   }
+
+   var currentNode = root;
+   var newNode = new Node( val );
+
+   while( currentNode ){ // traverse through tree for empty branch
+      if( val < currentNode.value ){
+          if( !currentNode.left ){
+             currentNode.left = newNode;
+             break;
+          }
+          else{
+             currentNode = currentNode.left;
+        }
+     }
+     else{
+         if( !currentNode.right ){
+            currentNode.right = newNode;
+            break;
+         }
+         else{
+            currentNode = currentNode.right;
+         }
+     }
+  }
+
+}
