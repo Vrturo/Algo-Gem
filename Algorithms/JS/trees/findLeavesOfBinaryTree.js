f5b6931681f84c98c7e425089735adf2473b0acc
Arturo Perez <VrturoPerez@gmail.com>
2016-07-25 21:41:14 -0700

    add unfinished solution

--- Algorithms/JS/trees/findLeavesOfBinaryTree.js
+++ Algorithms/JS/trees/findLeavesOfBinaryTree.js
@@ -37,4 +37,25 @@
  * @return {number[][]}
  */
 
+var findLeaves = function(root) {
+    var collection = [];
 
+    function helper( node, arr ){
+        console.log(node);
+        if( node === null ) return node;
+        else if( !node.left && !node.right ){
+            arr.push( node.val );
+        }
+        else if( node.left && !node.right) helper( node.left, arr );
+        else if( !node.left && node.right) helper( node.right, arr );
+        else{
+            helper(node.left, arr),
+            helper(node.right, arr);
+            node.left = null;
+            node.right = null;
+        }
+        return arr;
+    }
+    collection.push( helper(root, []) );
+    return collection;
+};
