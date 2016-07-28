d1e5dadac143c16cd90666782c6234c6baead328
Arturo Perez <VrturoPerez@gmail.com>
2016-07-28 15:43:45 -0700

    add to desc

--- Algorithms/JS/integers/isPalindrome.js
+++ Algorithms/JS/integers/isPalindrome.js
@@ -1,12 +1,19 @@
 // Given an input integer, determine whether any anagram of the integer is a palindrome
 
-// input
+// Some hints:
+// Could negative integers be palindromes? (ie, -1)
 
+// If you are thinking of converting the integer to string, note the restriction of using extra space.
+
+// You could also try reversing an integer. However, if you have solved the problem "Reverse Integer",
+// you know that the reversed integer might overflow. How would you handle such case?
+
+// input
 // 99
 
 // output
-
 // true
+
 
 
 function isPalindrome(num){
