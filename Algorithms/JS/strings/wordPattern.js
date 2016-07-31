24822426d9bad2ae2120e05c6e946cc75acb0a23
Arturo Perez <VrturoPerez@gmail.com>
2016-07-31 14:06:29 -0700

    add solution with test

--- Algorithms/JS/strings/wordPattern.js
+++ Algorithms/JS/strings/wordPattern.js
@@ -4,3 +4,30 @@
 // e.g 1. ("abc" , <"cdf", "too", "hgfdt" ,"paa">) -> output = "cdf"
 //       2. ("acc" , <"cdf", "too", "hgfdt" ,"paa">) -> output = "too", "paa"
 
+
+var wordPattern = function(pattern, str) {
+  var i,
+      j;
+
+    function decode( string ){
+      var storage = {},
+          arrayValues = [];
+
+      for( i = 0; i < string.length; i++ ){ // set counter. {"a": 1, "b": 1, "c": 1}
+        storage[string[i]] ? storage[string[i]] += 1 : storage[string[i]] = 1
+      }
+      for( key in storage ){ // push values into array. [1,1,1]
+        arrayValues.push( storage[key] );
+      }
+      return arrayValues; // [1,1,1]
+    }
+
+    var patternArr = decode( pattern ).toString(); // turn to string to compare array to array faster
+
+    for( j = 0; j < str.length; j++ ){
+        if( decode(str[j]).toString() === patternArr ) console.log( str[j] );
+    }
+}
+
+var test = ["cdf", "too", "hgfdt" ,"paa"]
+console.log( wordPattern("acc", test) )
