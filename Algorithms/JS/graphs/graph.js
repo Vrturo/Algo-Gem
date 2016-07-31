0df46ab10cfa995826db8488cf4284af1031d057
Arturo Perez <VrturoPerez@gmail.com>
2016-07-30 20:23:45 -0700

    add new file

--- Algorithms/JS/graphs/graph.js
+++ Algorithms/JS/graphs/graph.js
@@ -1,3 +1,12 @@
 // add functions to a Graph class to return various representations of the same graph.
 // Your graph will have two different components: Nodes and Edges.
 
+function Graph(){
+    this.vertices = [];
+    this.adjList = new Dictionary();
+}
+
+Graph.prototype.addVertex = function (vertex) {
+    this.vertices.push( vertex );
+    this.adjList.set( vertex, [] );
+}
