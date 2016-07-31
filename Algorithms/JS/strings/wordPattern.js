0fb985ed66577e84fb71e9330ac1ce7a017226f3
Arturo Perez <VrturoPerez@gmail.com>
2016-07-31 13:18:58 -0700

    add desc

--- Algorithms/JS/strings/wordPattern.js
+++ Algorithms/JS/strings/wordPattern.js
@@ -1,1 +1,6 @@
-wordPattern.js
+// Given a Pattern and a dictionary, print out all the strings that match the pattern.
+// where a character in the pattern is mapped uniquely to a character in the dictionary.
+
+// e.g 1. ("abc" , <"cdf", "too", "hgfdt" ,"paa">) -> output = "cdf"
+//       2. ("acc" , <"cdf", "too", "hgfdt" ,"paa">) -> output = "too", "paa"
+
