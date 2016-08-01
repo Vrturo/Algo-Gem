0e4e12a6ea93daa085fc7eeae206922b5ec54167
Arturo Perez <VrturoPerez@gmail.com>
2016-08-01 13:43:00 -0700

    add new solution

--- Algorithms/JS/trees/postOrderTraversal.js
+++ Algorithms/JS/trees/postOrderTraversal.js
@@ -10,5 +10,13 @@
 // Postorder traversal:
 // 3 22 16 37 99 45 23
 
+// A postorder traversal visits all of the child nodes of the left subtree up to the root node,
+// and then visits all of the child nodes of the right subtree up to the root node.
 
-
+function postOrder( node ){
+   if( node ){
+      postorder( node.left );
+      postorder( node.right );
+      console.log( node.value ); // print out current node, then traverse
+   }
+}
