5030cff07d49c4d12e1adb44744da05e0a6ee24e
Arturo Perez <VrturoPerez@gmail.com>
2016-08-01 13:40:55 -0700

    add new desc

--- Algorithms/JS/trees/postOrderTraversal.js
+++ Algorithms/JS/trees/postOrderTraversal.js
@@ -1,1 +1,14 @@
-postOrderTraversal.js
+// How to perform postOrder traversal
+
+
+//     23
+//    /  \
+//   16   45
+//  / \   /  \
+// 3 22  37  99
+
+// Postorder traversal:
+// 3 22 16 37 99 45 23
+
+
+
