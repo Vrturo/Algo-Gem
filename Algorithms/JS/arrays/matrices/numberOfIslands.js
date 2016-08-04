be8c2d833fbcba950b265dc67316c14e12e9461d
Arturo Perez <VrturoPerez@gmail.com>
2016-08-04 14:52:36 -0700

    add solution

--- Algorithms/JS/arrays/matrices/numberOfIslands.js
+++ Algorithms/JS/arrays/matrices/numberOfIslands.js
@@ -17,3 +17,32 @@
 // 00100
 // 00011
 // Answer: 3
+
+
+var numIslands = function (grid) {
+    var islands = 0,
+        row,
+        column;
+
+    // find land pieces
+    for ( row = 0; row < grid.length; row++ ) {
+        for ( column = 0; column < grid[row].length; column++ ) {
+            if ('1' === grid[row][column] ) {
+                walk( grid, row, column );
+                islands++;
+            }
+        }
+    }
+    return islands;
+};
+
+// do dfs walf and mark visited land pieces
+function walk( grid, row, col ) {
+    if ('1' === grid[row][col]) {
+        grid[row][col] = 'visited'; // mark land piece as visited
+        /* left*/       if( col > 0 ) walk( grid, row, col - 1 );
+        /* right */     if( col < grid[row].length - 1 ) walk( grid, row, col + 1 );
+        /* top */       if( row > 0) walk(grid, row - 1, col );
+        /* bottom */    if( row < grid.length - 1 ) walk( grid, row + 1, col );
+    }
+}
