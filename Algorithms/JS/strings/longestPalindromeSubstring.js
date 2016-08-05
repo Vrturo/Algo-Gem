ea8b670183e673c4e184db7f8fc1a899c318de13
Arturo Perez <VrturoPerez@gmail.com>
2016-08-05 12:21:51 -0700

    add solution

--- Algorithms/JS/strings/longestPalindromeSubstring.js
+++ Algorithms/JS/strings/longestPalindromeSubstring.js
@@ -1,7 +1,37 @@
 // Given a string S, find the longest palindromic substring in S.
 // You may assume that the maximum length of S is 1000, and there exists one unique longest palindromic substring.
 
-var longestPalindrome = function(s) {
+
+
+var longestPalindrome = function(s) { // 168ms runtime
+  var len = s.length,
+      result = "";
+  if( len < 2 ) return s;
+
+  var centeredPalindrome = function(left, right) {
+    while (left >= 0 && right < len && s[left] === s[right]) {
+      //expand in each direction.
+      left--;
+      right++;
+    }
+
+    return s.slice(left + 1, right);
+  };
+
+  for (var i = 0; i < len - 1; i++) { // loop through string
+    var oddPal = centeredPalindrome(i, i + 1), // allow one char to be odd
+        evenPal = centeredPalindrome(i, i); // reg palindrome check
+
+    if (oddPal.length > result.length) result = oddPal;
+    if (evenPal.length > result.length) result = evenPal;
+  }
+  return result;
+};
+
+// ------------------------------------------------------
+
+
+var longestPalindrome = function(s) { // 488ms runtime
   var max = '',
       i, j;
 
