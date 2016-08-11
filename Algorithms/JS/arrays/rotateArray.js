b7d12d92706fabd41219c26693a6d3533ad7b765
Arturo Perez <VrturoPerez@gmail.com>
2016-08-10 22:26:07 -0700

    add desc

--- Algorithms/JS/arrays/rotateArray.js
+++ Algorithms/JS/arrays/rotateArray.js
@@ -1,1 +1,9 @@
-rotateArray.js
+// Rotate an array of n elements to the right by k steps.
+
+// For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to [5,6,7,1,2,3,4].
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
