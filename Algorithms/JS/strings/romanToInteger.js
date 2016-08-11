d4b73a1172de6ed67983b5d0061a414e75f6e467
Arturo Perez <VrturoPerez@gmail.com>
2016-08-10 21:02:48 -0700

    add solution

--- Algorithms/JS/strings/romanToInteger.js
+++ Algorithms/JS/strings/romanToInteger.js
@@ -1,3 +1,31 @@
 // Given a roman numeral, convert it to an integer.
 
 // Input is guaranteed to be within the range from 1 to 3999.
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+var romanToInt = function(s) {
+    var map = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000 },
+        prev = 0,
+        result = 0;
+
+    for (var i = s.length - 1; i >= 0; i--) {
+        if ( map[s[i]] < prev ) {
+            result -= map[s[i]];
+        } else {
+            result += map[s[i]];
+        }
+        prev = map[s[i]];
+    }
+    return result;
+};
