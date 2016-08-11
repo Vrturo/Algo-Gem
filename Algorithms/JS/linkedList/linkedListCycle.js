a400b7d7d511d80e47d96f9ffad91dc2f395194f
Arturo Perez <VrturoPerez@gmail.com>
2016-08-10 21:21:03 -0700

    ad solution

--- Algorithms/JS/linkedList/linkedListCycle.js
+++ Algorithms/JS/linkedList/linkedListCycle.js
@@ -21,3 +21,21 @@
  */
 
 
+var hasCycle = function(head) {
+    if( head === null) return false; // list does not exist..so no loop either.
+
+    var slow = fast = head; // create two references to the start of the list.
+
+     while(true) {
+        slow = slow.next;          // 1 hop.
+        if( fast.next !== null )
+            fast = fast.next.next; // 2 hops.
+        else
+            return false;          // next node null => no loop.
+
+        if( slow === null || fast === null ) // if either hits null..no loop.
+            return false;
+        if( slow === fast ) // if the two ever meet...we must have a loop.
+            return true;
+    }
+};
