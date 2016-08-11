bd8aef5a811c42681801dcce20acc883bbcc5571
Arturo Perez <VrturoPerez@gmail.com>
2016-08-10 21:20:47 -0700

    add desc

--- Algorithms/JS/linkedList/linkedListCycle.js
+++ Algorithms/JS/linkedList/linkedListCycle.js
@@ -1,1 +1,23 @@
-linkedListCycle.js
+// Given a linked list, determine if it has a cycle in it.
+
+// which would return true if the given Node is the first of a list with a loop, and false otherwise?
+// How could you write so that it takes a constant amount of space and a reasonable amount of time?
+
+// Here's a picture of what a list with a loop looks like:
+
+// 1 -> 2 -> 3-> 4 -> 5 -> 6 -> 3
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+
+
