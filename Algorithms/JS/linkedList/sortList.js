2cc20ab4c5b86dc8077463c92840b7b9b26509b2
Arturo Perez <VrturoPerez@gmail.com>
2016-08-10 21:40:33 -0700

    add desc

---
+++ Algorithms/JS/linkedList/sortList.js
@@ -0,0 +1,15 @@
+// Sort a linked list in O(n log n) time using constant space complexity.
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+
