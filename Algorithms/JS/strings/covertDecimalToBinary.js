8d8edb5ea47109a8b8c0bd51f52d4fa04ee17188
Arturo Perez <VrturoPerez@gmail.com>
2016-08-18 00:04:29 -0700

    add new desc

--- Algorithms/JS/strings/covertDecimalToBinary.js
+++ Algorithms/JS/strings/covertDecimalToBinary.js
@@ -1,1 +1,20 @@
-covertDecimalToBinary.js
+// Convert binary (101) to it's decimal equivelant( 5)
+
+// 0   00
+// 1   01
+// 2   10
+// 3   11
+// 4   100
+// 5   101
+// 6   110
+// 7   111
+// 8   1000
+// 9   1001
+// 10  1010
+
+// Input: String
+// ex. '101'
+
+// Output: Integer
+// 5
+
