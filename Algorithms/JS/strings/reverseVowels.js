3124ff3747140dad56f1fb22ad43938ac9995cf7
Arturo Perez <VrturoPerez@gmail.com>
2016-08-17 23:23:40 -0700

    edit solution

--- Algorithms/JS/strings/reverseVowels.js
+++ Algorithms/JS/strings/reverseVowels.js
@@ -5,6 +5,44 @@
 //ex. Input: "united states"
 //Output: "enated stitus"
 
+
+
+var reverseVowels = function(s) {
+    var dict = {
+        a: true,
+        e: true,
+        i: true,
+        o: true,
+        u: true
+    },
+    arr = s.split(''),
+    l = 0,
+    r = arr.length - 1;
+
+    while (l < r) {
+        // find index of vowel
+        while (!dict[arr[l].toLowerCase()] && l < r) {
+            l++;
+        }
+        while (!dict[arr[r].toLowerCase()] && l < r) {
+            r--;
+        }
+
+        // swap
+        var tmp = arr[l];
+        arr[l] = arr[r];
+        arr[r] = tmp;
+
+        // continue
+        l++;
+        r--;
+    }
+    return arr.join('');
+}
+
+
+// --------------------------------------------------
+
 var reverseVowels = function(s) {
     var input = s.split(''),
         i = 0,
