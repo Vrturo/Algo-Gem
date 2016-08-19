a034fa199abcfafb335565627302782a000af6fe
Arturo Perez <VrturoPerez@gmail.com>
2016-08-18 18:28:55 -0700

    add solution

--- Algorithms/JS/arrays/houseRobber.js
+++ Algorithms/JS/arrays/houseRobber.js
@@ -28,3 +28,9 @@ var rob = function(nums) {
     }
     return nums[nums.length - 1];
 };
+
+var rob = function(nums) {
+    return nums.reduce(function(p, n) {
+        return [p[1], Math.max(p[0] + n, p[1])];
+    }, [0,0])[1];
+};
