890077e086ac7ff96470208abb26b77958eec963
Arturo Perez <VrturoPerez@gmail.com>
2016-08-18 18:28:13 -0700

    ad solution

--- Algorithms/JS/arrays/houseRobber.js
+++ Algorithms/JS/arrays/houseRobber.js
@@ -16,3 +16,15 @@
 //     12
 //     0
 //     1
+
+
+var rob = function(nums) {
+    if( nums.length < 1 ) return 0;
+    if( nums.length === 1 ) return nums[0];
+    nums[1] = Math.max( nums[0], nums[1] );
+
+    for( var i = 2; i < nums.length; i++ ){
+        nums[i] = Math.max( nums[i-2] + nums[i], nums[i - 1] );
+    }
+    return nums[nums.length - 1];
+};
