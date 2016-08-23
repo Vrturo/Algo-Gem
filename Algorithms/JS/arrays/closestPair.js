192b071a6a1f7704c7d6b22cd57cecded801c71e
Arturo Perez <VrturoPerez@gmail.com>
2016-08-23 15:49:41 -0700

    add desc

--- Algorithms/JS/arrays/closestPair.js
+++ Algorithms/JS/arrays/closestPair.js
@@ -1,1 +1,9 @@
-closestPair.js
+// Provide a function to find the closest two points among a set of given points in two dimensions
+
+// We are given an array of n points in the plane, and the problem is to find out the closest pair of points in the array.
+// This problem arises in a number of applications.
+// For example, in air-traffic control, you may want to monitor planes that come too close together,
+// since this may indicate a possible collision. Recall the following formula for distance between two points p and q.
+
+// Input: An array of n points P[]
+// Output: The smallest distance between two points in the given array.
