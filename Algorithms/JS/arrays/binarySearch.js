bd47bacfa643fb9d17ab2a0350ae58bd0e087919
Arturo Perez <VrturoPerez@gmail.com>
2016-08-23 13:33:13 -0700

    add solution

--- Algorithms/JS/arrays/binarySearch.js
+++ Algorithms/JS/arrays/binarySearch.js
@@ -1,54 +1,52 @@
 // implement a recursive way tp check if an item is in the array
 
+function binarySearch( item, arr ){
+    var l = 0,
+        r = arr.length -1
+    if( r>=l ){ // check if there are elements in the array
+            var mid = (r - l)/2;
 
-// function binarySearch(item, arr){
+            // If the element is present at the middle itself
+            if( arr[l] === item || arr[r] === item || arr[mid] === item ) return true;
 
-//     function helper(start, end){
-//       var mid = Math.floor((start + end)/2);
-//       if (arr[mid] === item){
-//         return true
-//       }
-//       if(start >= end - 1){
-//         if (arr[start] === item || arr[end] === item){
-//           return true;
-//         }
-//         else{
-//           return false;
-//         };
-//       }
-//       if (item > arr[mid]){
-//         return helper(mid, end);
-//       }
-//       else if(item < arr[mid]){
-//         return helper(start, mid);
-//       }
-//     }
-//     return helper(0, arr.length -1);
-// }
+            // If element is smaller than mid, then it can only
+            // be present in left subarray
+            if( arr[mid] > item )
+               return binarySearch( item, arr.splice( 0, mid ));
 
+            // Else the element can only be present in right
+            // subarray
+            return binarySearch( item, arr.splice(mid, r));
+    }
+
+    // We reach here when element is not present in array
+    return false;
+}
+
+// ----------------------------------------------------
 
 function binarySearch(item, arr){
       var start = 0,
           end = arr.length - 1,
           mid = Math.floor((start + end)/2);
 
-      if (arr[start] === item || arr[end] === item || arr[mid] === item){
+      if( arr[start] === item || arr[end] === item || arr[mid] === item ){
         return true;
       }
-      if(start >= end - 1){
-        if(arr[end] === item || arr[start] === item){
+      if( start >= end ){
+        if( arr[end] === item || arr[start] === item ){
           return true;
         }
         else {
           return false;
         };
       }
-      if (item > arr[mid]){
+      if( item > arr[mid] ){
         return binarySearch(item, arr.splice(0, mid));
       }
-      else if(item < arr[mid]){
+      else if( item < arr[mid] ){
         return binarySearch(item, arr.splice(mid, end));
       }
 }
 
-console.log(binarySearch(5, [1, 2, 3, 5, 7, 8]));
+console.log(binarySearch(4, [1, 2, 3, 5, 7, 8]));
