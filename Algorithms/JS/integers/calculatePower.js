e57d32c505f9fa9e148f84cf1d32f3cbef1ecd2d
Arturo Perez <VrturoPerez@gmail.com>
2016-08-24 16:13:36 -0700

    add solution + notes

--- Algorithms/JS/integers/calculatePower.js
+++ Algorithms/JS/integers/calculatePower.js
@@ -1,5 +1,24 @@
 // Write a program to calculate pow(x,n)
 
+function myPow(x, n){ // iterative or logn depending if exponent is even or odd or negative
+    if( n === 0 ) return 1;
+    var temp;
+    if( n%2 === 0 ){ // if even
+      temp = myPow( x, n/2 );
+      return temp*temp;
+    } else {
+        if( n < 0 ){ // if negative
+          return 1 / myPow( x, -n );
+        } else { // if odd
+          temp = 1;
+          while( n-- ) {
+              temp *= x;
+          }
+          return temp;
+      }
+    }
+}
+
 
 function myPow(x, n) { // iterative or logn depending if exponent is even or odd
     if( n === 0 ) return 1;
