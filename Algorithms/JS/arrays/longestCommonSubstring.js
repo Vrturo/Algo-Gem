4846f7edd9d3a42aa958e74ecb356923eefe37c8
Arturo Perez <VrturoPerez@gmail.com>
2016-08-24 14:17:39 -0700

    add unfinished d && c algorithm

--- Algorithms/JS/arrays/longestCommonSubstring.js
+++ Algorithms/JS/arrays/longestCommonSubstring.js
@@ -27,6 +27,41 @@ var longestCommonPrefix = function(strs) { //120 ms runtime
 };
 
 
+// ----------Unfinished Solution-----------------------------
+
+// var longestCommonPrefix = function(strs, low, mid) {
+//     // A Divide and Conquer based function to find the
+//     // longest common prefix. This is similar to the
+//     // merge sort technique
+//     var result = "";
+//     if( strs.length < 1 ) return "";
+
+//         if( low === undefined ) low = 0;
+//         var high = strs.length -1;
+
+//     if( low === high ) return strs[low];
+
+//     if( high > low ){
+//         // Same as (low + high)/2, but avoids overflow for
+//         // large low and high
+//         mid = low + (high - low) / 2;
+
+//         var str1 = longestCommonPrefix( strs, low, mid ),
+//             str2 = longestCommonPrefix( strs, mid+1, high );
+
+//         return commonPrefixUtil( str1, str2, result );
+//     }
+// };
+
+// function commonPrefixUtil( str1, str2, result ){
+//         var len;
+//         str1.length > str2.length ? len = str1.length : len = str2.length;
+
+//         while( (i< len) && str1.charAt(i) === str2.charAt(i) ) i++;
+//         if( str1.substring(0, i).length > result ) result = str1.substring(0, i);
+//         return result;
+// }
+
 // ------ Unfinished Solution ---------------------------------
 // var longestCommonPrefix = function(strs) {
 //     if( strs.length === 0 ) return "";
