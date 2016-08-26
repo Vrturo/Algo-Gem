fb3baff02d2339994ef6b836a89af1b986469bb0
Arturo Perez <VrturoPerez@gmail.com>
2016-08-25 20:29:32 -0700

    add desc

--- Algorithms/JS/arrays/matrices/ratMaze.js
+++ Algorithms/JS/arrays/matrices/ratMaze.js
@@ -1,1 +1,9 @@
-ratMaze.js
+// Given a maze, NxN matrix. A rat has to find a path from source to des­ti­na­tion.
+// maze[0][0] (left top corner)is the source and maze[N-1][N-1](right bot­tom cor­ner) is des­ti­na­tion.
+// There are few cells which are blocked, means rat can­not enter into those cells.
+// Rat can move in any direc­tion ( left, right, up and down).
+
+// Input: A 2D-matrix with 0’s and 1’s fill in it.
+// 0 means that cell is blocked and 1 means rat can move to that cell.
+
+// Output: Print solution matrix
