05312368c68519748effab58af81a472209ee1ab
Arturo Perez <VrturoPerez@gmail.com>
2016-08-26 12:28:41 -0700

    add desc

--- Algorithms/JS/arrays/matrices/nQueenProblem.js
+++ Algorithms/JS/arrays/matrices/nQueenProblem.js
@@ -1,1 +1,7 @@
-nQueenProblem.js
+// Objec­tive : In chess, a queen can move as far as she pleases, hor­i­zon­tally, ver­ti­cally, or diag­o­nally.
+// A chess board has 8 rows and 8 columns.
+// The stan­dard 8 by 8 Queen’s prob­lem asks how to place 8 queens on an ordi­nary chess board
+// so that none of them can hit any other in one move.
+
+// Here we are solv­ing it for N queens in NxN chess board.
+
