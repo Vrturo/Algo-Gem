a594898ee691ee3c5a4a1cfad78308f9cbb11e70
Arturo Perez <VrturoPerez@gmail.com>
2016-08-26 13:11:44 -0700

    edit solution:

--- Algorithms/JS/arrays/matrices/nQueenProblem.js
+++ Algorithms/JS/arrays/matrices/nQueenProblem.js
@@ -9,7 +9,10 @@
 var iterations = 0
 
 var print_board = function (columns) {
-  var n = columns.length, row = 0, col = 0
+  var n = columns.length,
+      row = 0,
+      col = 0;
+
   while (row < n) {
     while (col < n) {
       process.stdout.write(columns[row] === col ? 'Q ' : '# ')
@@ -23,29 +26,30 @@ var print_board = function (columns) {
 }
 
 var has_conflict = function (columns) {
-  var len = columns.length, last = columns[len - 1], previous = len - 2
+  var len = columns.length,
+      last = columns[len - 1],
+      previous = len - 2;
 
   while (previous >= 0) {
-    if (columns[previous] === last) return true
-    if (last - (len - 1) === columns[previous] - previous) return true
-    if (last + (len - 1) === columns[previous] + previous) return true
-    previous--
+    if (columns[previous] === last) return true;
+    if (last - (len - 1) === columns[previous] - previous) return true;
+    if (last + (len - 1) === columns[previous] + previous) return true;
+    previous--;
   }
 
   return false
 }
 
 var place_next_queen = function (total, queens, columns) {
-  if (queens === 0) return columns
-  columns = columns || []
+  if (queens === 0) return columns;
+  columns = columns || [];
 
   for (var column = 0; column < total; column++) {
-    columns.push(column)
-    iterations++
-    if ( !has_conflict(columns) && place_next_queen(total, queens - 1, columns) ) {
-      return columns
-    }
-    columns.pop(column)
+    columns.push(column);
+    iterations++;
+    if ( !has_conflict(columns) && place_next_queen(total, queens - 1, columns) ) return columns;
+
+    columns.pop(column);
   }
 
   return null
