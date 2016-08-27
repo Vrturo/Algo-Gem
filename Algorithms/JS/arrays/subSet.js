cde68500748d1a3dc5c41f76a484cddda6638a59
Arturo Perez <VrturoPerez@gmail.com>
2016-08-27 10:52:10 -0700

    add solution

--- Algorithms/JS/arrays/subSet.js
+++ Algorithms/JS/arrays/subSet.js
@@ -19,19 +19,55 @@
 
 
 var subsets = function(nums) {
-    var result = [];
-    function helper(set, i){
-        if (nums.length === i){
-            result.push(set);
-            return;
-        }
-        helper(set, Number(i+1));
-        helper(set + nums[i], Number(i+1));
+  var solution = [],
+      result = [],
+      used = [];  // nums, each element can only be used once
+
+  var backTracking = function(k, n) {
+    if( k===n ){
+      return result.push( solution.slice(0) );
+    } else {
+      for( var i=0; i<nums.length; i++ ){
+        if( used[i] ) continue;  // when true, express the element(used[i]) has been used
+        if( k>0 && solution[k-1] > nums[i]) continue; // elements have to be in ascending order
+        used[i] = true;
+        solution[k] = nums[i];
+        backTracking(k+1, n);
+        used[i] = false;
+      }
     }
-    helper([],0);
-    return result;
+  }
+
+  for( var i=0; i<=nums.length; i++ ){
+    backTracking(0, i);
+    // eg. subsets([1, 2, 3]);
+    // backTracking(0, 0); return [];
+    // backTracking(0, 1); return [1], [2], [3];
+    // backTracking(0, 2); return [1, 2], [1, 3], [2, 3];
+    // backTracking(0, 3); return [1, 2, 3];
+  }
+
+  return result;
 };
 
+console.log(subsets([1,2,3]))
+
+// --------------------------------
+// var subsets = function(nums) {
+//     var result = [];
+//     function helper(set, i){
+//         if (nums.length === i){
+//             result.push(set);
+//             return;
+//         }
+//         helper(set, Number(i+1));
+//         helper(set + nums[i], Number(i+1));
+//     }
+//     helper([],0);
+//     return result;
+// };
+
+// ---------------------------------------------------
 
 // Q: Given an array of characters, print all possible combinations. For example given a, b, c, print
 // a
