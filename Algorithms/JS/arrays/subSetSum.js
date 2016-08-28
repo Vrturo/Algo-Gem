b35a7a563b64c588a482556fda41f69ba1b74757
Arturo Perez <VrturoPerez@gmail.com>
2016-08-28 10:56:19 -0700

    remove solution

--- Algorithms/JS/arrays/subSetSum.js
+++ Algorithms/JS/arrays/subSetSum.js
@@ -3,3 +3,26 @@
 
 // Examples: set = [3, 34, 4, 12, 5, 2], sum = 9
 // Output:  True  //There is a subset (4, 5) with sum 9.
+
+
+
+function subSetSum( nums, target ){
+  var list = [];
+  backtrack( list, [], nums, 0);
+  return list;
+}
+
+function backtrack( list, tempList, nums, start ){
+    list.push( tempList );
+    for( var i = start; i < nums.length; i++ ){
+      tempList.push( nums[i] );
+      backtrack( list, tempList, nums, i+1 );
+      tempList.pop();
+    }
+}
+
+
+
+var set = [ [1, 2] ];
+console.log(subSetSum( set ))
+
