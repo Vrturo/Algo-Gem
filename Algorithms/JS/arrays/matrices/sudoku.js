d8e916231a42deb19b5486ca9b3c104789bfa9fe
Arturo Perez <VrturoPerez@gmail.com>
2016-08-31 15:14:57 -0700

    add desc

--- Algorithms/JS/arrays/matrices/sudoku.js
+++ Algorithms/JS/arrays/matrices/sudoku.js
@@ -1,1 +1,10 @@
-sudoku.js
+// The classic Sudoku game involves a grid of 81 squares.
+// The grid is divided into nine blocks, each containing nine squares.
+
+// The rules of the game are simple:
+// each of the nine blocks has to contain all the numbers 1-9 within its squares.
+// Each number can only appear once in a row, column or box.
+
+// The difficulty lies in that each vertical nine-square column,
+// or horizontal nine-square line across, within the larger square,
+// must also contain the numbers 1-9, without repetition or omission.
