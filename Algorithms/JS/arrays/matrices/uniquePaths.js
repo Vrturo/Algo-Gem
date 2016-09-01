30614524f43a992f404e2714830ae32f97048ebf
Arturo Perez <VrturoPerez@gmail.com>
2016-09-01 15:49:22 -0700

    add solution

--- Algorithms/JS/arrays/matrices/uniquePaths.js
+++ Algorithms/JS/arrays/matrices/uniquePaths.js
@@ -16,6 +16,26 @@
 // ex. 3
 // ex. 6
 
+
+var uniquePaths = function(m, n) {
+    if (m === 0 || n === 0) return 0;
+    if (m === 1 || n === 1) return 1;
+
+    var dp = [];
+    for (var i = 0; i < n; i++) {
+        dp[i] = 1;
+    }
+    for (i = 1; i < m; i++) {
+        for (var j = 1; j < n; j++) {
+            dp[j] += dp[j - 1];
+        }
+    }
+    return dp[n - 1];
+};
+
+// --------------------------------------------
+
+
 var uniquePaths = function(m, n) {
     var map = [];
     for(var i = 0; i < m; i++) {
