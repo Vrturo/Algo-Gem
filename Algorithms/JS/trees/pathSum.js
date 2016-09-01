b635ddc58e4c2ea44746ebd55b95ba92956f968c
Arturo Perez <VrturoPerez@gmail.com>
2016-08-31 23:02:22 -0700

    add desc

--- Algorithms/JS/trees/pathSum.js
+++ Algorithms/JS/trees/pathSum.js
@@ -1,1 +1,20 @@
-pathSum.js
+// Given a binary tree and a sum,
+// determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
+
+// For example:
+// Given the below binary tree and sum = 22,
+//               5
+//              / \
+//             4   8
+//            /   / \
+//           11  13  4
+//          /  \      \
+//         7    2      1
+// return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
+
+// Input: Array, Integer
+// ex. [5,4,8,11,13,4,7,2,1]
+// 22
+
+// Output: Boolean
+// ex. false
