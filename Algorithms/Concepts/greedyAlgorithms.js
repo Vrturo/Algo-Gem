f6b74b60fb7a24dda950501eff22f053d1694442
Arturo Perez <VrturoPerez@gmail.com>
2016-08-31 19:56:06 -0700

    add desc ad new file

--- Algorithms/Concepts/greedyAlgorithms.js
+++ Algorithms/Concepts/greedyAlgorithms.js
@@ -1,1 +1,10 @@
-greedyAlgorithms.js
+// Greedy is an algorithmic paradigm that builds up a solution piece by piece,
+// always choosing the next piece that offers the most obvious and immediate benefit.
+// Greedy algorithms are used for optimization problems.
+// An optimization problem can be solved using Greedy if the problem has the following property:
+// At every step, we can make a choice that looks best at the moment,
+// and we get the optimal solution of the complete problem.
+// If a Greedy Algorithm can solve a problem,
+// then it generally becomes the best method to solve that problem as the Greedy algorithms
+// are in general more efficient than other techniques like Dynamic Programming.
+// But Greedy algorithms cannot always be applied.
---
+++ Algorithms/JS/arrays/activitySelection.js
@@ -0,0 +1,1 @@
+activitySelection.js
