a8c94b50f19870308b635112b9bb7ae97769ccbb
Arturo Perez <VrturoPerez@gmail.com>
2016-09-01 15:39:55 -0700

    add desc:

--- Algorithms/JS/arrays/matrices/uniquePaths.js
+++ Algorithms/JS/arrays/matrices/uniquePaths.js
@@ -1,1 +1,17 @@
-uniquePaths.js
+// A robot is located at the top-left corner of a m x n grid.
+
+// The robot can only move either down or right at any point in time.
+// The robot is trying to reach the bottom-right corner of the grid.
+
+// How many possible unique paths are there?
+
+// Note: m and n will be at most 100.
+
+
+// Input: Integer, Integer
+// ex. 2, 3
+// ex. 3, 3
+
+// Output: Integer
+// ex. 3
+// ex. 6
