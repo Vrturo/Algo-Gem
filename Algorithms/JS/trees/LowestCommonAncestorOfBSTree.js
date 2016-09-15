5ec7fb95dc002a68458ad1801c20d17ec5bdb8c7
Arturo Perez <VrturoPerez@gmail.com>
2016-09-14 20:27:39 -0700

    add notes

--- Algorithms/JS/trees/LowestCommonAncestorOfBSTree.js
+++ Algorithms/JS/trees/LowestCommonAncestorOfBSTree.js
@@ -14,6 +14,13 @@
 // For example, the lowest common ancestor (LCA) of nodes 2 and 8 is 6.
 // Another example is LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself according to the LCA definition.
 
+
+// Walk down from the whole tree's root as long as both p and q are in the same subtree
+// (meaning their values are both smaller or both larger than root's).
+// This walks straight from the root to the LCA, not looking at the rest of the tree,
+// so it's pretty much as fast as it gets.
+
+
 var lowestCommonAncestor = function(root, p, q) { // 178 ms runtime
     while( (root.val - p.val) * (root.val - q.val) > 0 ){
         root = p.val < root.val ? root.left : root.right;
