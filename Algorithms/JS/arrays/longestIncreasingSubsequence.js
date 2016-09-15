199fce6a90c0cdb74b3a2f5c192edeea2160e833
Arturo Perez <VrturoPerez@gmail.com>
2016-09-15 08:38:16 -0700

    add desc

--- Algorithms/JS/arrays/longestIncreasingSubsequence.js
+++ Algorithms/JS/arrays/longestIncreasingSubsequence.js
@@ -1,1 +1,7 @@
-longestIncreasingSubsequence.js
+// Given an unsorted array of integers, find the length of longest increasing subsequence.
+
+// For example,
+// Given [10, 9, 2, 5, 3, 7, 101, 18],
+// The longest increasing subsequence is [2, 3, 7, 101], therefore the length is 4.
+
+// Note that there may be more than one LIS combination, it is only necessary for you to return the length.
