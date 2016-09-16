ba3fbaa673147826cf22be49c486ade1864f5e9d
Arturo Perez <VrturoPerez@gmail.com>
2016-09-16 16:19:05 -0700

    add notes

--- Algorithms/JS/arrays/longestIncreasingSubsequence.js
+++ Algorithms/JS/arrays/longestIncreasingSubsequence.js
@@ -8,19 +8,26 @@
 
 
 var lengthOfLIS = function(nums) {
-    if( nums.length < 1 ) return 0;
-    var maxRef = 1;
+    if( nums.length < 1 ) return 0; // if empty return 0
+    var maxRef = 1; // smallest ref can be is 1
+
     function lis( arr, n ){
-        if( n === 1 ) return 1;
+        if( n === 1 ) return 1; // base case
         var result,
             maxEnd = 1;
+
+        // If arr[i-1] is smaller than arr[n-1], and
+        // max ending with arr[n-1] needs to be updated, then update it
         for( var i=1; i<n; i++ ){
             result = lis(arr, i);
             if( arr[i-1] < arr[n-1] && result + 1 > maxEnd ) maxEnd = result +1;
         }
+        // Compare max_ending_here with the overall max.
+        // Update the overall max if needed
         if( maxRef < maxEnd ) maxRef = maxEnd;
         return maxEnd;
     }
+
     lis( nums, nums.length );
     return maxRef;
 };
