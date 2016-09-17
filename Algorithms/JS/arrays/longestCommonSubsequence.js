2aa94f9f9065a0b67689289caff44a739a0be02e
Arturo Perez <VrturoPerez@gmail.com>
2016-09-16 19:35:37 -0700

    add desc

--- Algorithms/JS/arrays/longestCommonSubsequence.js
+++ Algorithms/JS/arrays/longestCommonSubsequence.js
@@ -1,1 +1,9 @@
-longestCommonSubsequence.js
+// Given two sequences, find the length of longest subsequence present in both of them.
+// A subsequence is a sequence that appears in the same relative order,
+// but not necessarily contiguous. For example, “abc”, “abg”, “bdf”, “aeg”, ‘”acefg”, .. etc are subsequences of “abcdefg”.
+
+// So a string of length n has 2^n different possible subsequences.
+
+// Examples:
+// LCS for input Sequences “ABCDGH” and “AEDFHR” is “ADH” of length 3.
+// LCS for input Sequences “AGGTAB” and “GXTXAYB” is “GTAB” of length 4.
