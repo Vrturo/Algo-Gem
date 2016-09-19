881026dbe446ef5853d710f1a5aa7e7faebf54f3
Arturo Perez <VrturoPerez@gmail.com>
2016-09-19 12:26:01 -0700

    add solution

--- Algorithms/JS/strings/wordBreak.js
+++ Algorithms/JS/strings/wordBreak.js
@@ -7,3 +7,26 @@
 
 // Return true because "leetcode" can be segmented as "leet code".
 
+/**
+ * @param {string} s
+ * @param {set<string>} wordDict
+ *   Note: wordDict is a Set object, see:
+ *   https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
+ * @return {boolean}
+ */
+
+
+var wordBreak = function(s, wordDict) {
+    var map = [];
+
+    for ( var i = 0; i < s.length; i++ ){
+        for ( var j = 0; j <= i; j ++ ) {
+            var word = s.slice( j, i + 1 );
+            if( wordDict.has(word) && ( j === 0 || map[j-1] === true ) ) {
+                map[i] = true;
+            }
+        }
+    }
+
+    return map[s.length - 1] || false;
+};
