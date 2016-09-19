a12332f79e082556efba16eb6dcf7fa524f0a3d6
Arturo Perez <VrturoPerez@gmail.com>
2016-09-19 12:47:46 -0700

    edit solution:

--- Algorithms/JS/trees/maxDepthofBinTree.js
+++ Algorithms/JS/trees/maxDepthofBinTree.js
@@ -15,10 +15,8 @@
  */
 
 var maxDepth = function(root) {
-    if(root === undefined || root===null){
-        return 0;
-    }
-    return Math.max(maxDepth(root.right),maxDepth(root.left)) + 1;
+    if( root === undefined || root===null ) return 0;
+    return Math.max( maxDepth(root.right), maxDepth(root.left) ) +1;
 };
 
 
