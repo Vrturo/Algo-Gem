821b798948337fc3e5579e2a80bee8934198e8d1
Arturo Perez <VrturoPerez@gmail.com>
2016-09-20 14:47:40 -0700

    add solution

--- Algorithms/JS/integers/sumOfTwoIntegers.js
+++ Algorithms/JS/integers/sumOfTwoIntegers.js
@@ -2,3 +2,12 @@
 
 // Example:
 // Given a = 1 and b = 2, return 3.
+
+
+var getSum = function(a, b) {
+    if (b === 0){
+        return a;
+    } else {
+        return getSum( a ^ b, (a & b) << 1);
+    }
+};
