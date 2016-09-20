5238ff6080da685327000c716a76e6c95db3b348
Arturo Perez <VrturoPerez@gmail.com>
2016-09-20 14:50:41 -0700

    add notes

--- Algorithms/JS/integers/sumOfTwoIntegers.js
+++ Algorithms/JS/integers/sumOfTwoIntegers.js
@@ -4,10 +4,11 @@
 // Given a = 1 and b = 2, return 3.
 
 
-var getSum = function(a, b) {
+var getSum = function(a, b) { // 92 ms runtime
     if (b === 0){
         return a;
     } else {
-        return getSum( a ^ b, (a & b) << 1);
+        return getSum( a ^ b, (a & b) << 1); // Sum of two bits can be obtained by performing XOR (^) of the two bits.
+                                              // Carry bit can be obtained by performing AND (&) of two bits.
     }
 };
