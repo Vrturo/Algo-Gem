e80e0cef9b5546bc15d0aeaa5558d0c3e801a2ce
Arturo Perez <VrturoPerez@gmail.com>
2016-09-24 15:02:00 -0700

    add notes

--- Algorithms/JS/strings/editDistance.js
+++ Algorithms/JS/strings/editDistance.js
@@ -9,13 +9,19 @@
 
 
 var minDistance = function(word1, word2, m=word1.length, n=word2.length) {
+    // If first or second string string is empty, the only option is to
+    // insert all characters of full string into empty
     if (m === 0) return n;
     if (n === 0) return m;
+    // If last characters of two strings are same, nothing much to do.
+    // Ignore last characters and get count for remaining strings.
     if ( word1.charAt(m-1) === word2.charAt(n-1) ) return minDistance(word1, word2, m-1, n-1);
 
+    // If last characters are not same, consider all three operations on last character of first string,
+    // recursively compute minimum cost for all three operations and take minimum of three values.
     return 1 + Math.min( minDistance(word1,  word2, m, n-1),    // Insert
-                     minDistance(word1,  word2, m-1, n),        // Remove
-                     minDistance(word1,  word2, m-1, n-1)       // Replace
+                         minDistance(word1,  word2, m-1, n),    // Remove
+                         minDistance(word1,  word2, m-1, n-1)   // Replace
                    );
 
 };
