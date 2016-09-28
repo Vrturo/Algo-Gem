07ea45eaccdfb9f528ae0e111383039249546612
Arturo Perez <VrturoPerez@gmail.com>
2016-09-27 18:55:32 -0700

    edit notes

--- Algorithms/JS/listingsPerPage.js
+++ Algorithms/JS/listingsPerPage.js
@@ -8,53 +8,53 @@
 // After each page insert an empty space as a line break escept for the last page
 
 
-// function sortListing( n, listings ){
-//   var head = 0,
-//       linkedList = [];
-//   //created linkedList of indices
-//   for( var i = 1; i < listings.length; i++ ){
-//     linkedList.push( i );
-//   }
-//   linkedList.push( null );
+function sortListing( n, listings ){
+  var head = 0,
+      linkedList = [];
+  //created linkedList of indices
+  for( var i = 1; i < listings.length; i++ ){
+    linkedList.push( i );
+  }
+  linkedList.push( null );
 
-//   var results = [],
-//       cache = {},
-//       pageCount = 0;
+  var results = [],
+      cache = {},
+      pageCount = 0;
 
-//   var prev = null,
-//       current = head,
-//       listing;
+  var prev = null,
+      current = head,
+      listing;
 
-//   while( head !== null ){ //  Breaks when every value is inserted into results array
-//     if( pageCount === n ){
-//       // if page hits limit, add line break and reset
-//       results.push( 'page break' );
-//       cache = {};
-//       prev = null;
-//       current = head; // restart at the current head index.
-//       pageCount = 0;
-//     }
-//     if( current === null ){ // Hit the end of results so push in next value with repeated host
-//       results.push( listings[head] );
-//       pageCount++;
-//       head = linkedList[head];
-//     } else if( !cache[/[^,]*/.exec(listings[current])[0]] ){ // regex grabs up to 1st comma
-//         cache[/[^,]*/.exec(listings[current])[0]] = true; // cache listing
-//         results.push( listings[current] ); // push current listing into results
-//         pageCount++;
-//         // With each insert, the linked list gets updated by modifying what the prev points to.
-//         if( current === head ){
-//           head = linkedList[head];
-//         }
-//         linkedList[prev] = linkedList[current]; // keep moving down list
-//         current = linkedList[current];
-//     } else {
-//       prev = current; // keep moving down list
-//       current = linkedList[current];
-//     }
-//   }
-//   return results;
-// }
+  while( head !== null ){ //  Breaks when every value is inserted into results array
+    if( pageCount === n ){
+      // if page hits limit, add line break and reset
+      results.push( 'page break' );
+      cache = {};
+      prev = null;
+      current = head; // restart at the current head index.
+      pageCount = 0;
+    }
+    if( current === null ){ // Hit the end of results so push in next value with repeated host
+      results.push( listings[head] );
+      pageCount++;
+      head = linkedList[head];
+    } else if( !cache[/[^,]*/.exec(listings[current])[0]] ){ // regex grabs up to 1st comma
+        cache[/[^,]*/.exec(listings[current])[0]] = true; // cache listing
+        results.push( listings[current] ); // push current listing into results
+        pageCount++;
+        // With each insert, the linked list gets updated by modifying what the prev points to.
+        if( current === head ){
+          head = linkedList[head];
+        }
+        linkedList[prev] = linkedList[current]; // keep moving down list
+        current = linkedList[current];
+    } else {
+      prev = current; // keep moving down list
+      current = linkedList[current];
+    }
+  }
+  return results;
+}
 
 
 var testTwo = [ "11, 2, 99, San Francisco",
@@ -107,6 +107,16 @@ var testThree = [ "1, 2, 99, San Francisco",
 // -----------------------------------------------------------
 
 
+
+// Given two paramaters, an integer(n), a list of Arrays with Objects that conatin a Host_id, Listing_id, Score, City
+// Build a function that helps display n amount of objects per page.
+
+// The list of objects are given in order of Score, from High to Low.
+// The objects must be displayed in order of highest score first.
+// We dont want one host dominating the pages.
+// Each page must display any unique hosts that havent displayed yet.
+// After each page insert an empty space as a line break escept for the last page
+
 function paginate(num, results) {
     var solution = [],
         dup = [],
@@ -130,7 +140,7 @@ function paginate(num, results) {
         if( i >= results.length ){
             j = 0;
             while( j < dup.length ){ // at the end as long as theres dups, push into solutions till dup is empty
-                // solution.push( dup.splice(j,1)[1] ); // once results length hits, push all duplicates into solution
+                                    // once results length hits, push all duplicates into solution
                 if( pageCount === num ){
                     pageCount = 0;
                     solution.push( "" );
@@ -163,19 +173,21 @@ function paginate(num, results) {
 }
 
 
-var test = [    "11, 2, 99, San Francisco",
-                "10, 3, 97, Los Angeles",
-                "1, 24, 96, San Francisco",
+var test = [ "11, 2, 99, San Francisco",
+                "6, 3, 96, Los Angeles",
+                "6, 24, 92, San Francisco",
                 "3, 56, 90, San Francisco",
-                "4, 11, 46, San Francisco",
+                "6, 65, 80, Los Angeles",
+                "1, 29, 76, San Francisco",
+                "1, 19, 60, San Francisco",
+                "6, 10, 52, Los Angeles",
+                "11, 11, 46, San Francisco",
                 "5, 65, 40, Los Angeles",
+                "4, 29, 36, San Francisco",
+                "3, 19, 32, San Francisco",
+                "2, 10, 28, Los Angeles",
                 "2, 11, 26, San Francisco",
-                "11, 19, 20, San Francisco",
-                "11, 3, 19, Los Angeles",
-                "11, 24, 18, San Francisco",
-                "11, 56, 17, San Francisco",
-                "11, 24, 15, San Francisco",
-                "11, 56, 14, San Francisco"
+                "5, 19, 20, San Francisco"
             ];
 console.log( paginate( 3, test ) );
 
