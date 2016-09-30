173ce9de8235e36c1b89e7de9f2655331a641e1f
Arturo Perez <VrturoPerez@gmail.com>
2016-09-29 17:58:55 -0700

    add solution

--- Algorithms/JS/async/asyncTimer.js
+++ Algorithms/JS/async/asyncTimer.js
@@ -1,1 +1,31 @@
 // Create an async timer that can display the date every second
+
+
+function getDateAsync() {
+  return new Promise( function( resolve ){
+      // resolve it with the date
+      var date = new Date();
+      resolve(date);
+  });
+  // asynchronously return date
+}
+
+// function resolveDate(theDate) {
+//   // do something with the date
+// }
+
+function update() {
+  //getDateAsync().then(resolveDate);
+
+  getDateAsync().then(function (returnValue) {
+    // promise is resolved
+    // returnValue == the date
+    // update date
+    var timeDiv = document.getElementById('time');
+    timeDiv.innerHTML = returnValue;
+  });
+}
+
+setInterval(update, 1000);
+
+//var date = new Date();
