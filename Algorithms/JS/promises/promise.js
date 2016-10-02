3c80dc63530d00dc353e0dde5198568e080dcd09
Arturo Perez <VrturoPerez@gmail.com>
2016-10-02 13:00:26 -0700

    add simple example

--- Algorithms/JS/promises/promise.js
+++ Algorithms/JS/promises/promise.js
@@ -1,1 +1,12 @@
-promise.js
+new Promise(function(resolve) {
+  console.log('first');
+  resolve();
+  console.log('second');
+}).then(function() {
+  console.log('third');
+}); // -- > first
+    //      second
+    //      third
+
+// 'first', 'second' and 'third' all get logged.
+// Most notably, 'second' gets logged despite the fact that it comes after resolve().
