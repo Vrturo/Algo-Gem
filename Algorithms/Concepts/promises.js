45ec22447b0ad231914eafb62caaaa14176be210
Arturo Perez <VrturoPerez@gmail.com>
2016-10-02 12:47:34 -0700

    add promises notes

--- Algorithms/Concepts/promises.js
+++ Algorithms/Concepts/promises.js
@@ -1,1 +1,25 @@
-promises.js
+The Promise object is used for asynchronous computations.
+A Promise represents a value which may be available now, or in the future, or never.
+
+Promises help handle asynchronous work with ease
+There are many methods to handle asynchronous already but Promises are recommended option beacause they give you
+flexability, intuative syntax, and error handling
+
+The promise object is used for deferred and asynchronous computations
+
+What is asynchronous work?
+
+Normally code is synchronous. One line of code is executed, and the next line is executed immediately afterwards.
+
+asynchronous code is not guaranteed to execute in a timeline.
+You should assume that you have no idea when asynchronous function will complete.
+Jsut because one request is fired first, it will arrive before the next request.
+Or that one might fail and mght not even return
+
+4 states of Promises
+
+Fullfilled - resolved (it worked)
+
+Rejected - it didnt work
+
+Pending - Still waiting
