598ccc059c669588f588b60519f73caf01f61303
Arturo Perez <VrturoPerez@gmail.com>
2016-10-02 22:58:40 -0700

    add probability

--- Algorithms/JS/integers/fiveSidedDie.js
+++ Algorithms/JS/integers/fiveSidedDie.js
@@ -3,3 +3,17 @@
 
 // rand7() returns each integer with equal probability.
 // rand5() must also return each integer with equal probability.
+
+// We can write out each possible result from rand7()
+// (each of which is equally probable, per the problem statement)
+// and see that some results for rand5() are more likely because they are caused by more results from rand7():
+
+
+rand7() rand5()
+1       2
+2       3
+3       4
+4       5
+5       1
+6       2
+7       3
