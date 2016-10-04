0241d4dbb5dc06191512f2228d1a3c0f21e63873
Arturo Perez <VrturoPerez@gmail.com>
2016-10-04 16:24:51 -0700

    add solution

--- Algorithms/JS/arrays/matrices/meetingRooms.js
+++ Algorithms/JS/arrays/matrices/meetingRooms.js
@@ -8,3 +8,12 @@
 // [[5, 10],[15, 20],[0, 3]]
 // return true
 
+var canAttendMeetings = function(intervals) {
+    intervals.sort(function(a, b) {
+        return a.start - b.start;
+    });
+    for( var i=0; i<intervals.length-1; i++ ){
+        if( intervals[i].end > intervals[i+1].start ) return false;
+    }
+    return true;
+};
