5e62f294115649b0d3c48ee4df13c1098aae0dea
Arturo Perez <VrturoPerez@gmail.com>
2016-10-04 12:37:33 -0700

    add unfinished solution

--- Algorithms/JS/arrays/matrices/wordSearchTwo.js
+++ Algorithms/JS/arrays/matrices/wordSearchTwo.js
@@ -16,3 +16,45 @@
 // Return ["eat","oath"].
 // Note:
 // You may assume that all inputs are consist of lowercase letters a-z.
+
+
+// --------------------UNFINSISHED-----------
+
+var findWords = function(board, words) {
+    var cache = {};
+
+    function check(x, y){
+        if( board[x-1][y] ) // top
+        if( board[x-1][y+1] ) // tr diag
+        if( board[x][y+1] ) // right
+        if( board[x+1][y-1] ) // br diag
+        if( board[x][y+1] ) // bottom
+        if( board[x+1][y-1] ) // bl diag
+        if( board[x][y-1] ) //left
+        if( board[x-1][y-1] ) // tl diag
+    }
+
+                if( board[i-1] ){
+                    if( board[i-1][j] === 1 ) count+=1 // vertical t check
+                    if( board[i-1][j+1] ){ if( board[i-1][j+1] === 1 ) count+=1 }// rdiag t check
+                    if( board[i-1][j-1] ){ if( board[i-1][j-1] === 1 ) count+=1 }// ldiag t check
+                }
+                if( board[i+1] ){
+                    if( board[i+1][j] === 1 ) count+=1 // vertical b check
+    }
+    var counter = 0;
+    while( counter < words.length){
+        var word = words[counter];
+        for( var i=0; i<board.length; i++ ){
+            for( var j=0; j<board[i].length; j++ ){
+               if( word[0] === board[i][j] ){
+                   if( check(x, y) )
+               }
+            }
+        }
+
+        counter++;
+    }
+
+
+};
