bad6d2c3447e2bd13ac1cd3262a16f8cc1061750
Arturo Perez <VrturoPerez@gmail.com>
2016-10-04 16:24:38 -0700

    add description

--- Algorithms/JS/arrays/matrices/meetingRooms.js
+++ Algorithms/JS/arrays/matrices/meetingRooms.js
@@ -1,1 +1,10 @@
-meetingRooms.js
+// Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei),
+// determine if a person could attend all meetings.
+
+// For example,
+// Given [[0, 30],[5, 10],[15, 20]],
+// return false.
+
+// [[5, 10],[15, 20],[0, 3]]
+// return true
+
