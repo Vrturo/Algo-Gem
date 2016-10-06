077bdfc60cd45358e3777c95ca5383d3fcbdb5c9
Arturo Perez <VrturoPerez@gmail.com>
2016-10-05 19:56:37 -0700

    add input and data structure methods

--- Algorithms/JS/trees/minDepthBinaryTree.js
+++ Algorithms/JS/trees/minDepthBinaryTree.js
@@ -1,3 +1,16 @@
 // Given a binary tree, find its minimum depth.
 
 // The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
