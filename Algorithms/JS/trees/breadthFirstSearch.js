8af1efc4f42a9e6996bea1785e322e9e8273226e
Arturo Perez <VrturoPerez@gmail.com>
2016-10-06 11:10:37 -0700

    add description

--- Algorithms/JS/trees/breadthFirstSearch.js
+++ Algorithms/JS/trees/breadthFirstSearch.js
@@ -1,1 +1,5 @@
-breadthFirstSearch.js
+// Suppose you’d like to traverse through a family tree in JavaScript, printing each generation of children on a single line.
+
+// Breadth-first search (BFS) is an algorithm for traversing or searching tree or graph data structures.
+// It starts at the tree root (or some arbitrary node of a graph, sometimes referred to as a 'search key'[1])
+// and explores the neighbor nodes first, before moving to the next level neighbors.
