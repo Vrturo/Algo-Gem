71b117bc6ff1c9838d6f56dc412c6b2969494798
Arturo Perez <VrturoPerez@gmail.com>
2016-10-05 21:01:27 -0700

    add notes

--- Algorithms/JS/trees/minDepthBinaryTree.js
+++ Algorithms/JS/trees/minDepthBinaryTree.js
@@ -15,9 +15,10 @@
  * @return {number}
  */
 
-var minDepth = function(root) {
+var minDepth = function(root) { // 139ms runtime
     if( root === null ) return 0;
     var left = minDepth(root.left),
         right = minDepth(root.right);
+    // gather smallest depth and add 1 because depth starts at 1
     return (left === 0 || right === 0) ? left + right + 1: Math.min( left,right ) + 1;
 };
