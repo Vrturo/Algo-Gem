fc3352551a1743a90ae55b9810f3bf73d4256020
Arturo Perez <VrturoPerez@gmail.com>
2016-10-05 19:30:33 -0700

    add input and data structure

--- Algorithms/JS/trees/symmetricTree.js
+++ Algorithms/JS/trees/symmetricTree.js
@@ -14,3 +14,16 @@
 //   2   2
 //    \   \
 //    3    3
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
