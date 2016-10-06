d780241e3fb8854d3d09921237db0bf26bfbf19e
Arturo Perez <VrturoPerez@gmail.com>
2016-10-05 19:56:16 -0700

    add desc

--- Algorithms/JS/trees/minDepthBinaryTree.js
+++ Algorithms/JS/trees/minDepthBinaryTree.js
@@ -1,1 +1,3 @@
-minDepthBinaryTree.js
+// Given a binary tree, find its minimum depth.
+
+// The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
