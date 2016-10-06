e1da2931bc01c0f58fd1f1bce903703fa29da944
Arturo Perez <VrturoPerez@gmail.com>
2016-10-05 19:56:56 -0700

    add solution

--- Algorithms/JS/trees/minDepthBinaryTree.js
+++ Algorithms/JS/trees/minDepthBinaryTree.js
@@ -14,3 +14,10 @@
  * @param {TreeNode} root
  * @return {number}
  */
+
+var minDepth = function(root) {
+    if( root === null ) return 0;
+    var left = minDepth(root.left),
+        right = minDepth(root.right);
+    return (left === 0 || right === 0) ? left + right + 1: Math.min( left,right ) + 1;
+};
