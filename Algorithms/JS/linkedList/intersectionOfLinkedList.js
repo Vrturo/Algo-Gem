2b1282a9b8607d55a08bb4995d56653e2d0b1fba
Arturo Perez <VrturoPerez@gmail.com>
2016-10-11 11:33:02 -0700

    add solution

--- Algorithms/JS/linkedList/intersectionOfLinkedList.js
+++ Algorithms/JS/linkedList/intersectionOfLinkedList.js
@@ -31,3 +31,22 @@
  * @param {ListNode} headB
  * @return {ListNode}
  */
+
+
+var getIntersectionNode = function(headA, headB) {
+    var p1 = headA,
+        p2 = headB;
+
+    if( p1 === null || p2 === null ) return null;
+
+    while( p1 !== null && p2 !== null && p1 !== p2 ){
+        p1 = p1.next;
+        p2 = p2.next;
+
+        if( p1 === p2 ) return p1;
+
+        if( p1 === null ) p1 = headB;
+        if( p2 === null ) p2 = headA;
+    }
+    return p1;
+};
