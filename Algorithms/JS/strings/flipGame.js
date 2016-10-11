6629609e6c4df39ef5bbc7ec362bab5a23507764
Arturo Perez <VrturoPerez@gmail.com>
2016-10-10 22:32:44 -0700

    add first version of solution

--- Algorithms/JS/strings/flipGame.js
+++ Algorithms/JS/strings/flipGame.js
@@ -13,3 +13,32 @@
 //   "++--"
 // ]
 // If there is no valid move, return an empty list [].
+
+var generatePossibleNextMoves = function(s) {
+    var result = [],
+        counter = 0,
+        i=0;
+
+    while( i<s.length-1){
+        var current = check(i);
+        if( current !== 'no combo' ) result.push( current );
+        i++;
+    }
+
+    function check( index ){
+        var string = s.slice().split("");
+        for( var j=index; j<s.length; j++ ){
+            if( string[j] === '+' && string[j+1]=== '+' ){
+                counter++;
+                string.splice.apply( string,[j,1].concat( '-'.split('') ) );
+                string.splice.apply( string,[j+1,1].concat( '-'.split('') ) );
+                return string.join("");
+            }
+        }
+        return 'no combo';
+    }
+    result = result.filter(function(item, pos) {
+        return result.indexOf(item) == pos;
+    });
+    return counter < 1 ? [] : result;
+};
