200f6ec8599f89fd86932248ee1445ddf79c16e7
Arturo Perez <VrturoPerez@gmail.com>
2016-10-11 12:27:03 -0700

    add solution

--- Algorithms/JS/arrays/containsDuplicatesTwo.js
+++ Algorithms/JS/arrays/containsDuplicatesTwo.js
@@ -1,3 +1,13 @@
 // Given an array of integers and an integer k,
 // find out whether there are two distinct indices i and j in the array such that nums[i] = nums[j]
 // and the difference between i and j is at most k.
+
+
+var containsNearbyDuplicate = function(nums, k) {
+    for( var i=0; i<nums.length; i++){
+        for( var j=i+1; j<nums.length; j++){
+            if( nums[i] === nums[j] && (j - i) <= k ) return true;
+        }
+    }
+    return false;
+};
