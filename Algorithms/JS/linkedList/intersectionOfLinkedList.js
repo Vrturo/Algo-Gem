6ca9cc0322de12b0e862911d7f8265f1c8eb8565
Arturo Perez <VrturoPerez@gmail.com>
2016-10-11 11:34:32 -0700

    add notes

--- Algorithms/JS/linkedList/intersectionOfLinkedList.js
+++ Algorithms/JS/linkedList/intersectionOfLinkedList.js
@@ -43,8 +43,14 @@ var getIntersectionNode = function(headA, headB) {
         p1 = p1.next;
         p2 = p2.next;
 
+        // Any time they collide or reach end together without colliding
+        // then return any one of the pointers.
         if( p1 === p2 ) return p1;
 
+        // If one of them reaches the end earlier then reuse it
+        // by moving it to the beginning of other list.
+        // Once both of them go through reassigning,
+        // they will be equidistant from the collision point.
         if( p1 === null ) p1 = headB;
         if( p2 === null ) p2 = headA;
     }
