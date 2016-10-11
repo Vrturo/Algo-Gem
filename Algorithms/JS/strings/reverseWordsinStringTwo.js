9f663f544360e4b5be638dc9683eab543dd97473
Arturo Perez <VrturoPerez@gmail.com>
2016-10-10 21:19:58 -0700

    add solution

--- Algorithms/JS/strings/reverseWordsinStringTwo.js
+++ Algorithms/JS/strings/reverseWordsinStringTwo.js
@@ -7,3 +7,27 @@
 // return "blue is sky the".
 
 // Do it in-place without allocating extra space?
+
+var reverseWords = function(str) {
+    reverse(str, 0, str.length - 1);
+    var start = 0,
+        end = -1;
+
+    for( var i = 0; i < str.length; i++ ){
+        if( str[i] == ' ' ) {
+            reverse( str, start, i-1 );
+            start = i + 1;
+        }
+    }
+    reverse( str, start, str.length - 1 );
+};
+
+function reverse(string, start, end){
+    while (start < end) {
+        var temp = string[start];
+        string[start] = string[end];
+        string[end] = temp;
+        start++;
+        end--;
+    }
+}
