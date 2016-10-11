e6e3db8c8b22897177840f6823762385997ff276
Arturo Perez <VrturoPerez@gmail.com>
2016-10-11 16:53:48 -0700

    add descritpion

--- Algorithms/JS/integers/shoppingCart.js
+++ Algorithms/JS/integers/shoppingCart.js
@@ -1,1 +1,4 @@
-shoppingCart.js
+// Given a total, return all the possible items someone can order and have no change left over.
+
+// Input:  An integer N (total being spent)
+// Output: An array (number of ways to spend exact total)
