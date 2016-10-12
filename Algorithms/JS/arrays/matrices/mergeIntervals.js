f3f8a0ed9934a6d05da12c41c2cfa530e2fb4367
Arturo Perez <VrturoPerez@gmail.com>
2016-10-12 16:52:13 -0700

    add temp solution

--- Algorithms/JS/arrays/matrices/mergeIntervals.js
+++ Algorithms/JS/arrays/matrices/mergeIntervals.js
@@ -18,3 +18,38 @@
  * @param {Interval[]} intervals
  * @return {Interval[]}
  */
+
+
+
+var merge = function(intervals) {
+    if( intervals.length < 1 ) return intervals;
+    var start = [],
+        end = [];
+    for( var i=0; i<intervals.length; i++ ){
+        start.push( intervals[i].start );
+        end.push( intervals[i].end );
+    }
+    // if end[i] < start[j]
+        // store end
+        // j++
+    // else
+        // store start
+        // i++
+    var result = [],
+        tempStart = start[0];
+    for( var j=0, k=0; j< start.length || k<end.length; ){
+        if( start[j] < end[k] ){
+            tempStart = Math.min(tempStart, start[j]);
+            j++;
+        } else {
+            result.push( [tempStart, end[k+1]] );
+            tempStart = start[j];
+            k++;
+            j++;
+        }
+    }
+        // [ 1, 2, 8, 15 ]
+        // [ 3, 6, 10, 18 ]
+   return result;
+
+};
