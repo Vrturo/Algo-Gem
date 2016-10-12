c2f7b910bb2313f7db2c318e105800f1bf11cdac
Arturo Perez <VrturoPerez@gmail.com>
2016-10-12 16:56:21 -0700

    add solution

--- Algorithms/JS/arrays/matrices/mergeIntervals.js
+++ Algorithms/JS/arrays/matrices/mergeIntervals.js
@@ -19,6 +19,25 @@
  * @return {Interval[]}
  */
 
+function merge(intervals) { // 148 ms runtime
+  if (!intervals.length) return intervals;
+  intervals.sort((a, b) => a.start !== b.start ? a.start - b.start : a.end - b.end);
+  var prev = intervals[0],
+      res = [prev];
+
+  for (var curr of intervals) {
+    if (curr.start <= prev.end) {
+      prev.end = Math.max(prev.end, curr.end);
+    } else {
+      res.push(curr);
+      prev = curr;
+    }
+  }
+
+  return res;
+}
+
+//---------------------------------------------------------
 
 
 var merge = function(intervals) {
