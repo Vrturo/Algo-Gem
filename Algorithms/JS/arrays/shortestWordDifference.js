09903f3e9ffdf46881c33391f2b640ef81587689
Arturo Perez <VrturoPerez@gmail.com>
2016-10-11 19:08:24 -0700

    add solution

--- Algorithms/JS/arrays/shortestWordDifference.js
+++ Algorithms/JS/arrays/shortestWordDifference.js
@@ -8,3 +8,22 @@
 
 // Note:
 // You may assume that word1 does not equal to word2, and word1 and word2 are both in the list.
+
+/**
+ * @param {string[]} words
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number}
+ */
+
+
+var shortestDistance = function(words, word1, word2) {
+    var distance = Number.MAX_VALUE,
+        w1, w2;
+    for( var i=0; i<words.length; i++ ){
+        if( words[i] === word1 ) w1 = i;
+        if( words[i] === word2 ) w2 = i;
+        if( w1 !== undefined && w2 !== undefined) distance = Math.min( distance, Math.abs(w1 - w2) );
+    }
+   return distance;
+};
