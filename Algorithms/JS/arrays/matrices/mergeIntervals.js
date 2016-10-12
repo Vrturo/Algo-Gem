93ce4e5b817ca1ae799cb773e65ae2df655f7fca
Arturo Perez <VrturoPerez@gmail.com>
2016-10-12 16:51:01 -0700

    add given params

--- Algorithms/JS/arrays/matrices/mergeIntervals.js
+++ Algorithms/JS/arrays/matrices/mergeIntervals.js
@@ -3,3 +3,18 @@
 // For example,
 // Given [1,3],[2,6],[8,10],[15,18],
 // return [1,6],[8,10],[15,18].
+
+
+/**
+ * Definition for an interval.
+ * function Interval(start, end) {
+ *     this.start = start;
+ *     this.end = end;
+ * }
+ */
+
+
+/**
+ * @param {Interval[]} intervals
+ * @return {Interval[]}
+ */
