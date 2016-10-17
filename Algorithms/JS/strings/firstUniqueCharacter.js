6385e3b0bf9b9db254df108b5663cf8d608520e8
Arturo Perez <VrturoPerez@gmail.com>
2016-10-17 13:09:04 -0700

    tweak solution and add notes

--- Algorithms/JS/strings/firstUniqueCharacter.js
+++ Algorithms/JS/strings/firstUniqueCharacter.js
@@ -15,18 +15,18 @@
  */
 
 
-var firstUniqChar = function(s) {
+var firstUniqChar = function(s) { // 139 ms runtime
     var map = {};
     for( var i=0; i<s.length; i++ ){
-        if( map[ s[i] ] ){
-            map[ s[i] ][1]+=1;
+        if( map[ s[i] ] === undefined ){ // if letter hasn't been encountered
+            map[ s[i] ] = [i,1]; // set string letter as key and value as tuple of the index and letter count
         } else{
-            map[ s[i] ] = [i,1];
+            map[ s[i] ][1] += 1; // increment the second element in tuple which is letter count
         }
     }
 
-    for( var k in map ){
-        if( map[k][1] === 1 ) return map[k][0];
+    for( var k in map ){ // loop through hash
+        if( map[k][1] === 1 ) return map[k][0]; // if a character count is equal to 1 it is the first unique, so return
     }
-    return -1;
+    return -1; // return -1 if we didnt find a unique in our map
 };
