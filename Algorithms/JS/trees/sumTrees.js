1de46b9952e5fc0b2d556912a2bef823c9c34042
Arturo Perez <VrturoPerez@gmail.com>
2016-10-19 13:28:53 -0700

    add notes

--- Algorithms/JS/trees/sumTrees.js
+++ Algorithms/JS/trees/sumTrees.js
@@ -29,22 +29,29 @@ function isSumTree(root) {
 
 // ---------------------------------------
 
-
+/* A helper function to get the sum of values in tree with root
+     as root */
 function sum( node ) {
     if (node === null) return 0;
     return sum(node.left) + node.data + sum(node.right);
 }
 
+/* returns 1 if sum property holds for the given
+   node and both of its children */
 function isSumTree( node ) {
     var ls, rs;
 
-
+    /* If node is NULL or it's a leaf node then
+       return true */
     if ( (node === null) || (node.left === null && node.right === null) )
         return true;
 
+    /* Get sum of nodes in left and right subtrees */
     ls = sum(node.left);
     rs = sum(node.right);
 
+    /* if the node and both of its children satisfy the
+       property return 1 else 0*/
     if ( (node.data == ls + rs) && (isSumTree(node.left) !== 0 ) && ( isSumTree(node.right)) != 0 )
         return 1;
 
