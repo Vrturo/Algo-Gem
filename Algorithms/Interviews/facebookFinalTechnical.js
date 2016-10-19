b898e7938251a98913c83a91b8c6368a2068bc9a
Arturo Perez <VrturoPerez@gmail.com>
2016-10-18 17:09:46 -0700

    add new interview file

---
+++ Algorithms/Interviews/facebookFinalTechnical.js
@@ -0,0 +1,25 @@
+Facebook Final Technical Interview. Software Engineer Role
+
+10 min Intro
+20 min Resume Project Talk
+  - Asked for detailed information on (Stack, challenges, decisions, etc..)
+30 min on algorithm
+
+
+// Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0?
+// Find all unique triplets in the array which gives the sum of zero.
+
+// Note:
+// Elements in a triplet (a,b,c) must be in non-descending order. (ie, a ≤ b ≤ c)
+// The solution set must not contain duplicate triplets.
+//     For example, given array S = [-1 0 1 2 -1 -4 ],
+
+//     A solution set is:
+//     (-1, 0, 1)
+//     (-1, -1, 2)
+
+// BREAKDOWN PROBLEM
+
+var threeSum = function(nums) {
+    // Enter Code here..
+}
