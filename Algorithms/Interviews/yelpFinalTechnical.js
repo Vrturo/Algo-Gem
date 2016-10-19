6157513f5e7bca554f4109651bc82b6cb78ed362
Arturo Perez <VrturoPerez@gmail.com>
2016-10-18 17:11:23 -0700

    add new interview file

---
+++ Algorithms/Interviews/yelpFinalTechnical.js
@@ -0,0 +1,30 @@
+Yelp Final Technical Interview. Full Stack Engineer Role.
+
+10 min Intro
+20 min Resume Project Talk
+  - Asked for detailed information on (Stack, challenges, decisions, etc..)
+30 min on algorithm
+
+
+// A user is going through Yelp's page and wants to check if a restaurant has a reservation open
+
+// Build a function that efficiently takes a user's input, and checks against available time slots
+// time slots are in 30 minute increments
+
+// *Note: Array is sorted
+// 10am === 10
+// 1pm === 13
+// 1:30pm === 13.5
+
+
+//example
+
+// input: String, Array
+// '10:30pm', [9, 9.5, 12, 13, 15, 16, 17, 18.5, 19, 19.5]
+
+// output: Boolean
+// True
+
+function availablity( input, available ){
+  // Enter Code here...
+}
