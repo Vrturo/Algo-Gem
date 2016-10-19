7c670d088dd0b347ef64ebb4b7c2cd692ce6720c
Arturo Perez <VrturoPerez@gmail.com>
2016-10-18 17:10:48 -0700

    add new interview file

---
+++ Algorithms/Interviews/gustoFinalTechnical.js
@@ -0,0 +1,56 @@
+Gusto Final Technical Interview. Full Stack Engineer Role
+
+10 min Intro
+50 min on algorithm
+
+/**
+ You are the support team for an international man of mystery. The only entrance to
+Dr. Evil's lair is a series of long hallways with lights scattered along the lengths.
+The IMOM can avoid detection if he can traverse the length of the hallway without being
+hit by the circular light radiated from any of the lights. The data of each light's
+position and light radius has been transmitted back to you. Quickly determine if the
+IMOM can enter the lair undetected and foil Dr. Evil's sinister & flamboyant plot.
+ ^
+ |
+ Y ------------------------------------------------------
+ |      ^             /'''''\
+ |      |            (   x light
+ |  hall_width        \     /
+ |      |              `````              /'''''\
+ |      |                                (   x light
+ |      |                                 \     /
+ |-X----------------------------------------------------->
+ distance between points p1 & p2 is `d = Math.sqrt((p2.x - p1.x)**2 + (p2.y - p1.y)**2)`
+ hall_two: http://i.imgur.com/kHOtT7z.png
+ hall_three: http://i.imgur.com/i2oKJk2.png
+*/
+
+// Define any classes/dependencies here
+
+
+/**
+ * @param lights is an array of hashes with shape { x: #, y: #, radius: # }
+ *        describing a light's position and reach
+ * @param hall_width is the width of the infinitely long hallway (y-axis w.r.t. the points)
+ *
+ * do any setup/conversion of data you desire and return an object that implements a #traversable function
+*/
+
+
+// example Input
+ // [
+ //    { x: 20, y: 10, radius: 12 },
+ //    { x: 34, y:  6, radius: 8 },
+ //    { x: 30, y: 30, radius: 12 },
+ //    { x: 40, y: 40, radius: 11 }
+ //  ], 50
+
+// Input: Array, Integer
+
+// Output: Boolean
+
+function parse(lights, hallWidth) {
+
+
+}
+
