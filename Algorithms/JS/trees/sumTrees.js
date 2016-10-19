a3706ede19d4133b930622b0e2aa740cb7902be1
Arturo Perez <VrturoPerez@gmail.com>
2016-10-19 13:07:35 -0700

    add description

--- Algorithms/JS/trees/sumTrees.js
+++ Algorithms/JS/trees/sumTrees.js
@@ -1,1 +1,11 @@
-sumTrees.js
+// Write a function that returns true if the given Binary Tree is SumTree else false.
+// A SumTree is a Binary Tree where the value of a node is equal to sum of the nodes present in its left subtree and right subtree.
+// An empty tree is SumTree and sum of an empty tree can be considered as 0. A leaf node is also considered as SumTree.
+
+// Following is an example of SumTree.
+
+//           26
+//         /   \
+//       10     3
+//     /    \     \
+//   4      6      3
