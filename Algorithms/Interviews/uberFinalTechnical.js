15490bc4fdeea96ab1e6efabcef79e2f70d187b2
Arturo Perez <VrturoPerez@gmail.com>
2016-10-18 17:10:56 -0700

    add new interview file

---
+++ Algorithms/Interviews/uberFinalTechnical.js
@@ -0,0 +1,25 @@
+Uber Final Technical Interview. Front End Engineer Role.
+
+10 min Intro
+20 min Resume Project Talk
+  - Asked for detailed information on (Stack, challenges, decisions, etc..)
+30 min on algorithm
+
+
+// Implement a Calculator
+// Input is a string
+// String can contain positive numbers or zero, + and * operator.
+// * Note: Multiplication is prioritized over addition
+// Output is sum of string
+
+// input example:
+// i = '2+10*3+6*10+1'
+// output = 93
+
+
+function calculate(i) {
+
+  // Enter code here..
+
+}
+
