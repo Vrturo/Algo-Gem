5e76741806a6ed80b91d2e1b63de0acb0ff35c7c
Arturo Perez <VrturoPerez@gmail.com>
2016-10-18 19:41:04 -0700

    add resources

---
+++ bigList.js
@@ -0,0 +1,45 @@
+- [ ] COMPUTER ARCHITECTURE
+81 HOURS EXPECTED https://www.coursera.org/learn/build-a-computer#faqs http://www.nand2tetris.org
+80 hours expected Berkley CS 152 http://www-inst.eecs.berkeley.edu/~cs152/sp16/ https://www.youtube.com/playlist…
+- [ ] PROBLEM SOLVING, ALGORITHMS AND DATA STRUCTURES
+Total Hours: 115 hours
+Reading The Algorithm Design Manual 30 hours
+Lectures http://www3.cs.stonybrook.edu/~algorith/video-lectures/ 50 Hours
+Practice Exercises https://leetcode.com/problemset/algorithms/?cong=true 20 hours
+Reading: How To Solve It by G Polya 15 hours
+- [ ] Operating Systems & Web Browser
+Total Hours: 70 hours
+Reading Operating System: Three Easy Pieces http://pages.cs.wisc.edu/~remzi/OSTEP/ 20 hours
+Exercises 30 hours
+Read: How Browsers Work http://www.html5rocks.com/…/tuto…/internals/howbrowserswork/ 20 Hours
+*Tutorial: building a browser rendering engine https://limpet.net/mbrubeck/…/08/08/toy-layout-engine-1.html 100 hours
+- Learn Rust?
+- [ ] Networking
+Total Hours: 60 Hours
+Reading: Computer Networking: A Top-Down Approach 30 Hours
+Examples WIreshark-Lab http://www-net.cs.umass.edu/wireshark-labs/ 30 hours
+- [ ] Databases
+Total Hours
+Reading: Transaction Processing: Concepts and Techniques
+Reading: Data and Reality: A Timeless Perspective on Perceiving and Managing Information in Our Imprecise World
+Reading: Architecture of a Database System
+software: Leveldb by Jeff Dean
+- [ ] Mathematics
+-Big O Notation
+-Discrete Mathematics
+-Calculus 1 & 2
+
+Front End
+
+Homework for my front-end team:
+1. Official React Tutorial (http://bit.ly/2efaQQK)
+2. Official Redux Docs (http://bit.ly/2eCvrBe)
+3. Redux Video Lectures (http://bit.ly/2eCwgcX)
+4. React Video Lectures (http://bit.ly/2effjCK)
+5. Functional Programming: the Failure of State (http://bit.ly/2effeiE)
+6. JavaScript Koans (http://bit.ly/2eCxxkk)
+7. React Koans (http://bit.ly/2efcthe)
+8. ES6 Koans (http://bit.ly/2eCxdlM)
+9. You Might Not Need Redux (http://bit.ly/2efbTjs)
+10. ReduxSaga Tutorial (http://bit.ly/2effeza)
+11. Why and How to Bind “This” (http://bit.ly/2efbT2W)
