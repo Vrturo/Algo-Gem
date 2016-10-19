36744fd388994248c240ef3605b688798aeb2199
Arturo Perez <VrturoPerez@gmail.com>
2016-10-18 17:09:18 -0700

    add new interview file

---
+++ Algorithms/Interviews/snapChatFirstTechnical.js
@@ -0,0 +1,27 @@
+Snapchat First Technical Interview. Front End Engineer Role
+
+10 min Intro
+20 min Resume Project Talk
+  - Asked for detailed information on (Stack, challenges, decisions, etc..)
+30 min on algorithm
+
+// jQuery selector
+// $(selector) => list[elements]
+// selector:
+// .class
+// .class1.class2
+// .class1 .class2 .class3.class4
+// $('.class1.class2.class3') => returns elements
+
+// Given a srtring (selector), Create a function that implements jQuery's hierarchy class selector
+
+// dom: <elem class1 ><... > <elem class2>  <elem class3>     < >  <... ></elem>
+// var selector = '.class1.class2.class3'
+
+// Input: String
+// Output: list of elments
+
+
+var $ = function(selector) {
+        // Enter code here
+};
