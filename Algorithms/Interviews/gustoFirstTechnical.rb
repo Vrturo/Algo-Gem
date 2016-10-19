9d5fac11f2f183ce0f42c2f1540c26ff08d5de88
Arturo Perez <VrturoPerez@gmail.com>
2016-10-18 17:08:56 -0700

    add interview file

---
+++ Algorithms/Interviews/gustoFirstTechnical.rb
@@ -0,0 +1,22 @@
+# Gusto 1st Technical Interview. Full Stack Engineer Role
+#
+#20 min Intro
+#40 min on algorithm
+
+# Get Nth Leaf
+
+# Let's say we have the following tree:
+
+#           A
+#       B        C
+#     D E F    G   H
+#             J K
+# Our goal is to write a method get_nth_leaf that finds the nth leaf in the tree when performing an in-order depth-first traversal.
+
+# def get_nth_leaf(num)
+
+# end
+
+# get_nth_leaf(0) - D
+# get_nth_leaf(3) - J
+# get_nth_leaf(5) - H
