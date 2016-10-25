02274bdbb3e82b1c394c765349a7bd415987383e
Arturo Perez <VrturoPerez@gmail.com>
2016-10-25 15:24:29 -0700

    add description

--- Algorithms/JS/trees/buildBinarySearchTree.js
+++ Algorithms/JS/trees/buildBinarySearchTree.js
@@ -1,1 +1,20 @@
-buildBinarySearchTree.js
+/*
+ *  1. Write a function that takes in an array of integers and performs the insert method on each
+ *     item of the array in order.
+ *
+ *  Input: Array
+ *  Output: Binary Search Tree
+ *
+ *  Example: [4, 2, 5, 1, 3, 7, 6, 8]
+ *  Output this binary search tree:
+ *
+ *              4
+ *            /   \
+ *          2       5
+ *        /   \       \
+ *      1       3       7
+ *                    /   \
+ *                  6      8
+ */
+
+
