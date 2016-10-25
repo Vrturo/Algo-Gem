661a775e3987fa3315521c91581d7d617e1b85d3
Arturo Perez <VrturoPerez@gmail.com>
2016-10-24 19:35:45 -0700

    ad solution

--- Algorithms/JS/trees/sumOfLeftLeaves.js
+++ Algorithms/JS/trees/sumOfLeftLeaves.js
@@ -21,3 +21,17 @@
  * @param {TreeNode} root
  * @return {number}
  */
+
+var sumOfLeftLeaves = function(root) {
+    if( !root ) return 0;
+    var sum = 0;
+    if( root.left !== null ) {
+        if( root.left.left === null && root.left.right === null ) {
+            sum += root.left.val;
+        } else {
+            sum += sumOfLeftLeaves(root.left);
+        }
+    }
+    if( root.right !== null) sum += sumOfLeftLeaves(root.right );
+    return sum;
+};
