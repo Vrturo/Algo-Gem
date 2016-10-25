51942ce3a34e234fe81c1778306c9bba27a983e6
Arturo Perez <VrturoPerez@gmail.com>
2016-10-24 19:35:24 -0700

    ad data structure, methods, and params

--- Algorithms/JS/trees/sumOfLeftLeaves.js
+++ Algorithms/JS/trees/sumOfLeftLeaves.js
@@ -9,3 +9,15 @@
 //    15   7
 
 // There are two left leaves in the binary tree, with values 9 and 15 respectively. Return 24.
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
