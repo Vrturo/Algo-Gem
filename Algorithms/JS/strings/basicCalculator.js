ae6e81173f76153166fd3b80162dc8b2d4f4fb9a
Arturo Perez <VrturoPerez@gmail.com>
2016-10-28 14:07:50 -0700

    add solution

--- Algorithms/JS/strings/basicCalculator.js
+++ Algorithms/JS/strings/basicCalculator.js
@@ -19,3 +19,37 @@
  */
 
 
+var calculate = function(s) {
+    var result = 0,
+        num = 0,
+        sign = 1,
+        stack = [];
+
+    for( var i = 0; i < s.length; i++ ){
+        if( Number(s[i]) || s[i] === '0' ){
+            num = JSON.stringify(num)+s[i];
+            num = Number(num);
+        }else if( s[i] === '+' ){
+            result += sign * num;
+            num = 0;
+            sign = 1;
+        }else if( s[i] === '-' ){
+            result += sign * num;
+            num = 0;
+            sign = -1;
+        }else if( s[i] === '(' ){
+            stack.push(result);
+            stack.push(sign);
+            sign = 1;
+            result = 0;
+        }else if( s[i] == ')' ){
+            result += sign * num;
+            num = 0;
+            result *= stack.pop();
+            result += stack.pop();
+        }
+    }
+
+    if( num !== 0 ) result += sign * num;
+    return result;
+};
