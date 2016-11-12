4b8a2442b6c8681e3c31f383972c91b39038e028
Arturo Perez <VrturoPerez@gmail.com>
2016-11-11 19:10:30 -0800

    add description

---
+++ Algorithms/JS/graphs/BreadthFirstTraversal.js
@@ -0,0 +1,23 @@
+/*
+ *     For the example graph below, what is the expected output if we printed each
+ *     vertex value from vertex A outwards using:
+ *
+ *     a. BREADTH FIRST traversal?
+ *
+ *     NOTE: Assume the order of edges will be alphabetical. E.g., Vertex D has edges to
+ *           5 vertices in the following order: B, C, E, F, G
+ *
+ *     NOTE: The traversal should take care of redundancy and not print the same vertex
+ *           value twice.
+ *
+ *     Example Graph:
+ *
+ *              B     E
+ *            /   \  /
+ *          A       D  --- F
+ *            \   /   \   /
+ *              C       G
+ */
+
+
+
