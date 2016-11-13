345a7987327d2b1bb8603d300427d4fcdc9241af
Arturo Perez <VrturoPerez@gmail.com>
2016-11-13 14:54:53 -0800

    add solution

--- Algorithms/JS/arrays/findAllDuplicates.js
+++ Algorithms/JS/arrays/findAllDuplicates.js
@@ -17,3 +17,14 @@
  */
 
 
+var findDuplicates = function(nums) {
+    var result = [];
+    for (var i = 0; i < nums.length; ++i) {
+        var index = Math.abs(nums[i])-1;
+        if (nums[index] < 0){
+            result.push(Math.abs(index+1));
+        }
+        nums[index] = -nums[index];
+    }
+    return result;
+};
