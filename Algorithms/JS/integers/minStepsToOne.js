16aa71b3814aeaf1bfa1648ba444e0f991ea1a01
Arturo Perez <VrturoPerez@gmail.com>
2016-11-13 15:29:43 -0800

    add desc

--- Algorithms/JS/integers/minStepsToOne.js
+++ Algorithms/JS/integers/minStepsToOne.js
@@ -1,1 +1,13 @@
-minStepsToOne.js
+/*
+ *    Minimum Steps to One
+ *
+ *     Given a positive integer, you can perform any combination of these 3 steps:
+ *      1.) Subtract 1 from it.
+ *      2.) If its divisible by 2, divide by 2.
+ *      3.) If its divisible by 3, divide by 3.
+ *
+ *     Find the minimum number of steps that it takes get from N to 1
+ *
+ *  Input: Positive Integer N
+ *  Output:Integer
+ */
