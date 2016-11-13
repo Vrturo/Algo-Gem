4894c1981a4679c9e0ea47035dd279aa04959366
Arturo Perez <VrturoPerez@gmail.com>
2016-11-12 20:55:31 -0800

    add desc

--- Algorithms/JS/graphs/depthFirstTraversal.js
+++ Algorithms/JS/graphs/depthFirstTraversal.js
@@ -1,1 +1,12 @@
-depthFirstTraversal.js
+/*
+ *      Using a stack and while loop, write a function that takes in a graph and
+ *      outputs an array of values from vertex A outwards ordered by DEPTH
+ *      FIRST traversal.
+ *
+ *      Input: Graph
+ *      Output: Array
+ *
+ *  NOTE: You may use an array or linked list for your stack.
+ *
+ *  HINT: Use a hash table to handle redundancy
+ */
