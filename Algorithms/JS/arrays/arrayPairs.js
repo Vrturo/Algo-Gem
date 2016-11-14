fcba0cf700f3cfcd5eb6f2dd20b9f3ebe899b1b8
Arturo Perez <VrturoPerez@gmail.com>
2016-11-13 17:14:27 -0800

    add description

--- Algorithms/JS/arrays/arrayPairs.js
+++ Algorithms/JS/arrays/arrayPairs.js
@@ -1,1 +1,13 @@
-arrayPairs.js
+/**
+ * Given an array of integers, create an array of two-item arrays using Helper Method Recursion
+ *
+ * Input:   Array of Integers
+ * Output:  Array of two-item Arrays
+ *
+ * Example: arrayPairs([1, 2, 3, 4, 5, 6])  =>    [[1,2], [3,4], [5,6]]
+ * Example: arrayPairs([1, 2, 3, 4, 5])     =>    [[1,2], [3,4], [5, undefined]]
+ **/
+
+function arrayPairs(arr){
+
+}
