fc91e4590702cdf9658ba1dfdd006e71a41390ca
Arturo Perez <VrturoPerez@gmail.com>
2016-11-13 19:06:30 -0800

    add solution

--- Algorithms/JS/arrays/flattenArray.js
+++ Algorithms/JS/arrays/flattenArray.js
@@ -8,5 +8,20 @@
  **/
 
 function flatten(arr){
+  var result = [];
 
+  function helper(currObj, res){
+    for( var i = 0; i<currObj.length; i++ ){
+      if( typeof currObj[i] === 'number'){
+        res.push( currObj[i] );
+      } else {
+        helper( currObj[i], res )
+      }
+    }
+  }
+  helper(arr, result);
+  return result;
 }
+
+var ex = [1, [2, 3, [4]], 5, [[6]]];
+console.log( flatten(ex) );
