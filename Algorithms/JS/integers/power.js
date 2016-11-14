53d27b80a36ce8c07e134515823760ffcbb5fd98
Arturo Perez <VrturoPerez@gmail.com>
2016-11-13 19:59:20 -0800

    add description

--- Algorithms/JS/integers/power.js
+++ Algorithms/JS/integers/power.js
@@ -1,1 +1,13 @@
-power.js
+/**
+ *     Given a base and an exponent, create a function to find the power using
+ *     Helper Method Recursion
+ *
+ * Input:   Two Integers, base and exponent
+ * Output:  Integer
+ *
+ * Example: power(3, 4) => 81
+ **/
+
+function power(a, b){
+
+}
