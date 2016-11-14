78f4d4d315711df24884967f84a796cf947b8c7f
Arturo Perez <VrturoPerez@gmail.com>
2016-11-13 18:58:48 -0800

    add desc

--- Algorithms/JS/arrays/flattenArray.js
+++ Algorithms/JS/arrays/flattenArray.js
@@ -1,1 +1,12 @@
-flattenArray.js
+/**
+ * Flatten a nested array using Helper Method of Recursion
+ *
+ * Input:   Array of Integers and Arrays
+ * Output:  Array of Integers
+ *
+ * Example: flatten([1, [2, 3, [4]], 5, [[6]]]) => [1, 2, 3, 4, 5, 6]
+ **/
+
+function flatten(arr){
+
+}
