2f201fd1ef716d784b958d0ced0da0bd22a3067f
Arturo Perez <VrturoPerez@gmail.com>
2016-11-13 19:09:32 -0800

    add steps

--- Algorithms/JS/arrays/flattenArray.js
+++ Algorithms/JS/arrays/flattenArray.js
@@ -7,6 +7,19 @@
  * Example: flatten([1, [2, 3, [4]], 5, [[6]]]) => [1, 2, 3, 4, 5, 6]
  **/
 
+
+// creata variable result to return as our flattened array at the end
+// create helper function that takes in:
+  // the current Obj we're in
+  // our result array to push our values
+  // our function loops through our current object
+    // if the current item in the current object is a number
+      // push the item into the result aray
+    // else call our helper onto our nested object
+
+// call our function on our outermost array
+
+
 function flatten(arr){
   var result = [];
 
