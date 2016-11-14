1426825abea1cba79b37cb23540580ac59fdf672
Arturo Perez <VrturoPerez@gmail.com>
2016-11-13 18:09:38 -0800

    add solution

--- Algorithms/JS/arrays/arrayPairs.js
+++ Algorithms/JS/arrays/arrayPairs.js
@@ -9,5 +9,25 @@
  **/
 
 function arrayPairs(arr){
+  var result = [];
 
+  function helper(i, buildUp, res){
+    if( i > arr.length ){
+      return;
+    } else {
+      if( i % 2 === 0 ){
+        buildUp.push(arr[i]);
+        helper(i+1, buildUp, res);
+      } else {
+        buildUp.push(arr[i])
+        res.push(buildUp);
+        helper(i+1, [], res)
+      }
+    }
+  }
+  helper( 0, [], result );
+  return result;
 }
+
+var ex = [1, 2, 3, 4, 5, 6, 7];
+console.log(arrayPairs(ex))
