3b05eda42f1b12b8b9f0b652dd6e10462c6d9eea
Arturo Perez <VrturoPerez@gmail.com>
2016-11-14 21:00:53 -0800

    add solution

--- Algorithms/JS/integers/minStepsToOne.js
+++ Algorithms/JS/integers/minStepsToOne.js
@@ -13,6 +13,13 @@
  */
 
 
+// solved using pure recurion w/ side effects
+// set base case if n = 1, return amount of steps we took
+
+// if n is divisible by 3 divide and increase steps by 1
+// if n is divisible by 2 divide and increase steps by 1
+// if n isnt divisible by 2 or 3 subtract by 1
+
 
 function minStepsToOne(n, steps=0){
   if( n === 1 ) return steps;
