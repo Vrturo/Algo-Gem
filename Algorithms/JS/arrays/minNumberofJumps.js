24f58a405c2301a763271b34c6bd9e1e7d3fb126
Arturo Perez <VrturoPerez@gmail.com>
2016-11-14 21:08:05 -0800

    add desc

--- Algorithms/JS/arrays/minNumberofJumps.js
+++ Algorithms/JS/arrays/minNumberofJumps.js
@@ -1,1 +1,16 @@
-minNumberofJumps.js
+/*
+ *     Given an array of non-negative integers, starting at the first index of the array.
+ *     Each element in the array represents your maximum jump length at that position.
+ *     What is the minimum number of jumps to reach the last index?
+ *
+ *  Input: Array of non-negative integers
+ *  Output: Integer
+ *
+ *  Example: jump([2,3,1,1,4])
+ *  Result: 2 because traveling from 2 -> 3 -> 4 uses 2 jumps
+ */
+
+
+function jumpCountToLast(n){
+  // YOUR WORK HERE
+}
