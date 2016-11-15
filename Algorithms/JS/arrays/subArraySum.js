785485948840c3cb18f18b607b4f527f4b8740a4
Arturo Perez <VrturoPerez@gmail.com>
2016-11-15 11:35:58 -0800

    add desc

--- Algorithms/JS/arrays/subArraySum.js
+++ Algorithms/JS/arrays/subArraySum.js
@@ -1,1 +1,13 @@
-subArraySum.js
+// Given an unsorted array of nonnegative integers, find a continous subarray which adds to a given number.
+
+// Examples:
+
+// Input: arr[] = {1, 4, 20, 3, 10, 5}, sum = 33
+// Ouptut: Sum found between indexes 2 and 4
+
+// Input: arr[] = {1, 4, 0, 0, 3, 10, 5}, sum = 7
+// Ouptut: Sum found between indexes 1 and 4
+
+// Input: arr[] = {1, 4}, sum = 0
+// Output: No subarray found
+// There may be more than one subarrays with sum as the given sum. The following solutions print first such subarray.
