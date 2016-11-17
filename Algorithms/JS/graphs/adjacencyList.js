9647b6f1c32392d7a36d5b222748ff748fa65073
Arturo Perez <VrturoPerez@gmail.com>
2016-11-17 09:51:51 -0800

    add desc

--- Algorithms/JS/graphs/adjacencyList.js
+++ Algorithms/JS/graphs/adjacencyList.js
@@ -1,1 +1,12 @@
-adjacencyList.js
+// Build a graph using an adjacency list.
+// No vertex class
+// Should have a storage property that holds vertex id along with all the edges.
+
+// Methods:
+// addVertex
+// removeVertex
+// addEdge
+// removeEdge
+// neighbors(vertex) -> return the array of neighbors for a vertex
+
+
