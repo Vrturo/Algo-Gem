803c92fa383c0472be26ca37200f812aa5f4f369
Arturo Perez <VrturoPerez@gmail.com>
2016-11-17 10:16:37 -0800

    add neighbors method

--- Algorithms/JS/graphs/adjacencyList.js
+++ Algorithms/JS/graphs/adjacencyList.js
@@ -54,6 +54,26 @@ class Graph{
     this.totalEdges--;
   }
 
+  neighbors(val){
+    return this.vertices[val]
+  }
+
 }
 
-
+var graph = new Graph;
+var myVertices = ['A','B','C','D','E','F','G','H','I']; // 9
+for( var i=0; i<myVertices.length; i++ ){
+  graph.addVertex( myVertices[i] );
+}
+graph.addEdge('A', 'B');
+graph.addEdge('A', 'C');
+graph.addEdge('A', 'D');
+graph.addEdge('C', 'D');
+graph.addEdge('C', 'G');
+graph.addEdge('D', 'G');
+graph.addEdge('D', 'H');
+graph.addEdge('B', 'E');
+graph.addEdge('B', 'F');
+graph.addEdge('E', 'I');
+// console.log(graph)
+console.log(graph.neighbors('A'))
