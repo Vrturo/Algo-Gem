b4d505338127962296fcb709a7e1a0cc228d2354
Arturo Perez <VrturoPerez@gmail.com>
2016-12-04 15:40:33 -0800

    add example

--- Concepts/Javascript/closures.js
+++ Concepts/Javascript/closures.js
@@ -126,19 +126,55 @@ mjID.getID(); // 567: It returns the updated celebrityId variable 
 }
 ​
 ​var actionCelebs = [{
-                        name:"Stallone", id:0
+                        name: "Stallone", id:0
                       }, {
-                        name:"Cruise", id:0
+                        name: "Cruise", id:0
                       }, {
-                        name:"Willis", id:0
+                        name: "Willis", id:0
                     }];
-​var createIdForActionCelebs = celebrityIDCreator (actionCelebs);
+​var createIdForActionCelebs = celebrityIDCreator(actionCelebs);
 ​var stalloneID = createIdForActionCelebs [0];  
 console.log( stalloneID.id() ); // 103
 
+// In the preceding example, by the time the anonymous functions are called, the value of i is 3
+// (the length of the array and then it increments).
+// The number 3 was added to the uniqueID to create 103 for ALL the celebritiesID.
+// So every position in the returned array get id = 103, instead of the intended 100, 101, 102.
 
+// The reason this happened was because, as we have discussed in the previous example,
+// the closure (the anonymous function in this example) has access to the outer function’s variables by reference, not by value.
+// So just as the previous example showed that we can access the updated variable with the closure,
+// this example similarly accessed the i variable when it was changed,
+// since the outer function runs the entire for loop and returns the last value of i, which is 103.
 
+// To fix this side effect (bug) in closures,
+// you can use an Immediately Invoked Function Expression (IIFE), such as the following:
 
+function celebrityIDCreator (theCelebrities) {
+    var i,
+        uniqueID = 100;
+    for (i = 0; i < theCelebrities.length; i++) {
+        theCelebrities[i]["id"] = function (j)  { // the j parametric variable is the i passed in on invocation of this IIFE​
+            return function () {
+                return uniqueID + j; // each iteration of the for loop passes the current value of i into this IIFE and it saves the correct value to the array​
+            } () // BY adding () at the end of this function, we are executing it immediately and returning just the value of uniqueID + j, instead of returning a function.​
+        } (i); // immediately invoke the function passing the i variable as a parameter​
+    }
+​
+    return theCelebrities;
+}
+​
+​var actionCelebs = [{
+                        name: "Stallone", id:0
+                      }, {
+                        name: "Cruise", id:0
+                      }, {
+                        name: "Willis", id:0
+                    }];
+​var createIdForActionCelebs = celebrityIDCreator (actionCelebs);
+​var stalloneID = createIdForActionCelebs [0];
+ console.log(stalloneID.id); // 100​
+​var cruiseID = createIdForActionCelebs [1]; console.log(cruiseID.id); // 101
 
 
 
