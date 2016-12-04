b3e5a4d9db4bc6be378507c7c5875acdfab22326
Arturo Perez <VrturoPerez@gmail.com>
2016-12-03 17:42:18 -0800

    add params

--- Algorithms/JS/arrays/sortTransformedArray.js
+++ Algorithms/JS/arrays/sortTransformedArray.js
@@ -1,15 +1,26 @@
-Given a sorted array of integers nums and integer values a, b and c.
-Apply a function of the form f(x) = ax2 + bx + c to each element x in the array.
+// Given a sorted array of integers nums and integer values a, b and c.
+// Apply a function of the form f(x) = ax2 + bx + c to each element x in the array.
 
-The returned array must be in sorted order.
+// The returned array must be in sorted order.
 
-Expected time complexity: O(n)
+// Expected time complexity: O(n)
 
-Example:
-nums = [-4, -2, 2, 4], a = 1, b = 3, c = 5,
+// Example:
+// nums = [-4, -2, 2, 4], a = 1, b = 3, c = 5,
 
-Result: [3, 9, 15, 33]
+// Result: [3, 9, 15, 33]
 
-nums = [-4, -2, 2, 4], a = -1, b = 3, c = 5
+// nums = [-4, -2, 2, 4], a = -1, b = 3, c = 5
 
-Result: [-23, -5, 1, 7]
+// Result: [-23, -5, 1, 7]
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} a
+ * @param {number} b
+ * @param {number} c
+ * @return {number[]}
+ */
+
+
