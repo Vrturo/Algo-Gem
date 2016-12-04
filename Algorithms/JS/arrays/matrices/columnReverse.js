d30b705bca19951cb6070b5a677558a4db03944e
Arturo Perez <VrturoPerez@gmail.com>
2016-12-03 22:16:08 -0800

    add desc

--- Algorithms/JS/arrays/matrices/columnReverse.js
+++ Algorithms/JS/arrays/matrices/columnReverse.js
@@ -1,1 +1,8 @@
-columnReverse.js
+// Given a 2d array, output the reverse the order of each column.
+
+// Input:
+// Array
+// [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]
+
+// Output:
+// 4 3 2 1 8 7 6 5 12 11 10 9
