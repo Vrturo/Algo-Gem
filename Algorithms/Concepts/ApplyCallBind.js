acd8fdd13e76c19e87a031d542a42b1c64f67fd4
Arturo Perez <VrturoPerez@gmail.com>
2016-12-04 14:05:03 -0800

    add description

---
+++ Algorithms/Concepts/ApplyCallBind.js
@@ -0,0 +1,8 @@
+// Apply vs. Call vs. Bind in JavaScript
+
+
+// Call invokes the function and allows you to pass in arguments one by one.
+// Apply invokes the function and allows you to pass in arguments as an array.
+// Bind returns a new function, allowing you to pass in a this array and any number of arguments.
+
+
