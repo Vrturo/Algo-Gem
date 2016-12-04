f2ec712e616b6dc19b087f43ccb1954d8fa628db
Arturo Perez <VrturoPerez@gmail.com>
2016-12-04 14:05:12 -0800

    add examples

--- Algorithms/Concepts/ApplyCallBind.js
+++ Algorithms/Concepts/ApplyCallBind.js
@@ -6,3 +6,50 @@
 // Bind returns a new function, allowing you to pass in a this array and any number of arguments.
 
 
+// Apply vs. Call vs. Bind Examples
+
+// ------------------------------------------------------
+
+// - Call
+
+var person1 = {firstName: 'Jon', lastName: 'Kuperman'};
+var person2 = {firstName: 'Kelly', lastName: 'King'};
+
+function say(greeting) {
+    console.log(greeting + ' ' + this.firstName + ' ' + this.lastName);
+}
+
+say.call(person1, 'Hello'); // Hello Jon Kuperman
+say.call(person2, 'Hello'); // Hello Kelly King
+
+ //--------------------------------------------------------
+
+// - Apply
+
+var person1 = {firstName: 'Jon', lastName: 'Kuperman'};
+var person2 = {firstName: 'Kelly', lastName: 'King'};
+
+function say(greeting) {
+    console.log(greeting + ' ' + this.firstName + ' ' + this.lastName);
+}
+
+say.apply(person1, ['Hello']); // Hello Jon Kuperman
+say.apply(person2, ['Hello']); // Hello Kelly King
+
+ //--------------------------------------------------------
+
+// - Bind
+
+
+var person1 = {firstName: 'Jon', lastName: 'Kuperman'};
+var person2 = {firstName: 'Kelly', lastName: 'King'};
+
+function say() {
+    console.log('Hello ' + this.firstName + ' ' + this.lastName);
+}
+
+var sayHelloJon = say.bind(person1);
+var sayHelloKelly = say.bind(person2);
+
+sayHelloJon(); // Hello Jon Kuperman
+sayHelloKelly(); // Hello Kelly King
