5337a9d64a0e59f0b0ccb0787d77db6b29760599
Arturo Perez <VrturoPerez@gmail.com>
2016-12-04 14:59:19 -0800

    add example

--- Concepts/Javascript/closures.js
+++ Concepts/Javascript/closures.js
@@ -74,7 +74,35 @@ function celebrityName (firstName) {
 ​// Yet, the closure still has access to the outer function's variables and parameter​
 mjName ("Jackson"); // This celebrity is Michael Jackson 
 
+// -------------------------------------------------------------------------------------
 
+// Closures store references to the outer function’s variables;
+
+//     they do not store the actual value. 
+//     Closures get more interesting when the value of the outer function’s variable changes before the closure is called.
+
+function celebrityID () {
+    var celebrityID = 999;
+    // We are returning an object with some inner functions​
+    // All the inner functions have access to the outer function's variables​
+    return {
+        getID: function ()  {
+            // This inner function will return the UPDATED celebrityID variable​
+            // It will return the current value of celebrityID, even after the changeTheID function changes it​
+          return celebrityID;
+        },
+        setID: function (theNewID)  {
+            // This inner function will change the outer function's variable anytime​
+            celebrityID = theNewID;
+        }
+    }
+​
+}
+​
+​var mjID = celebrityID (); // At this juncture, the celebrityID outer function has returned.​
+mjID.getID(); // 999​
+mjID.setID(567); // Changes the outer function's variable​
+mjID.getID(); // 567: It returns the updated celebrityId variable 
 
 
 
