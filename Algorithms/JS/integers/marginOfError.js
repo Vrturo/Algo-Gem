e0d3320a75d9c2bcb743ab63d558609ec57565b9
Arturo Perez <VrturoPerez@gmail.com>
2016-12-04 18:02:40 -0800

    add desc

--- Algorithms/JS/integers/marginOfError.js
+++ Algorithms/JS/integers/marginOfError.js
@@ -1,1 +1,20 @@
-marginOfError.js
+// Given the function firstDegree, find the margin of error and return the values a and c.
+
+function firstDegree(x) {
+  //f(x) = ax + c
+  var data = [];
+
+  var a = Math.floor(Math.random()*100),
+      c = Math.floor(Math.random()*100),
+      funcVal = 0;
+
+  for(var i = 0; i<200; i++) { //introduce error to the equation
+    x = i;
+    funcVal = a*x + c;
+    funcVal += Math.floor(Math.random()*10);
+    data.push(funcVal);
+  }
+
+  var answer = [a,c];
+  return [data,answer];
+}
