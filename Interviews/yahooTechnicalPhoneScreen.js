bafb35e7ef003ff14e09278407f8aa9d6cc80c3d
Arturo Perez <VrturoPerez@gmail.com>
2016-12-07 16:14:33 -0800

    fill  file

--- Interviews/yahooTechnicalPhoneScreen.js
+++ Interviews/yahooTechnicalPhoneScreen.js
@@ -1,1 +1,10 @@
-yahooTechnicalPhoneScreen.js
+Yahoo Technical Phone Screen Interview. Front End Engineer Role.
+
+10 min Intro
+10 min Resume Project Talk (technologies used and why)
+10 min on random questions
+    What happens when you type yahoo.com in a browser?
+    What's the difference between a GET and a POST request?
+15 min on algorithm questions
+    How would you write a function that takes out duplicates in an array? Time and Space Complexity?
+    How would you write a function that checks if two strings are Anagrams? Time and Space Complexity?
