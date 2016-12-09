40a404b63363ddb9c6e6db4e16f1bfc1365d9b4b
Arturo Perez <VrturoPerez@gmail.com>
2016-12-08 17:09:33 -0800

    add interview

--- Interviews/ebayFinalTechnical.js
+++ Interviews/ebayFinalTechnical.js
@@ -1,1 +1,36 @@
-ebayFinalTechnical.js
+// Ebay Technical Interview. Senior Front End Engineer Role - Shopping experience team
+
+// 10 min Intro (Project work and Technologies)
+// 40 min HTML challenge
+// 10 Q&A
+
+Assume you have a CommonJS module with the following interface that you can use
+to retrieve items from a webservice. n is the number of items to retrieve. The callback
+function has 1 parameter which is the array of items returned from the web service.
+
+var itmServ = require('item-service');
+itmServ.getItems(n, callback);
+
+The array consists of items with the following structure:
+[
+    {
+        seller: 'seller1',
+        image: 'http://url/to/image',
+        title: 'Really great item',
+        price: '$3.00',
+        itemId: 12345,
+        detailURL: 'http://url/to/view/item/details'
+    },
+    ...
+]
+
+Using the above item-service module, write the necessary javascript/css/html to layout the items on a
+page meeting the following requirements.
+
+1. The items should be arranged on the page as 4 rows and 4 columns.
+2. No 2 adjacent items in a row should have the same seller.
+3. Each item should be formatted as follows:
+   A div 200x200 pixels with the image as a background.
+   The title should appear centered at the top.
+   The price should appear centered at the bottom.
+4. Clicking anywhere in the div should take you to the detailURL page.
