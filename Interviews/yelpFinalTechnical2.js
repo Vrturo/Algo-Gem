45be126c36dbbad2deb1100fa900cf0208be5d13
Arturo Perez <VrturoPerez@gmail.com>
2016-12-10 23:52:24 -0800

    add algo but js fn still missing

--- Interviews/yelpFinalTechnical2.js
+++ Interviews/yelpFinalTechnical2.js
@@ -1,1 +1,53 @@
-yelpFinalTechnical2.js
+// Yelp Final Technical Interview. Full Stack Javascript Engineer Role.
+
+// 10 min Intro
+// 15 min Resume Project Talk
+//   - Asked for detailed information on (Stack, challenges, decisions, etc..)
+// 10 min on JS question
+// 15 min on lazy BFS algorithm
+
+// -------------------------------------------------------
+
+// JS question
+
+// What does this return?? Why?
+
+var x = [];
+
+function (){
+
+
+}
+
+//--------------------------------------------------------------
+
+
+// Lazy BFS algorithm
+
+
+    foo -------
+   /   \       |
+  bar   baz    quux
+   |            |
+   x            y
+
+
+foo = {
+    children: [bar, baz, quux]
+}
+
+x = {
+    children: []
+}
+
+
+function createWalker(root) {
+   // write code here
+}
+
+
+var walker = createWalker(foo);
+walker(); // foo
+walker(); // bar
+...
+walker(); // y
