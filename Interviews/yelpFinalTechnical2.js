1bac99650b3c5ca07e8c999b7ea6aff09f072969
Arturo Perez <VrturoPerez@gmail.com>
2016-12-14 09:53:30 -0800

    finish interview

--- Interviews/yelpFinalTechnical2.js
+++ Interviews/yelpFinalTechnical2.js
@@ -14,11 +14,18 @@
 
 var x = [];
 
-function (){
 
-
+var count = 0;
+  for( var i = 0; i<10; i++ ){
+    function(){
+      return i
+    }
+  }
 }
 
+for(var j = 0; j<x.length; j++){
+  console.log(x[j]())
+}
 //--------------------------------------------------------------
 
 
