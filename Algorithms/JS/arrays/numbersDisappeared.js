064d25ca9c50b48b6e1b6c50e395387095b8a97d
Arturo Perez <VrturoPerez@gmail.com>
2016-12-26 15:17:30 -0800

    add description

--- Algorithms/JS/arrays/numbersDisappeared.js
+++ Algorithms/JS/arrays/numbersDisappeared.js
@@ -1,1 +1,17 @@
-numbersDisappeared.js
+Given an array of integers where 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+
+Find all the elements of [1, n] inclusive that do not appear in this array.
+
+Could you do it without extra space and in O(n) runtime? You may assume the returned list does not count as extra space.
+
+Example:
+
+Input:
+1. [4,3,2,7,8,2,3,1]
+2. [2, 2]
+3. [1, 1]
+
+Output:
+1. [5,6]
+2. [1]
+3. [2]
