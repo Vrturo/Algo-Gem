20b79382822ddd329560a2370a44ddf98724691e
Arturo Perez <VrturoPerez@gmail.com>
2016-12-29 13:39:40 -0800

    add first solution

--- Algorithms/JS/arrays/matrices/islandPerimeter.js
+++ Algorithms/JS/arrays/matrices/islandPerimeter.js
@@ -21,3 +21,16 @@
  */
 
 
+var islandPerimeter = function(grid) {
+    var islands = 0, neighbours = 0;
+    for(var i=0; i<grid.length; i++){
+        for(var j=0; j<grid[i].length; j++){
+            if (grid[i][j] == 1) {
+                    islands++; // count islands
+                    if (i < grid.length - 1 && grid[i + 1][j] == 1) neighbours++; // count down neighbours
+                    if (j < grid[i].length - 1 && grid[i][j + 1] == 1) neighbours++; // count right neighbours
+                }
+        }
+    }
+    return result;
+};
