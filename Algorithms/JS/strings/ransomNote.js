b7292f56308c837d5793a3e63b2d8a727223b2f5
Arturo Perez <VrturoPerez@gmail.com>
2016-12-29 16:06:43 -0800

    add fist solution

--- Algorithms/JS/strings/ransomNote.js
+++ Algorithms/JS/strings/ransomNote.js
@@ -21,3 +21,20 @@
  */
 
 
+var canConstruct = function(ransomNote, magazine) {
+    var map = {};
+
+    for(var i=0; i<magazine.length; i++){
+        if (map[magazine[i]]) {
+            map[magazine[i]]+=1;
+        } else {
+            map[magazine[i]]=1;
+        }
+    }
+    for(var j=0; j<ransomNote.length; j++){
+        if(!map[ransomNote[j]] || map[ransomNote[j]] < 1) return false;
+        if(map[ransomNote[j]]) map[ransomNote[j]] -=1;
+    }
+    return true;
+};
+
