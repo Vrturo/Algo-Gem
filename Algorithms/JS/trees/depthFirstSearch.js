c9768c6bba8aa9d89182fc847bc33094946801f2
Arturo Perez <VrturoPerez@gmail.com>
2017-01-04 18:34:17 -0800

    syntax

--- Algorithms/JS/trees/depthFirstSearch.js
+++ Algorithms/JS/trees/depthFirstSearch.js
@@ -1,7 +1,5 @@
 // How do you implement Depth First Search
 
-
-
 function dfs( node ){
   if( node ){ // if node exists, output value. Then call dfs on children if any
     console.log( node.value );
