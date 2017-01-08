cc6f71fa0b2099f6959271fc154d4c5968f3d474
Arturo Perez <VrturoPerez@gmail.com>
2017-01-08 12:55:06 -0800

    create new array file

---
+++ Algorithms/JS/arrays/maxNonNegativeSubArray.js
@@ -0,0 +1,1 @@
+maxNonNegativeSubArray.js
--- test.js
+++ test.js
@@ -1,13 +1,38 @@
-function foo(){
-  return {
-    bar: "hello"
+function maxset(A){
+        var result = [],
+            temp = [],
+            negCount = 0;
+        for( var i = 0; i < A.length; i++ ){
+            if( A[i] >= 0 ){
+                temp.push(A[i]);
+            } else {
+                result.push( temp );
+                temp = [];
+                negCount += 1
+            }
+        }
+        if( negCount === A.length ) return 0;
+        if( A[A.length -1] > 0 ) result.push(temp);
+        var maxSum = 0,
+            length = 0,
+            sum,
+            sub;
+        for( var j =0, sum = result[j[0]]; j< result.length; j++ ){
+          sum = result[j].reduce(function(a, b) { return a + b; }, 0);
+          if( sum > maxSum ){
+            maxSum = sum;
+            length = result[j].length;
+            sub = result[j];
+          }
+          if( sum === maxSum){
+            if( result[j].length > length ){
+              maxSum = sum;
+              length = result[j].length;
+              sub = result[j];
+            }
+          }
+        }
+        return sub;
   }
-}
 
-function fooTwo(){
-  return {
-    bar: "hello"
-  }
-}
-
-console.log( foo(), fooTwo())
+console.log( maxset([ 0, 0, -1, 0 ]) )
