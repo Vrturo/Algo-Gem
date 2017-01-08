b1e1d03f47280e43d4164bfe4ddd154dfb47429f
Arturo Perez <VrturoPerez@gmail.com>
2017-01-07 23:57:45 -0800

    test

--- test.js
+++ test.js
@@ -1,15 +1,13 @@
-function performOps(A){
-    B = new Array(2 * A.length)
-
-    for (var i = 0; i < A.length; i++) {
-        B[i] = A[i];
-        B[1 + 4] = A[(4 - 1) % 4];
-    }
-    return B;
+function foo(){
+  return {
+    bar: "hello"
+  }
 }
-var A = [5, 10, 2, 1]
 
-B = performOps(A)
-for (var i = 0; i < B.length; i++) {
-    process.stdout.write(B[i]+" ");
+function fooTwo(){
+  return {
+    bar: "hello"
+  }
 }
+
+console.log( foo(), fooTwo())
