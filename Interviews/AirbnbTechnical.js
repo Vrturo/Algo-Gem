23576a7d82e65bf10c3c9e88884cde22a84f1636
Arturo Perez <VrturoPerez@gmail.com>
2017-01-09 13:50:09 -0800

    re do challenge

--- Interviews/AirbnbTechnical.js
+++ Interviews/AirbnbTechnical.js
@@ -1,12 +1,27 @@
-var items = ["Fruit", "Fries", "Salad", "Wings", "Stick", "Plate"]
-var prices = [2.15, 2.75, 3.35, 3.55, 4.20, 5.80]
+
+// Imagine you're at a restaurant and you have an exact amount of money and ONLY want to spend that exact amount.
+// Nothing more, Nothing less. ONLY that EXACT amount.
+
+// Write a function that will return all the combinations of items that you can get with that exact amount.
+
+// youre given a number as a total variable and have access to the menu with the name and price.
+// you can arrange the given information in any data structure you'd like to access it
+// return all the total possible combinations in an array
+
+
+// items:  Fruit   Fries   Salad   Wings   Stick   Plate
+// prices:  2.15   2.75    3.35    3.55    4.20    5.80
+
+
+// ex.
+
+// total = 4.30
+
+// return ["Fruit, Fruit"]
 
 
 function appetize(total){
 
 }
 
-// return all possible combinations
 
-// ex. total = 4.30
-// return ["Fruit, Fruit"]
