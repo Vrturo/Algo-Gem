edf68c9b54fedf23b98e753d3833554631cda05b
Arturo Perez <VrturoPerez@gmail.com>
2017-01-09 16:54:05 -0800

    add structure

--- Algorithms/JS/trees/kthSmallestElementinaBST.js
+++ Algorithms/JS/trees/kthSmallestElementinaBST.js
@@ -8,3 +8,11 @@
 // Follow up:
 // What if the BST is modified (insert/delete operations) often and you need to find the kth smallest frequently?
 // How would you optimize the kthSmallest routine?
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
