57308287c1ec21d723614c0e8021517938f58ff4
Arturo Perez <VrturoPerez@gmail.com>
2017-01-10 16:31:39 -0800

    add solution

--- Algorithms/JS/arrays/kthSmallestElementinArray.js
+++ Algorithms/JS/arrays/kthSmallestElementinArray.js
@@ -14,3 +14,56 @@
  * @param {number} k
  * @return {number}
  */
+
+
+var findKthSmallest = function(nums, k, l=0, r=nums[0]) {
+    if (k > 0 && k <= r - l + 1){
+            // Partition the array around a random element and
+            // get position of pivot element in sorted array
+            var pos = randomPartition(nums, l, r);
+
+            // If position is same as k
+            if (pos-l == k-1) return nums[pos];
+
+            // If position is more, recur for left subarray
+            if (pos-l > k-1) return findkthLargest(nums, k, l, pos-1);
+
+            // Else recur for right subarray
+            return findkthLargestt(nums, k-pos+l-1, pos+1, r);
+        }
+
+
+    // Utility method to swap arr[i] and arr[j]
+    function swap(arr, i, j){
+        var temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    // Standard partition process of QuickSort().  It considers
+    // the last element as pivot and moves all smaller element
+    // to left of it and greater elements to right. This function
+    // is used by randomPartition()
+    function partition(arr, l, r){
+        var x = arr[r], i = l;
+        for (var j = l; j <= r - 1; j++){
+            if (arr[j] <= x){
+                swap(arr, i, j);
+                i++;
+            }
+        }
+        swap(arr, i, r);
+        return i;
+    }
+
+    // Picks a random pivot element between l and r and
+    // partitions arr[l..r] arount the randomly picked
+    // element using partition()
+    function randomPartition(arr, l, r){
+        var n = r-l+1;
+        var pivot = arr[0];
+        swap(arr, l + pivot, r);
+        return partition(arr, l, r);
+    }
+
+};
