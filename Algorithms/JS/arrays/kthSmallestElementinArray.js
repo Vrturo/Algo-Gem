82009800e61d529df4b5ff8c353b762e5f902219
Arturo Perez <VrturoPerez@gmail.com>
2017-01-10 16:30:40 -0800

    add desc

--- Algorithms/JS/arrays/kthSmallestElementinArray.js
+++ Algorithms/JS/arrays/kthSmallestElementinArray.js
@@ -1,1 +1,11 @@
-kthSmallestElementinArray.js
+// Kth Smallest Element in an Array
+
+// Find the kth smallest element in an unsorted array. Note that it is the kth smallest element in the sorted order,
+// not the kth distinct element.
+
+// For example,
+// Given [3,2,1,5,6,4] and k = 2, return 2.
+
+// Note:
+// You may assume k is always valid, 1 ≤ k ≤ array's length.
+
