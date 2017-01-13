0e0c44f689e13e0b8a8e74b3cc9f4df82f78a386
Arturo Perez <VrturoPerez@gmail.com>
2017-01-12 17:29:46 -0800

    remove solution

--- Algorithms/JS/integers/isPrimeNumber.js
+++ Algorithms/JS/integers/isPrimeNumber.js
@@ -23,25 +23,3 @@ console.log(isPrimeNumber(25))
 // --------------------------------------------------------------------------
 
 
-
-// function isPrimeNumber(n){
-//   if (n <= 1) { return false}
-
-//   divisor = 2;
-
-//   while (n > divisor){
-//     if (n % divisor === 0){
-//       return false
-//     } else { // the divisor is increased 1 at a time. after 3 i can increase by 2.
-//               // if a number is divisible by any even number, it will be divisible by 2.
-//         if (divisor > 2){
-//           divisor += 2
-//         }
-//         divisor ++
-//     }
-//   }
-//   return true;
-// }
-// console.log(isPrimeNumber(131))
-//
-
