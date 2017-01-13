56eb315f76271b1f9fc36dbabe9b0422a91e5805
Arturo Perez <VrturoPerez@gmail.com>
2017-01-12 20:43:37 -0800

    edit solution

--- Algorithms/JS/integers/isPrimeNumber.js
+++ Algorithms/JS/integers/isPrimeNumber.js
@@ -23,14 +23,10 @@ function isPrimeNumber(n){
 // --------------------------------------------------------------------------
 
 function isPrimeNumber(n){
-    if( n === ( 1 || 2) ){
-      return n === 2
-    } else {
-      for(var x = 2; x < n; x++){
-        if(n % x === 0) return false;
-      }
-      return true;
+    for(var x = 2; x < n; x++){
+      if(n % x === 0) return false;
     }
+    return n > 1
 };
 
 console.log(isPrimeNumber(2))
