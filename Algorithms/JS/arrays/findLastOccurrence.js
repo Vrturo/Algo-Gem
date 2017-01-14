45af8b50c91f71008ca6b6214a9f1953d88bfbbc
Arturo Perez <VrturoPerez@gmail.com>
2017-01-14 13:29:27 -0800

    add logic

--- Algorithms/JS/arrays/findLastOccurrence.js
+++ Algorithms/JS/arrays/findLastOccurrence.js
@@ -16,13 +16,25 @@
  * @return {number}
  */
 
+// create a function that performs a binary search until item is found and increments index until last occurence is found
+// set lowest index, and highest index variables
+// set result to -1 in case no occurence is found
+// create a while that breaks once lowest idnex meets highest index
+  // create a mid variable by adding index's and dividing by 2
+  // if current mid element is equal to our item we increment up the array
+  // if the current mid element is greater than the item
+    // we reset our high index to the element before our mid element
+  // if the current mid element is less than the item
+    // we reset our low index to the element after our mid element
+// return result at the end
+
 function findLast(nums, k){
     var low = 0,
         high = nums.length -1,
         result = -1,
         mid;
 
-    while( low<=high ){
+    while( low<=high ){ // once low meets high index, loops stops
         mid = Math.floor( (low+high)/2 );
 
         if( nums[mid] === k ){
