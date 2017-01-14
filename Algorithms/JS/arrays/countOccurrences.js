e783488ea18de03c34198e6327808ac3f2fbaae9
Arturo Perez <VrturoPerez@gmail.com>
2017-01-13 22:29:33 -0800

    add solution

--- Algorithms/JS/arrays/countOccurrences.js
+++ Algorithms/JS/arrays/countOccurrences.js
@@ -10,3 +10,33 @@
  * @param {array[], number} nums k
  * @return {number}
  */
+
+ function countOccur(nums, k, searchFirst){
+    var low = 0,
+        high = nums.length -1,
+        result = -1,
+        mid;
+
+    while( low<=high ){
+        mid = Math.floor( (low+high)/2 );
+
+        if( nums[mid] === k ){
+          result = mid;
+          searchFirst ? high = mid - 1 : low = mid + 1;
+        } else if( k < nums[mid]){
+          high = mid-1
+        } else {
+          low = mid+1
+        }
+  }
+  return result;
+ }
+var test = [1,2,2,2,2,3,4,5,5,5,5,6,6,7];
+var k = 5;
+var firstIndex = countOccur(test, k, true);
+if(firstIndex === -1){
+    return 0;
+} else {
+    var lastIndex = countOccur(test, k, false);
+    console.log( (lastIndex - firstIndex)+1)
+}
