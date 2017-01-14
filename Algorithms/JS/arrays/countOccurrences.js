7a546eebd7d6ae660d3fe3c26215187d5eba20dd
Arturo Perez <VrturoPerez@gmail.com>
2017-01-13 22:13:11 -0800

    add esc

--- Algorithms/JS/arrays/countOccurrences.js
+++ Algorithms/JS/arrays/countOccurrences.js
@@ -1,1 +1,7 @@
-countOccurrences.js
+// Given a sorted array of integers, find the number of occurrences of a given target value.
+// Your algorithm’s runtime complexity must be in the order of O(log n).
+// If the target is not found in the array, return 0
+
+// **Example : **
+// Given [5, 7, 7, 8, 8, 10] and target value 8,
+// return 2.
