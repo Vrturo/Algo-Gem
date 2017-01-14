738e9847d31329e80f5299281ba7b004fe711169
Arturo Perez <VrturoPerez@gmail.com>
2017-01-13 22:07:25 -0800

    add desc

--- Algorithms/JS/arrays/findLastOccurrence.js
+++ Algorithms/JS/arrays/findLastOccurrence.js
@@ -1,1 +1,12 @@
-findLastOccurrence.js
+// In this problem, solve a famous programming interview question to find the index of the last occurrences of a number
+// in a sorted array using binary search in O(log n) time.
+
+// ex. input
+// [1,2,2,2,2,3,4,5,5,5,5,6,6,7] 2
+// [1,2,2,2,2,3,4,5,5,5,5,6,6,7] 8
+// [1,2,2,2,2,3,4,5,5,5,5,6,6,7] 5
+
+// ex. output
+// 4
+// -1
+// 10
