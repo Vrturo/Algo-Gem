a01694ff421328f73869c9883a08c68081a90f6c
Arturo Perez <VrturoPerez@gmail.com>
2017-01-14 11:54:58 -0800

    modify solution

--- Algorithms/JS/arrays/countOccurrences.js
+++ Algorithms/JS/arrays/countOccurrences.js
@@ -11,32 +11,39 @@
  * @return {number}
  */
 
- function countOccur(nums, k, searchFirst){
-    var low = 0,
-        high = nums.length -1,
-        result = -1,
-        mid;
+ function countOccur(nums, k){
 
-    while( low<=high ){
-        mid = Math.floor( (low+high)/2 );
 
-        if( nums[mid] === k ){
-          result = mid;
-          searchFirst ? high = mid - 1 : low = mid + 1;
-        } else if( k < nums[mid]){
-          high = mid-1
-        } else {
-          low = mid+1
+    function bSearch(arr, searchFirst){
+        var low = 0,
+          high = arr.length -1,
+          result = -1,
+          mid;
+
+        while( low<=high ){
+            mid = Math.floor( (low+high)/2 );
+
+            if( arr[mid] === k ){
+              result = mid;
+              searchFirst ? high = mid - 1 : low = mid + 1;
+            } else if( k < arr[mid]){
+              high = mid-1
+            } else {
+              low = mid+1
+            }
         }
-  }
-  return result;
- }
+        return result;
+    }
+
+    var firstIndex = bSearch(test, true);
+    if(firstIndex === -1){
+        return 0;
+    } else {
+        var lastIndex = bSearch(test, false);
+        return (lastIndex - firstIndex)+1
+    }
+}
 var test = [1,2,2,2,2,3,4,5,5,5,5,6,6,7];
 var k = 5;
-var firstIndex = countOccur(test, k, true);
-if(firstIndex === -1){
-    return 0;
-} else {
-    var lastIndex = countOccur(test, k, false);
-    console.log( (lastIndex - firstIndex)+1)
-}
+
+console.log( countOccur(test, k) );
