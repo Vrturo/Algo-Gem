a338282c4777cd0225e602660ea7802ea19ba35d
Arturo Perez <VrturoPerez@gmail.com>
2017-01-13 19:51:06 -0800

    add solution

--- Algorithms/JS/arrays/findFirstOccurrence.js
+++ Algorithms/JS/arrays/findFirstOccurrence.js
@@ -10,3 +10,32 @@
 // 1
 // -1
 // 7
+
+/**
+ * @param {array[], number} nums k
+ * @return {number}
+ */
+
+
+function findFirst(nums, k){
+    var low = 0,
+        high = nums.length -1,
+        result = -1,
+        mid;
+
+    while( low<=high ){
+        mid = Math.floor( (low+high)/2 );
+
+        if( nums[mid] === k ){
+          result = mid;
+          high = mid - 1;
+        } else if( k < nums[mid]){
+          high = mid-1
+        } else {
+          low = mid+1
+        }
+  }
+  return result;
+}
+
+console.log(findFirst([1,2,2,2,2,3,4,5,5,5,5,6,6,7], 5))
