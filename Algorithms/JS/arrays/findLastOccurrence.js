3b4f6af8a829c39ea65fcd1b741f50f63128a72c
Arturo Perez <VrturoPerez@gmail.com>
2017-01-13 22:09:23 -0800

    add solution

--- Algorithms/JS/arrays/findLastOccurrence.js
+++ Algorithms/JS/arrays/findLastOccurrence.js
@@ -16,3 +16,25 @@
  * @return {number}
  */
 
+function findLast(nums, k){
+    var low = 0,
+        high = nums.length -1,
+        result = -1,
+        mid;
+
+    while( low<=high ){
+        mid = Math.floor( (low+high)/2 );
+
+        if( nums[mid] === k ){
+          result = mid;
+          low = mid + 1;
+        } else if( k < nums[mid]){
+          high = mid-1
+        } else {
+          low = mid+1
+        }
+  }
+  return result;
+}
+
+console.log(findLast([1,2,2,2,2,3,4,5,5,5,5,6,6,7], 5))
