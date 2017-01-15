5881a47fb7837c62d08a86947c5c2622358e16b2
Arturo Perez <VrturoPerez@gmail.com>
2017-01-15 13:23:16 -0800

    add first attempt

--- Algorithms/JS/arrays/searchRotatedSortedArray.js
+++ Algorithms/JS/arrays/searchRotatedSortedArray.js
@@ -20,3 +20,40 @@
  * @param {number} target
  * @return {number}
  */
+
+var search = function(nums, target) {
+    var result = -1,
+        pivot;
+    for( var i = 0; i<=nums.length; i++ ){
+        if(nums[i] > nums[i+1]){
+            pivot = i+1;
+            break;
+        }
+    }
+    console.log(pivot, nums.length - pivot)
+    var sorted = nums.splice(pivot);
+    sorted = sorted.concat(nums);
+    var low = 0,
+        high = sorted.length -1,
+        mid;
+    while(low <= high){
+        mid = Math.floor( (high+low)/2 );
+
+        if(sorted[mid] === target){
+            result = mid;
+            break;
+        } else if( target < sorted[mid] ){
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+
+    if(pivot === undefined){
+        return result;
+    } else if(result === -1){
+        return -1;
+    } else {
+        return result + pivot;
+    }
+};
