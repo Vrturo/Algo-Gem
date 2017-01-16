31bf2913f77c1b378246ebb27073e39e894895df
Arturo Perez <VrturoPerez@gmail.com>
2017-01-16 15:20:58 -0800

    add desc

--- Algorithms/JS/arrays/findMinRotatedSortedArray.js
+++ Algorithms/JS/arrays/findMinRotatedSortedArray.js
@@ -1,1 +1,18 @@
-findMinRotatedSortedArray.js
+// Find Minimum in Rotated Sorted Array
+
+// Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+// (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+
+// Find the minimum element.
+
+// You may assume no duplicate exists in the array.
+
+// ex.
+// Input:
+// [ 4 5 6 7 0 1 2]
+// [ 9 14 25 56 4 6 8 ]
+
+// Output:
+// 0
+// 9
