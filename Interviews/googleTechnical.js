0fea9a463cfd3b4118de8d6953e7163df6c781f2
Arturo Perez <VrturoPerez@gmail.com>
2017-01-17 14:46:09 -0800

    add desc

--- Interviews/googleTechnical.js
+++ Interviews/googleTechnical.js
@@ -1,1 +1,11 @@
-googleTechnical.js
+Google Technical Interview. Software Engineer Role
+
+10 min Intro
+10 min Resume Project Talk
+  - Asked for detailed information on (Stack, challenges, decisions, etc..)
+compare binary search trees vs hash tables
+  - advantages vs disadvantages
+  - why use one over the other?
+30 min on algorithm
+
+
