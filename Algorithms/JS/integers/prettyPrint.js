2d32975499842dd415b55e7e3cf01a83a0632a11
Arturo Perez <VrturoPerez@gmail.com>
2017-01-19 17:55:27 -0800

    add desc

--- Algorithms/JS/integers/prettyPrint.js
+++ Algorithms/JS/integers/prettyPrint.js
@@ -1,1 +1,30 @@
-prettyPrint.js
+// PRETTYPRINT
+
+// Print concentric rectangular pattern in a 2d matrix.
+// Let us show you some examples to clarify what we mean.
+
+// Example 1:
+
+// Input: A = 4.
+// Output:
+
+// 4 4 4 4 4 4 4
+// 4 3 3 3 3 3 4
+// 4 3 2 2 2 3 4
+// 4 3 2 1 2 3 4
+// 4 3 2 2 2 3 4
+// 4 3 3 3 3 3 4
+// 4 4 4 4 4 4 4
+// Example 2:
+
+// Input: A = 3.
+// Output:
+
+// 3 3 3 3 3
+// 3 2 2 2 3
+// 3 2 1 2 3
+// 3 2 2 2 3
+// 3 3 3 3 3
+// The outermost rectangle is formed by A, then the next outermost is formed by A-1 and so on.
+
+// You will be given A as an argument to the function you need to implement, and you need to return a 2D array.
