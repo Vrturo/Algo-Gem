dfb4dd9097f20dda15cd0ff7c13e9e9790a05d2f
Arturo Perez <VrturoPerez@gmail.com>
2017-01-19 17:56:34 -0800

    add first attempt

--- Algorithms/JS/integers/prettyPrint.js
+++ Algorithms/JS/integers/prettyPrint.js
@@ -28,3 +28,37 @@
 // The outermost rectangle is formed by A, then the next outermost is formed by A-1 and so on.
 
 // You will be given A as an argument to the function you need to implement, and you need to return a 2D array.
+
+function prettyPrint(A){
+      var matrix = [],
+          x = 2*A-2,
+          y = A;
+
+
+      function genMat(k,n,m){
+          var x = k,
+              y = n;
+          for(var i=k; i<2*n-1;i++){
+              for(var j=k; j<2*n-1; j++){
+                  if( i===x||j===x||i===y||j===y){
+                      if(matrix[i][j] === 0) matrix[i][j] = m;
+                  }
+              }
+          }
+      }
+
+      for(var i=0;i<2*A-1;i++){ // create empty matrix
+          matrix[i] = [];
+          for(var j=0;j<2*A-1;j++){
+              matrix[i][j]=0;
+          }
+      }
+      for(i=0; i<A; i++){
+          genMat(i,x,y);  // Construction of outer box
+          x--;
+          y--;
+      }
+      return matrix
+}
+
+console.log( prettyPrint(3) )
