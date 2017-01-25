74f629220ccca711b65fbb3650eb003d8d8056d5
Arturo Perez <VrturoPerez@gmail.com>
2017-01-24 16:52:42 -0800

    add solution

--- Algorithms/JS/strings/romanToInteger.js
+++ Algorithms/JS/strings/romanToInteger.js
@@ -29,3 +29,33 @@ var romanToInt = function(s) { // 332 ms - 94%
     }
     return result;
 };
+
+// ---------------------------------------------------------
+
+var romanToInt = function(s) {
+
+    var lib = {
+        "I": 1, //can be in front of "V" or "X"
+        "V": 5,
+        "X": 10, //can be in front of "L" or "C"
+        "L": 50,
+        "C": 100, //can be in front of "D" or "M"
+        "D": 500,
+        "M": 1000
+    };
+    var ret = 0;
+    for(var i = 0; i < s.length; i++){
+        if((i < s.length - 1) && (s[i] === "I" || s[i] === "X" || s[i] === "C")){
+            if(lib[s[i + 1]] > lib[s[i]]){
+                ret = ret + (lib[s[i + 1]] - lib[s[i]]);
+                i++;
+                continue;
+            }
+        }
+
+        ret = ret + lib[s[i]];
+    }
+
+    return ret;
+
+};
