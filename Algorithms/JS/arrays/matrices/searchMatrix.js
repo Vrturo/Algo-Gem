dbe59b6116723008b29a15bb49ecffe506c83f22
Arturo Perez <VrturoPerez@gmail.com>
2017-01-24 16:07:22 -0800

    add logic

--- Algorithms/JS/arrays/matrices/searchMatrix.js
+++ Algorithms/JS/arrays/matrices/searchMatrix.js
@@ -21,7 +21,23 @@
  */
 
 
+// create variables to keep track of
+  // rLo: the lowest index of the current row
+  // rHi: the highest index of the current row
+  // cLo: the lowest index of the current column
+  // cHi: the highest index of the current column
+  // mid: the index of the current row
 
+// create a while that breaks once low row index meets high row index
+  // set mid by length of rows divided by 2
+  // once mid is set, grab length of current row and set it as our highest column index
+  // create 4 conditionals
+    // if the first or last index of our current row is the target, return true
+    // if target is greater than lowest index of row AND target is less than highest index of current row
+      // we can check each element in current row to see if target exists
+      // decrement and increment lowest and highest index till each element is covered to see if target exists
+    // if the target is less than the lowest element on the current row we can check arrays before
+    // case thats left is.. the target is greater than the highest element on the current row, so we check arrays after
 
 var searchMatrix = function(matrix, target) {
     var rLo = 0,
