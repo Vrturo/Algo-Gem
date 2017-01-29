5be94a0ef9097b55d4674c38e8230e51565fab68
Arturo Perez <VrturoPerez@gmail.com>
2017-01-28 20:50:54 -0800

    add algos

--- Algorithms/JS/strings/streamOccurrence.js
+++ Algorithms/JS/strings/streamOccurrence.js
@@ -1,1 +1,60 @@
-streamOccurrence.js
+Given an stream of strings as input, return the highest occuring string in the stream.
+Assume the stream interface only has the following public methods:
+
+hasNext() --> returns a boolean
+getNext() --> returns the next string
+
+Example:
+Input: 'a' --> 'bb' --> 'ccc' --> 'z' --> 'ccc' --> 'bb' --> 'a' --> 'dd'
+Return: 'z'
+
+If there are multiple strings that ocurr once, return the first unique string.
+
+function highestOccurence(input){
+    var map = {},
+        current = input.getNext(),
+        result = [0, current];
+
+    while( input.hasNext() ){
+        current = input.getNext();
+        if( map[current] ){
+            map[current]++;
+            if( map[current] > result[0] ) result = [map[current], current]
+        } else {
+            map[current] = 1;
+        }
+    }
+
+    return result[1];
+}
+
+// -----------------------------------------------------------------------------
+
+Given an stream of strings as input, return the first unique string in the stream.
+Assume the stream interface only has the following public methods:
+
+hasNext() --> returns a boolean
+getNext() --> returns the next string
+
+Example:
+Input: 'a' --> 'bb' --> 'ccc' --> 'z' --> 'ccc' --> 'bb' --> 'a' --> 'dd'
+Return: 'z'
+
+function uniqueOccurence(input){
+    var map = {},
+        max = 0,
+        current;
+
+    while( input.hasNext() ){
+        current = input.getNext();
+        if( map[current] ){
+            map[current]++;
+        } else {
+            map[current] = 1;
+        }
+    }
+
+    for( var k in map ){
+       if( map[k] === 1 ) return k;
+    }
+}
--- Interviews/amazonFinalTechnical.js
+++ Interviews/amazonFinalTechnical.js
@@ -5,11 +5,10 @@ Why do you want to work at Amazon?
 10 min on an algo question (no coding, jsut explaining)
   - Given an x amount of correctly spelled words, A user wants you to build a program that does a spell check on emails
     You may choose whichever way you want to organize or build this application
-20 min on algorithm question
+20 min on two algorithm questions
 15 min of OO Design Questions
 
-// Algorithm
-
+// Algorithms
 
 Given an stream of strings as input, return the highest occuring string in the stream.
 Assume the stream interface only has the following public methods:
@@ -23,22 +22,22 @@ Return: 'z'
 
 If there are multiple strings that ocurr once, return the first unique string.
 
-function occurence(input){
-    var map = {},
-        max = 0,
-        current;
-
-    while( input.hasNext() ){
-        current = input.getNext();
-        if( map[current] ){
-            map[current]++;
-        } else {
-            map[current] = 1;
-        }
-    }
-
-    for( var k in map ){ // isnt
-       if( map[k] === 1 ) return k; // constant
-    }
+function highestOccurence(input){
+  // write code here
 }
 
+// --------------------------------------------------------------------------------
+
+Given an stream of strings as input, return the first unique string in the stream.
+Assume the stream interface only has the following public methods:
+
+hasNext() --> returns a boolean
+getNext() --> returns the next string
+
+Example:
+Input: 'a' --> 'bb' --> 'ccc' --> 'z' --> 'ccc' --> 'bb' --> 'a' --> 'dd'
+Return: 'z'
+
+function uniqueOccurence(input){
+    // write code here
+}
