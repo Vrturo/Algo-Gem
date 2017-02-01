6c4b8aa4ac1799d9d41d912d99e7f7c70b90e1ca
Arturo Perez <VrturoPerez@gmail.com>
2017-02-01 15:24:02 -0800

    add logic

--- Algorithms/JS/integers/squareRoot.js
+++ Algorithms/JS/integers/squareRoot.js
@@ -9,12 +9,26 @@
  * @return {number}
  */
 
-const sqrt = function mySqrt(x) {
+
+// if integer given is 0, return 0
+// create a range from 1 - Max integer
+// create a loop that will break once we find sqrt
+  // reset mid to the difference of highest and lowest integer divided by, plus lowest integer
+  // if our given integer divided by 2 is less than our mid
+    // set our highest index to one less than mid
+  // else
+    // if mid plus 1 is greater than our given integer divided by mid plus 1
+      // we found our sqrt
+    // if we didnt find our sqrt
+      // set our left to mid plus 1
+
+const sqrt = function mySqrt(x) { // Binary Search approach
   if (x === 0) return 0;
   let left = 1;
   let right = Number.MAX_VALUE;
+  let mid;
   while (true) {
-    const mid = left + ((right - left) / 2);
+    mid = left + ((right - left) / 2);
     if (mid > x / mid) {
       right = mid - 1;
     } else {
