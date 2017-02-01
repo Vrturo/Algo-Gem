d73adef69ef0ec7c7abd99527432017437132af3
Arturo Perez <VrturoPerez@gmail.com>
2017-02-01 14:19:06 -0800

    add solution

--- Algorithms/JS/integers/squareRoot.js
+++ Algorithms/JS/integers/squareRoot.js
@@ -8,3 +8,18 @@ Compute and return the square root of x.
  * @param {number} x
  * @return {number}
  */
+
+var mySqrt = function(x) {
+    if (x === 0) return 0;
+    var left = 1,
+        right = Number.MAX_VALUE;
+    while (true) {
+        var mid = left + (right - left)/2;
+        if (mid > x/mid) {
+            right = mid - 1;
+        } else {
+            if (mid + 1 > x/(mid + 1)) return mid;
+            left = mid + 1;
+        }
+    }
+};
