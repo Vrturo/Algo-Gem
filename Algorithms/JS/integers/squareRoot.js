ae383686880eb95fa469c08876efc27e29963a5d
Arturo Perez <VrturoPerez@gmail.com>
2017-02-01 14:32:31 -0800

    linted

--- Algorithms/JS/integers/squareRoot.js
+++ Algorithms/JS/integers/squareRoot.js
@@ -9,17 +9,19 @@
  * @return {number}
  */
 
-var mySqrt = function(x) {
-    if (x === 0) return 0;
-    var left = 1,
-        right = Number.MAX_VALUE;
-    while (true) {
-        var mid = left + (right - left)/2;
-        if (mid > x/mid) {
-            right = mid - 1;
-        } else {
-            if (mid + 1 > x/(mid + 1)) return mid;
-            left = mid + 1;
-        }
+const sqrt = function mySqrt(x) {
+  if (x === 0) return 0;
+  let left = 1;
+  let right = Number.MAX_VALUE;
+  while (true) {
+    const mid = left + ((right - left) / 2);
+    if (mid > x / mid) {
+      right = mid - 1;
+    } else {
+      if (mid + 1 > x / (mid + 1)) return mid;
+      left = mid + 1;
     }
+  }
 };
+
+console.log(sqrt(5));
