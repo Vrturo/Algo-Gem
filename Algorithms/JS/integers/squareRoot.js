63d8987e926d1b8133f9e625b451d3a14ce9ece8
Arturo Perez <VrturoPerez@gmail.com>
2017-02-01 14:25:37 -0800

    add eslint locally

--- Algorithms/JS/integers/squareRoot.js
+++ Algorithms/JS/integers/squareRoot.js
@@ -1,8 +1,8 @@
-Sqrt(x)
+// Sqrt(x)
 
-Implement int sqrt(int x).
+// Implement int sqrt(int x).
 
-Compute and return the square root of x.
+// Compute and return the square root of x.
 
 /**
  * @param {number} x
