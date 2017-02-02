f97d423833305adccd6a5eba1ed6291b32aa3a57
Arturo Perez <VrturoPerez@gmail.com>
2017-02-02 12:51:14 -0800

    add desc

--- Algorithms/JS/arrays/removeDuplicatesFromArr.js
+++ Algorithms/JS/arrays/removeDuplicatesFromArr.js
@@ -1,1 +1,11 @@
-removeDuplicatesFromArr.js
+Remove Element from ArrayBookmark Suggest Edit
+
+Given an array and a value, remove all the instances of that value in the array.
+Also return the number of elements left in the array after the operation.
+It does not matter what is left beyond the expected length.
+
+ Example:
+If array A is [4, 1, 1, 2, 1, 3]
+and value elem is 1,
+then new length is 3, and A is now [4, 2, 3]
+Try to do it in less than linear additional space complexity.
