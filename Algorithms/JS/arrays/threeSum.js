a545d02a24cc0e1ab6a23f0f383e229f99e1578b
Arturo Perez <VrturoPerez@gmail.com>
2017-02-01 19:57:02 -0800

    add first solution

--- Algorithms/JS/arrays/threeSum.js
+++ Algorithms/JS/arrays/threeSum.js
@@ -16,3 +16,34 @@
  * @param {number} target
  * @return {number}
  */
+
+function threeSumClosest(A, B){
+  var map = {},
+          sum;
+
+  for( var i = 0; i < A.length; i++ ){
+
+      for( var j=i+1; j<A.length; j += 1){
+
+          for(var k=j+1; k<A.length; k += 1){
+              sum = A[i] + A[j] + A[k]
+              if(sum === B) return B;
+              if(map[sum] === undefined) {
+                  map[sum] = sum;
+
+              } else {
+                  continue;
+              };
+          }
+
+      }
+
+  }
+  var sumArr = Object.keys(map);
+  var closest = sumArr.reduce(function (prev, curr) {
+    return (Math.abs(curr - B) < Math.abs(prev - B) ? curr : prev);
+  });
+  return closest
+
+}
+console.log(threeSumClosest([2, 1, -9, -7, -8, 2, -8, 2, 3, -8 ], -1))
