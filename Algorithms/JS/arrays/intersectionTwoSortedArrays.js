3a6b72b23e2a234f7540e897528b5e31b0934b69
Arturo Perez <VrturoPerez@gmail.com>
2017-02-01 20:32:57 -0800

    add desc

--- Algorithms/JS/arrays/intersectionTwoSortedArrays.js
+++ Algorithms/JS/arrays/intersectionTwoSortedArrays.js
@@ -1,1 +1,19 @@
-intersectionTwoSortedArrays.js
+// Intersection Of Sorted ArraysBookmark Suggest Edit
+
+// Find the intersection of two sorted arrays.
+// OR in other words,
+// Given 2 sorted arrays, find all the elements which occur in both the arrays.
+
+// Example :
+
+// Input :
+//     A : [1 2 3 3 4 5 6]
+//     B : [3 3 5]
+
+// Output : [3 3 5]
+
+// Input :
+//     A : [1 2 3 3 4 5 6]
+//     B : [3 5]
+
+// Output : [3 5]
