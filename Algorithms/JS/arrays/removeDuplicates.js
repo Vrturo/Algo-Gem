0ababb19e99dd59547a486bd8045b71a05eeb823
Arturo Perez <VrturoPerez@gmail.com>
2017-02-02 13:07:28 -0800

    edit desc

--- Algorithms/JS/arrays/removeDuplicates.js
+++ Algorithms/JS/arrays/removeDuplicates.js
@@ -1,5 +1,11 @@
-// Remove duplicate numbers from an array
-// return an array of each number once
+Given an array and a value, remove all the instances of that value in the array.
+return the number of elements in the array once after the operation.
+
+Example:
+If array A is [4, 1, 1, 2, 1, 3]
+and value elem is 1,
+then new length is 3, and A is now [4, 2, 3]
+
 
 function removeDuplicates(arr){
   var count = {};
