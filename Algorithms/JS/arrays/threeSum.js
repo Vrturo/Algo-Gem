a63820fa6ab25bf9744e9c7abb8d6402373efea1
Arturo Perez <VrturoPerez@gmail.com>
2017-02-01 19:55:43 -0800

    add desc

--- Algorithms/JS/arrays/threeSum.js
+++ Algorithms/JS/arrays/threeSum.js
@@ -1,1 +1,12 @@
-threeSum.js
+// 3 sum
+
+// Given an array S of n integers, find three integers in S such that the sum is closest to a given number, target.
+// Return the sum of the three integers.
+
+// Assume that there will only be one solution
+
+// Example:
+// given array S = {-1 2 1 -4},
+// and target = 1.
+
+// The sum that is closest to the target is 2. (-1 + 2 + 1 = 2)
