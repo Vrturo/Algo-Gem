757ddd9bd239b28839e52c11a33de32cae3b0fb4
Arturo Perez <VrturoPerez@gmail.com>
2017-02-04 13:21:25 -0800

    add solution

--- Algorithms/JS/arrays/containerWithMostWater.js
+++ Algorithms/JS/arrays/containerWithMostWater.js
@@ -11,3 +11,13 @@ Note: You may not slant the container and n is at least 2.
  * @return {number}
  */
 
+var maxArea = function(height) {
+    var l = 0,
+        r = height.length - 1,
+        maxArea = 0;
+    while (l < r) {
+        maxArea = Math.max(maxArea, ((r-l)*Math.min(height[l], height[r])));
+        height[l] < height[r] ? l += 1 : r -= 1;
+    }
+    return maxArea;
+};
