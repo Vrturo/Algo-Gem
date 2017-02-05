d5395bc84b461b72ea1a3d7181930dd7a0d92ac6
Arturo Perez <VrturoPerez@gmail.com>
2017-02-04 16:35:57 -0800

    add desc

--- Algorithms/JS/strings/implementStrStr().js
+++ Algorithms/JS/strings/implementStrStr().js
@@ -1,1 +1,7 @@
-implementStrStr().js
+Implement strStr().
+
+strstr - locate a substring ( needle ) in a string ( haystack )
+
+Try not to use standard library string functions for this question.
+
+Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
