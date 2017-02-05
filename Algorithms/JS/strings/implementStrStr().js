cbbe44e315d2264be309585cc45552eb85229d7d
Arturo Perez <VrturoPerez@gmail.com>
2017-02-04 19:54:53 -0800

    add solution

--- Algorithms/JS/strings/implementStrStr().js
+++ Algorithms/JS/strings/implementStrStr().js
@@ -11,3 +11,18 @@
  * @param {string} needle
  * @return {number}
  */
+
+const str = function strStr(haystack, needle) {
+  const lenOne = haystack.length;
+  const lenTwo = needle.length;
+  if (lenOne < lenTwo) {
+    return -1;
+  } else if (lenTwo === 0) {
+    return 0;
+  }
+  const threshold = lenOne - lenTwo;
+  for (let i = 0;i <= threshold;i += 1) {
+    if (haystack.substring(i,i + lenTwo) === needle) return i;
+  }
+  return -1;
+};
