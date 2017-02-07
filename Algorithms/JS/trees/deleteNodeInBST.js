cc15d0a02fc883fec42cdf9d21dd2b63f4d77071
Arturo Perez <VrturoPerez@gmail.com>
2017-02-06 21:06:56 -0800

    add logic

--- Algorithms/JS/trees/deleteNodeInBST.js
+++ Algorithms/JS/trees/deleteNodeInBST.js
@@ -51,6 +51,17 @@
  * @return {TreeNode}
  */
 
+// Recursively find the node that has the same value as the key, while setting the left/right nodes equal to the returned subtree
+// Once the node is found, have to handle the below 4 cases
+  // node doesn't have left or right - return null
+  // node only has left subtree - return the left subtree
+  // node only has right subtree- return the right subtree
+  // node has both left and right
+    // find the minimum value in the right subtree
+    // set that value to the currently found node
+    // recursively delete the minimum value in the right subtree
+// return final object
+
 const deleteNode = function (root, key) {
   if (root === null) return null;
 
