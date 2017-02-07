4f69b75844fe156385ddec0a722534b375f94829
Arturo Perez <VrturoPerez@gmail.com>
2017-02-06 20:35:25 -0800

    add solution

--- Algorithms/JS/trees/deleteNodeInBST.js
+++ Algorithms/JS/trees/deleteNodeInBST.js
@@ -50,3 +50,30 @@
  * @param {number} key
  * @return {TreeNode}
  */
+
+ var deleteNode = function(root, key,parent=null) {
+    if (root === null) return null;
+    if (key < root.val) {
+        root.left = deleteNode(root.left, key);
+    } else if (key > root.val) {
+        root.right = deleteNode(root.right, key);
+    } else {
+        if (root.left === null) {
+            return root.right;
+        } else if (root.right === null) {
+            return root.left;
+        }
+        var minNode = findMin(root.right);
+        root.val = minNode.val;
+        root.right = deleteNode(root.right, root.val);
+    }
+    return root;
+};
+
+function findMin(node){
+    while(node.left !== null) node = node.left;
+    return node;
+}
+
+
+
