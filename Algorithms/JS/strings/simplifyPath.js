34d65e3405a726dc7dd5c7b51b362dd625a5f861
Arturo Perez <VrturoPerez@gmail.com>
2017-02-07 12:24:34 -0800

    linted

--- Algorithms/JS/strings/simplifyPath.js
+++ Algorithms/JS/strings/simplifyPath.js
@@ -1,41 +1,40 @@
-Simplify Path
+// Simplify Path
 
-Given an absolute path for a file (Unix-style), simplify it.
+// Given an absolute path for a file (Unix-style), simplify it.
 
-For example,
-path = "/home/", => "/home"
-path = "/a/./b/../../c/", => "/c"
+// For example,
+// path = "/home/", => "/home"
+// path = "/a/./b/../../c/", => "/c"
 
-Corner Cases:
+// Corner Cases:
 
-Did you consider the case where path = "/../"?
-In this case, you should return "/"
+// Did you consider the case where path = "/../"?
+// In this case, you should return "/"
 
-Another corner case is the path might contain multiple slashes '/' together, such as "/home//foo/"
-In this case, you should ignore redundant slashes and return "/home/foo"
+// Another corner case is the path might contain multiple slashes '/' together, such as "/home//foo/"
+// In this case, you should ignore redundant slashes and return "/home/foo"
 
 /**
  * @param {string} path
  * @return {string}
 */
 
-var simplifyPath = function(path) {
-    var stack = [];
-    path = path.split('/');
-    for (var i = 0; i < path.length; i += 1) {
-        if (path[i] === '.'){
-            continue;
-        } else if (path[i] === '..') {
-            stack.pop();
-        } else if (path[i] === ''){
-            continue;
-        } else {
-            stack.push(path[i]);
-        }
+const simplifyPath = function sp(path) {
+  const stack = [];
+  path = path.split('/');
+  for (let i = 0; i < path.length; i += 1) {
+    if (path[i] === '.' || path[i] === '') continue;
+    if (path[i] === '..') {
+      stack.pop();
+    } else {
+      stack.push(path[i]);
     }
-    var result = '';
-    for(var j = 0; j < stack.length; j += 1) {
-       result = result + '/' + stack[j];
-    }
-    return result.length ? result : '/';
+  }
+  let result = '';
+  for (let j = 0; j < stack.length; j += 1) {
+    result = result + '/' + stack[j];
+  }
+  return result.length ? result : '/';
 };
+
+simplifyPath('/a/./b/../../c/');
