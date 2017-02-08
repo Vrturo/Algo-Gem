20182dcf90b9c7f69276d37a9e6b70a5434c3c17
Arturo Perez <VrturoPerez@gmail.com>
2017-02-07 21:28:33 -0800

    add desc

--- Algorithms/JS/graphs/shortestPath.js
+++ Algorithms/JS/graphs/shortestPath.js
@@ -1,1 +1,15 @@
-shortestPath.js
+// Given an unweighted, undirected graph which represents a metro map as follows
+// vertices are stations
+// edges are the path between two stations
+
+
+// Given a start station and ending station, determine the minimum number of stops that must be made to get to the destination.
+
+// Input: A Graph (unweighted/undirected), a starting Vertex, and an ending Vertex
+// Output: Integer
+
+// Example
+// Input: The graph represented below, Vertex A, Vertex F
+
+// Output: 2 Stops (From A stop at C, and then stop at F)
+
