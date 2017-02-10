10801493a14ceed94c4b8e30810a1d4a6aad83a0
Arturo Perez <VrturoPerez@gmail.com>
2017-02-10 12:14:34 -0800

    add attempt

--- Algorithms/JS/graphs/unWeightedShortestPath.js
+++ Algorithms/JS/graphs/unWeightedShortestPath.js
@@ -253,6 +253,55 @@ function shortestPathFour(graph, a, b) {
   }
   return distance[b.value];
 }
+
+// ----------------------------------------------------------------------------------
+
+// Work in progress
+
+
+function shortestPathFive(graph, a, b) {
+  const distance = {};
+  const visited = {};
+  let pointer = 0;
+  distance[a.value] = 0;
+  visited[a.value] = true;
+  let toVisit = [a];
+  let val;
+
+  while (pointer < toVisit.length){
+    val = toVisit[pointer];
+    for (const edge in val.edges) {
+      if (!distance[edge]) distance[edge] = Number.MAX_VALUE;
+      if (!visited[edge]) {
+        visited[edge] = false;
+        toVisit.push(edge);
+      }
+    }
+    pointer += 1
+  }
+
+  pointer = 0
+  toVisit = [a]
+  let minDistance;
+  while (pointer < toVisit.length) {
+    val = toVisit[pointer];
+    for (const edge in val.edges) {
+      if (!visited[edge]) {
+        minDistance = Math.min(distance[edge], distance[val] + 1)
+        distance[edge] = minDistance;
+        visited[edge] = true;
+        toVisit.push(edge);
+      }
+      if (edge !== val){
+        minDistance = Math.min(distance[edge], distance[val] + 1);
+        distance[edge] = minDistance;
+      }
+    }
+    pointer += 1
+  }
+  return distance[b.value]
+}
+
 // function benchMark( g, a, b, results){
 //   var start = new Date();
 //   shortestPathThree(g, a, b);
@@ -263,4 +312,4 @@ function shortestPathFour(graph, a, b) {
 // console.time('bm');
 // console.log( benchMark( graph, graph.getVertex('A'), graph.getVertex('F')) );
 // console.timeEnd('bm');
-console.log( shortestPathFour( graph, graph.getVertex('A'), graph.getVertex('F')) );
+console.log( shortestPathFive( graph, graph.getVertex('A'), graph.getVertex('F')) );
