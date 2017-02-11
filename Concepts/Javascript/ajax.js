3e92849ed933c11a1d1c3e5755cc7d17e21dca2a
Arturo Perez <VrturoPerez@gmail.com>
2017-02-11 14:08:18 -0800

    fill ajax

--- Concepts/Javascript/ajax.js
+++ Concepts/Javascript/ajax.js
@@ -1,1 +1,9 @@
-ajax.js
+// What is Ajax?
+
+// Asynchronous Javascript and XML -
+//   A set of web development techniques using many web technologies on the client-side to create asynchronous Web applications.
+//   With Ajax, web applications can send data to and retrieve from a server asynchronously (in the background)
+//   without interfering with the display and behavior of the existing page.
+//   By decoupling the data interchange layer from the presentation layer, Ajax allows for web pages,
+//   and by extension web applications, to change content dynamically without the need to reload the entire page.
+//   [4] In practice, modern implementations commonly substitute JSON for XML due to the advantages of being native to JavaScript.
