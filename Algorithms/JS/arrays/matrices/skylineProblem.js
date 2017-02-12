3c5523142ce2b5cafc1879f544876583bf275286
Arturo Perez <VrturoPerez@gmail.com>
2017-02-11 20:15:52 -0800

    add new ds file

--- Algorithms/JS/arrays/matrices/skylineProblem.js
+++ Algorithms/JS/arrays/matrices/skylineProblem.js
@@ -1,1 +1,6 @@
-skylineProblem.js
+Skyline Problem
+
+You are given a set of n rectangles in no particular order.
+They have varying widths and heights, but their bottom edges are collinear, so that they look like buildings on a skyline.
+For each rectangle, you’re given the x position of the left edge, the x position of the right edge, and the height.
+Your task is to draw an outline around the set of rectangles so that you can see what the skyline would look like when silhouetted at night.
