fe56dc8959658d8c4127af3c6ef41d8029bf21f6
Arturo Perez <VrturoPerez@gmail.com>
2017-02-11 21:56:57 -0800

    add desc

--- Algorithms/Data Structures/doublyLinkedList.js
+++ Algorithms/Data Structures/doublyLinkedList.js
@@ -1,1 +1,24 @@
-doublyLinkedList.js
+// Doubly Linked List
+
+// A Doubly Linked List (DLL) contains an extra pointer, typically called previous pointer,
+//   together with next pointer and data which are there in singly linked list.
+
+
+// Advantages over singly linked list:
+
+// 1) A DLL can be traversed in both forward and backward direction.
+
+// 2) The delete operation in DLL is more efficient if pointer to the node to be deleted is given.
+
+// In singly linked list, to delete a node, pointer to the previous node is needed.
+//   To get this previous node, sometimes the list is traversed.
+//   In DLL, we can get the previous node using previous pointer.
+
+// Disadvantages over singly linked list:
+
+// 1) Every node of DLL Require extra space for an previous pointer.
+//    It is possible to implement DLL with single pointer.
+
+// 2) All operations require an extra pointer previous to be maintained.
+//    For example, in insertion, we need to modify previous pointers together with next pointers.
+//    For example in following functions for insertions at different positions, we need 1 or 2 extra steps to set previous pointer.
