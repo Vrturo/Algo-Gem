ee87d19f6ef4ca517c4fa70048d80fcaca382c7b
Arturo Perez <VrturoPerez@gmail.com>
2017-02-11 22:24:18 -0800

    edit solution

--- Algorithms/JS/linkedList/reverseSinglyLinkedList.js
+++ Algorithms/JS/linkedList/reverseSinglyLinkedList.js
@@ -3,30 +3,30 @@
 
 function reversesll(sll){
 
-  if(!sll.head || !sll.head.next) return sll; //check if empty
+  if (!sll.head || !sll.head.next) return sll; //check if empty
 
-  var nodes = [],
-      current = sll.head;
+  const nodes = [];
+  let current = sll.head;
 
-  while(current){ //storing all the nodes in an array
+  while (current) { //storing all the nodes in an array
     nodes.push(current);
     current = current.next;
   }
 
-  var reversedLL = new LinkedList();
+  const reversedLL = new LinkedList();
 
   reversedLL.head = nodes.pop();
   current = reversedLL.head;
 
-  var node = nodes.pop();
+  let node = nodes.pop();
   //make sure to make next of the newly inserted node to be null
   //other wise the last node of your SLL will retain its old next.
-  while(node){
-     node.next = null;
-     current.next = node;
+  while (node) {
+    node.next = null;
+    current.next = node;
 
-     current = current.next;
-     node = nodes.pop();
+    current = current.next;
+    node = nodes.pop();
   }
   return reversedLL;
 }
