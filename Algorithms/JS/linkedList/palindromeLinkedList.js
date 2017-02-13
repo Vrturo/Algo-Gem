7272ba0f4496d8183ef0d2670fa86533a95f4152
Arturo Perez <VrturoPerez@gmail.com>
2017-02-13 13:30:40 -0800

    add ds

--- Algorithms/JS/linkedList/palindromeLinkedList.js
+++ Algorithms/JS/linkedList/palindromeLinkedList.js
@@ -2,3 +2,12 @@
 
 // Follow up:
 // Could you do it in O(n) time and O(1) space?
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
