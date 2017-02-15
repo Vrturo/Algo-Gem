b4b7dcd0dd89473566a7b2cdc8497ac3900c113f
Arturo Perez <VrturoPerez@gmail.com>
2017-02-15 11:17:47 -0800

    add logic

--- Algorithms/JS/linkedList/palindromeLinkedList.js
+++ Algorithms/JS/linkedList/palindromeLinkedList.js
@@ -17,6 +17,29 @@
  * @return {boolean}
  */
 
+
+// Logic
+
+// This can be solved by reversing the 2nd half and compare the two halves. Let's start with an example [1, 1, 2, 1].
+
+// In the beginning, set two pointers fast and slow starting at the head.
+
+// 1 -> 1 -> 2 -> 1 -> null
+// sf
+// (1) Move: fast pointer goes to the end, and slow goes to the middle.
+
+// 1 -> 1 -> 2 -> 1 -> null
+//           s          f
+// (2) Reverse: the right half is reversed, and slow pointer becomes the 2nd head.
+
+// 1 -> 1    null <- 2 <- 1
+// h                      s
+// (3) Compare: run the two pointers head and slow together and compare.
+
+// 1 -> 1    null <- 2 <- 1
+//      h            s
+
+
 var isPalindrome = function(head) {
   let fast = head;
   let slow = head;
@@ -24,9 +47,9 @@ var isPalindrome = function(head) {
     fast = fast.next.next;
     slow = slow.next;
   }
-  if (fast !== null) { // odd nodes: let right half smaller
-    slow = slow.next;
-  }
+  // odd nodes: let right half smaller
+  if (fast !== null) slow = slow.next;
+
   slow = reverse(slow);
   fast = head;
 
