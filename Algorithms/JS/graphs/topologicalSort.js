d2605f05ba84a3f2c1d9ce54ed4eb9e38242a22e
Arturo Perez <VrturoPerez@gmail.com>
2017-02-16 11:54:41 -0800

    add test case, constructor, solution

--- Algorithms/JS/graphs/topologicalSort.js
+++ Algorithms/JS/graphs/topologicalSort.js
@@ -1,5 +1,33 @@
 // Adjacency List constructor
 
+
+
+function topologicalSort(g){
+  var result = [],
+      visited = {},
+      stack = [];
+
+  function helper(v) {
+    visited[v] = true;
+    var edges = g.vertices[v];
+    for (var i = 0; i < edges.length; i += 1) {
+      if (!visited[edges[i]]) helper(edges[i]);
+    }
+    stack.push(v);
+  }
+
+  for (var k in g.vertices) { // loop through graph vertices
+    if (!visited[k]) helper(k);
+  }
+
+  while (stack.length) { // return stack in reverse
+    result.push(stack.pop());
+  }
+  return result;
+}
+
+
+
 class Graph{
   constructor() {
     this.vertices = {};
@@ -42,3 +70,25 @@ class Graph{
   }
 
 }
+
+var graph = new Graph;
+graph.addVertex('A');
+graph.addVertex('B');
+graph.addVertex('C');
+graph.addVertex('D');
+graph.addVertex('E');
+graph.addVertex('F');
+graph.addVertex('G');
+
+graph.addEdge('A', 'C');
+graph.addEdge('A', 'D');
+graph.addEdge('A', 'B');
+graph.addEdge('B', 'E');
+graph.addEdge('C', 'F');
+graph.addEdge('D', 'G');
+graph.addEdge('F', 'G');
+graph.addEdge('F', 'E');
+
+
+console.log(graph)
+console.log(topologicalSort(graph))
