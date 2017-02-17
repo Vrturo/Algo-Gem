6d6392eaf789d7e9754e1cb7aa29e18b04a2e8b6
Arturo Perez <VrturoPerez@gmail.com>
2017-02-17 11:42:54 -0800

    lint

--- Algorithms/JS/linkedList/reverseLinkedList2.js
+++ Algorithms/JS/linkedList/reverseLinkedList2.js
@@ -1,15 +1,15 @@
-Reverse Linked List 2
+// Reverse Linked List 2
 
-Reverse a linked list from position m to n. Do it in-place and in one-pass.
+// Reverse a linked list from position m to n. Do it in-place and in one-pass.
 
-For example:
-Given 1->2->3->4->5->NULL, m = 2 and n = 4,
+// For example:
+// Given 1->2->3->4->5->NULL, m = 2 and n = 4,
 
-return 1->4->3->2->5->NULL.
+// return 1->4->3->2->5->NULL.
 
-Note:
-Given m, n satisfy the following condition:
-1 ≤ m ≤ n ≤ length of list.
+// Note:
+// Given m, n satisfy the following condition:
+// 1 ≤ m ≤ n ≤ length of list.
 
 /**
  * Definition for singly-linked list.
@@ -27,12 +27,12 @@ Given m, n satisfy the following condition:
  */
 
 
-var reverseBetween = function(head, m, n) {
-    let current = head;
-    const dup = [head.val];
-    while (current.next) {
-        current = current.next;
-        dup.push(current.val);
-    }
-    return dup.slice(0, m - 1).concat(dup.slice(m - 1, n).reverse(), dup.slice(n));
+const reverseBetween = function rev(head, m, n) {
+  let current = head;
+  const dup = [head.val];
+  while (current.next) {
+    current = current.next;
+    dup.push(current.val);
+  }
+  return dup.slice(0, m - 1).concat(dup.slice(m - 1, n).reverse(), dup.slice(n));
 };
