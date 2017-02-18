0de994f3d3ef3b545ff412579b1af1cc3bba4689
Arturo Perez <VrturoPerez@gmail.com>
2017-02-18 10:30:06 -0800

    add solution

--- Algorithms/JS/arrays/evaluateReversePolishNotation.js
+++ Algorithms/JS/arrays/evaluateReversePolishNotation.js
@@ -10,3 +10,34 @@
 //   ["4", "13", "5", "/", "+"] -> (4 + (13 / 5)) -> 6
 // [ "2", "3", "+", "4", "5", "+", "+", "6", "7", "+", "*", "2", "*" ] ->  (((2 + 3) + (4 + 5)) * (6 + 7)) * 2 -> 364
 
+
+
+
+var evalRPN = function(tokens) {
+    var a, b;
+    var stack = [];
+    for (var i = 0; i < tokens.length; i += 1) {
+        if (tokens[i] === '+') {
+            stack.push(stack.pop() + stack.pop());
+            console.log(stack)
+        } else if (tokens[i] === '/') {
+            b = stack.pop();
+            a = stack.pop();
+            stack.push(Math.floor(a / b));
+            console.log(stack)
+        } else if(tokens[i] === '*') {
+            stack.push((stack.pop() * stack.pop()));
+            console.log(stack)
+        } else if(tokens[i] === '-') {
+            b = stack.pop();
+            a = stack.pop();
+            stack.push(a - b);
+            console.log(stack)
+        } else {
+            stack.push(Number(tokens[i]));
+            console.log(stack)
+        }
+    }
+    return stack.pop();
+};
+
