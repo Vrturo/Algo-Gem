fc735154a5d45e2ea3c21f466c6f11bda89cce35
Arturo Perez <VrturoPerez@gmail.com>
2017-02-18 14:38:42 -0800

    add desc

--- Algorithms/JS/strings/redundantBraces.js
+++ Algorithms/JS/strings/redundantBraces.js
@@ -1,1 +1,13 @@
-redundantBraces.js
+// Redundant Braces
+
+// Write a program to validate if the input string has redundant braces?
+
+// Input will be always a valid expression
+
+// and operators allowed are only + , * , - , /
+
+// Example:
+
+// ((a + b)) has redundant braces so answer will be true
+// (a + (a + b)) doesn't have have any redundant braces so answer will be false
+
