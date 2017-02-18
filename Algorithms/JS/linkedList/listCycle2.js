2b0d999fc5dee4f517887378dbc9c9bfd103945d
Arturo Perez <VrturoPerez@gmail.com>
2017-02-17 16:26:46 -0800

    add solution

--- Algorithms/JS/linkedList/listCycle2.js
+++ Algorithms/JS/linkedList/listCycle2.js
@@ -20,3 +20,23 @@ Can you solve it without using extra space?
  * @param {ListNode} head
  * @return {ListNode}
  */
+
+var detectCycle = function(head) {
+    let slow = head;
+    let fast = head;
+
+    while (fast !== null && fast.next !== null){
+        fast = fast.next.next;
+        slow = slow.next;
+
+        if (fast === slow){
+            let slow2 = head;
+            while (slow2 != slow){
+                slow = slow.next;
+                slow2 = slow2.next;
+            }
+            return slow;
+        }
+    }
+    return null;
+};
