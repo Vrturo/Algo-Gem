bcb591eb44080823bda2e9051108fbf371039191
Arturo Perez <VrturoPerez@gmail.com>
2017-02-18 15:06:47 -0800

    add logci

--- Algorithms/JS/strings/redundantBraces.js
+++ Algorithms/JS/strings/redundantBraces.js
@@ -14,6 +14,15 @@
 // param A : string
 // return an integer
 
+// create stack to keep track of elements per brace pair
+// create a count var to know if too little elemnts are in each brace pair
+// loop through string
+  // keep pushing elements onto the stack till we encounter ')'
+  // When element is ')', start popping elements till a matching '(' is found
+  // If the number of elements popped are less than 2 we know we have a redundant brace
+    // return false
+// if loop doesnt return false we don't have any redundant braces, so return true
+
 
 function braces(A) {
   const stack = [];
