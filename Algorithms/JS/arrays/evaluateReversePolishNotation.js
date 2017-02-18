34a86cb7f5a7adf4b126ae284c8d49cb94875aca
Arturo Perez <VrturoPerez@gmail.com>
2017-02-18 10:29:32 -0800

    add new array file w/ desc

---
+++ Algorithms/JS/arrays/evaluateReversePolishNotation.js
@@ -0,0 +1,12 @@
+// Evaluate Reverse Polish Notation
+
+// Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+
+// Valid operators are +, -, *, /. Each operand may be an integer or another expression.
+
+// Examples:
+
+//   ["2", "1", "+", "3", "*"] -> ((2 + 1) * 3) -> 9
+//   ["4", "13", "5", "/", "+"] -> (4 + (13 / 5)) -> 6
+// [ "2", "3", "+", "4", "5", "+", "+", "6", "7", "+", "*", "2", "*" ] ->  (((2 + 3) + (4 + 5)) * (6 + 7)) * 2 -> 364
+
--- test.js
+++ test.js
@@ -236,6 +236,29 @@
 //   return distance[b.value]
 // }
 
-var a = [2, 9, 9];
-a.indexOf(2); // 0
-a.indexOf(7); // -1
+var test = [ "2", "3", "+", "4", "5", "+", "+", "6", "7", "+", "*", "2", "*" ]
+
+function evalRPN(tokens) {
+    var a, b;
+    var stack = [];
+    for (var i = 0; i < tokens.length; i += 1) {
+        if (tokens[i] === '+') {
+            stack.push(stack.pop() + stack.pop());
+        } else if (tokens[i] === '/') {
+            b = stack.pop();
+            a = stack.pop();
+            stack.push(Math.floor(a / b));
+        } else if(tokens[i] === '*') {
+            stack.push((stack.pop() * stack.pop()));
+        } else if(tokens[i] === '-') {
+            b = stack.pop();
+            a = stack.pop();
+            stack.push(a - b);
+        } else {
+            stack.push(Number(tokens[i]));
+        }
+    }
+    return stack.pop();
+}
+
+console.log(evalRPN(test))
