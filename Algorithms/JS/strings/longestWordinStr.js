db88ddff47a493cb92aba7ff6256fb818f71d3fa
Arturo Perez <VrturoPerez@gmail.com>
2017-03-11 11:24:12 -0800

    lint/add logic

--- Algorithms/JS/strings/longestWordinStr.js
+++ Algorithms/JS/strings/longestWordinStr.js
@@ -13,10 +13,18 @@
 // 6
 // 5
 
+// Logic
+// Split string onto an array sperated by each word
+// create a max variable that stores the highest count
+// loop through the array of words
+  // check count of each word
+  // if word is higher than max count, update mas count
+// return max count
+
 function findLongestWord(str) {
-  var words = str.split(' ');
-  var max = 0;
-  for (var i = 0; i < words.length; i += 1) {
+  const words = str.split(' ');
+  let max = 0;
+  for (let i = 0; i < words.length; i += 1) {
     if (words[i].length > max) max = words[i].length;
   }
   return max;
