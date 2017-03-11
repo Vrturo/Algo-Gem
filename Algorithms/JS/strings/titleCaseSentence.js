5a7a535a11b7db3963f2e86b9721512898c4a7fa
Arturo Perez <VrturoPerez@gmail.com>
2017-03-11 11:36:38 -0800

    add params/ solution

--- Algorithms/JS/strings/titleCaseSentence.js
+++ Algorithms/JS/strings/titleCaseSentence.js
@@ -5,3 +5,17 @@
 
 // For the purpose of this exercise, you should also capitalize connecting words like "the" and "of".
 
+/**
+ * @param {string} s
+ * @return {str}
+ */
+
+function titleCase(str) {
+  var words = str.split(' ');
+  for (var i = 0; i < words.length; i += 1) {
+    words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase();
+  }
+  return words.join(' ');
+}
+
+titleCase("I'm a little tea pot");
