7637d8b277348e56989eaf8c1b8f4205d48f5cb1
Arturo Perez <VrturoPerez@gmail.com>
2017-03-11 10:33:09 -0800

    add solution

--- Algorithms/JS/strings/longestWordinStr.js
+++ Algorithms/JS/strings/longestWordinStr.js
@@ -12,3 +12,14 @@
 // Output: Integer
 // 6
 // 5
+
+function findLongestWord(str) {
+  var words = str.split(' ');
+  var max = 0;
+  for (var i = 0; i < words.length; i += 1) {
+    if (words[i].length > max) max = words[i].length;
+  }
+  return max;
+}
+
+console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
