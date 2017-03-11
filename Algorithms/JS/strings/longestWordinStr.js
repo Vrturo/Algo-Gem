07db7bfbb195fb72fccbc376a9a7e61694aa7a5e
Arturo Perez <VrturoPerez@gmail.com>
2017-03-11 10:32:30 -0800

    add test cases

--- Algorithms/JS/strings/longestWordinStr.js
+++ Algorithms/JS/strings/longestWordinStr.js
@@ -3,3 +3,12 @@
 // Return the length of the longest word in the provided sentence.
 
 // Your response should be a number.
+
+
+// Input: String
+// ex. "The quick brown fox jumped over the lazy dog"
+// "May the force be with you"
+
+// Output: Integer
+// 6
+// 5
