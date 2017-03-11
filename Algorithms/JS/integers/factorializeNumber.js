3eb9ed50a8d7729c03297c15b162c5cc8fff883d
Arturo Perez <VrturoPerez@gmail.com>
2017-03-11 10:12:39 -0800

    add desc

--- Algorithms/JS/integers/factorializeNumber.js
+++ Algorithms/JS/integers/factorializeNumber.js
@@ -1,1 +1,12 @@
-factorializeNumber.js
+// Factorialize a Number
+
+// Return the factorial of the provided integer.
+
+// If the integer is represented with the letter n,
+// a factorial is the product of all positive integers less than or equal to n.
+
+// Factorials are often represented with the shorthand notation n!
+
+// For example: 5! = 1 * 2 * 3 * 4 * 5 = 120
+
+
