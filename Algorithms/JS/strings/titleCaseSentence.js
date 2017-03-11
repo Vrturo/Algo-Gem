50434133c14498a4ad39ac2fe56d4c6529e5da7b
Arturo Perez <VrturoPerez@gmail.com>
2017-03-11 11:33:24 -0800

    add desc

--- Algorithms/JS/strings/titleCaseSentence.js
+++ Algorithms/JS/strings/titleCaseSentence.js
@@ -1,1 +1,7 @@
-titleCaseSentence.js
+// Title Case a Sentence
+
+// Return the provided string with the first letter of each word capitalized.
+// Make sure the rest of the word is in lower case.
+
+// For the purpose of this exercise, you should also capitalize connecting words like "the" and "of".
+
