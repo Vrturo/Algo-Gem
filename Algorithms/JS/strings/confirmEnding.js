f9d294936161bb4d6f99898f823514a808bfb88a
Arturo Perez <VrturoPerez@gmail.com>
2017-03-12 09:46:15 -0700

    add desc ++ params

--- Algorithms/JS/strings/confirmEnding.js
+++ Algorithms/JS/strings/confirmEnding.js
@@ -1,1 +1,19 @@
-confirmEnding.js
+// Confirm the Ending
+
+// Check if a string (first argument, str) ends with the given target string (second argument, target).
+
+// This challenge can be solved with the .endsWith() method, which was introduced in ES2015. 
+// But for the purpose of this challenge, we would like you to use one of the JavaScript substring methods instead.
+
+// Sample Input
+
+// "Bastian", "n"
+// "Connor", "n"
+// "Open sesame", "same"
+
+// Sample Output
+
+// true
+// false
+// true
+
