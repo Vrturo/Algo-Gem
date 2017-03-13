4ba1204ef9cb715c9b6c9545f3b0be0319d23e2b
Arturo Perez <VrturoPerez@gmail.com>
2017-03-13 11:30:35 -0700

    add desc + params

--- Algorithms/JS/strings/turncateString.js
+++ Algorithms/JS/strings/turncateString.js
@@ -1,1 +1,14 @@
-turncateString.js
+// Truncate a string
+
+// Truncate a string (first argument) if it is longer than the given maximum string length (second argument). 
+// Return the truncated string with a ... ending.
+
+// Note that inserting the three dots to the end will add to the string length.
+
+// However, if the given maximum string length num is less than or equal to 3, 
+// then the addition of the three dots does not add to the string length in determining the truncated string.
+
+/**
+ * @param {string} str
+ * @return {string}
+ */
