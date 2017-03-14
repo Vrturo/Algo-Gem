ca61ef75146fe590594fea2068ff27d0e45a6939
Arturo Perez <VrturoPerez@gmail.com>
2017-03-14 16:30:55 -0700

    add desc

--- Algorithms/JS/arrays/diffArrays.js
+++ Algorithms/JS/arrays/diffArrays.js
@@ -1,1 +1,5 @@
-diffArrays.js
+// Diff Two Arrays
+
+// Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. 
+// In other words, return the symmetric difference of the two arrays.
+
