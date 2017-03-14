7a3e4396957911eaa1ecf57df885e637b5a45789
Arturo Perez <VrturoPerez@gmail.com>
2017-03-14 16:33:14 -0700

    add input/out && params

--- Algorithms/JS/arrays/diffArrays.js
+++ Algorithms/JS/arrays/diffArrays.js
@@ -3,3 +3,22 @@
 // Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. 
 // In other words, return the symmetric difference of the two arrays.
 
+// Ex.
+// Input:
+
+// ["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]
+// ["andesite", "grass", "dirt", "dead shrub"], ["andesite", "grass", "dirt", "dead shrub"]
+
+// Output:
+
+// ["pink wool"]
+// []
+// [4]
+// ["piglet", 4]
+
+/**
+ * @param {array[], array[]} arr1 arr2
+ * @return {array}
+ */
+
+ 
