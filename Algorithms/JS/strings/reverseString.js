01131c0b79052844a8733ee79d1f0f50f2d08026
Arturo Perez <VrturoPerez@gmail.com>
2017-04-16 11:03:18 -0700

    remove duplicate functions

--- Algorithms/JS/strings/reverseString.js
+++ Algorithms/JS/strings/reverseString.js
@@ -1,20 +1,5 @@
 // Reverse a string
 
-const reverseString = function (input) {
-  let result = '';
-
-  function travel(index) {
-    if (index >= 0) {
-      result += input[index];
-      travel(index - 1);
-    }
-  }
-  travel(input.length - 1);
-  return result;
-};
-
-// console.log(reverseString('test'));
-
 function reverse(str) {
   let rtnStr = '';
   for (let i = str.length - 1; i >= 0; i -= 1) {
@@ -46,6 +31,24 @@ const reverse = function (input) {
   return input.length === 0 ? '' : input[input.length - 1] + reverse(input.slice(0, input.length - 1));
 };
 
+// ----------------------------------------------------
+
+const reverseString = function (input) {
+  let result = '';
+
+  function travel(index) {
+    if (index >= 0) {
+      result += input[index];
+      travel(index - 1);
+    }
+  }
+  travel(input.length - 1);
+  return result;
+};
+
+// console.log(reverseString('test'));
+
+// ----------------------------------------------------------------------
 
 // USING REVERSE
 
@@ -53,26 +56,3 @@ function reverse(str) {
   return (!str || str.length < 2) ? str : str.split('').reverse().join('');
 }
 
-// ----------------------------------------------------------------------
-
-const reverseTwo = function (string) {
-  const arr = string.split('');
-  for (let i = 0, j = string.length - 1; i = Math.floor(string.length / 2) - 1; i += 1) {
-    arr[i] = string[j];
-    arr[j] = string[i];
-    j -= 1;
-  }
-  return arr.join('');
-};
-console.log(reverseTwo('strin'))
-
-
-const reverse = function(string) {
-  const arr = string.split('');
-  for (let i = 0, j = string.length - 1; i <= Math.floor(string.length / 2) - 1; i += 1) {
-    arr[i] = string[j];
-    arr[j] = string[i];
-    j -= 1;
-  }
-  return arr.join('');
-};
