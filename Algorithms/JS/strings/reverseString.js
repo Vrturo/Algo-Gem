bb8f54706225f67eee0791b7e9ddc74c54b16057
Arturo Perez <VrturoPerez@gmail.com>
2017-04-15 18:54:15 -0700

    lint

--- Algorithms/JS/strings/reverseString.js
+++ Algorithms/JS/strings/reverseString.js
@@ -1,65 +1,78 @@
 // Reverse a string
 
-var reverseString = function(input){
-  var result = ""
+const reverseString = function (input) {
+  let result = '';
 
-  function travel(index){
-    if (index < 0){
-      return
-    }else{
-      result += input[index]
-      travel(index-1)
+  function travel(index) {
+    if (index >= 0) {
+      result += input[index];
+      travel(index - 1);
     }
   }
-    travel(input.length - 1);
-    return result
-}
+  travel(input.length - 1);
+  return result;
+};
 
+// console.log(reverseString('test'));
 
-var reverse = function(input){
-  if (input.length === 0){
-    return "";
-  };
-  return input[input.length - 1] + reverse(input.slice(0, input.length - 1));
-}
-
-function reverse(str){
-  var rtnStr = '';
-  for(var i = str.length-1; i>=0;i--){
-    rtnStr +=str[i];
+function reverse(str) {
+  let rtnStr = '';
+  for (let i = str.length - 1; i >= 0; i -= 1) {
+    rtnStr += str[i];
   }
   return rtnStr;
 }
 
 function reverse(str) {
-  str = str.split('');
-  var len = str.length,
-      halfIndex = Math.floor(len / 2) - 1,
-      revStr;
-  for (var i = 0; i <= halfIndex; i++) {
-    revStr = str[len - i - 1];
-    str[len - i - 1] = str[i];
-    str[i] = revStr;
+  const strArr = str.split('');
+  const len = str.length;
+  const halfIndex = Math.floor(len / 2) - 1;
+  for (let i = 0; i <= halfIndex; i += 1) {
+    strArr[i] = str[len - i - 1];
+    strArr[len - i - 1] = str[i];
   }
-  return str.join('');
+  return strArr.join('');
 }
 
 // RECURSIVE
 
-function reverse (str) {
-    if (str === "") {
-        return "";
-    } else {
-        return reverse(str.substr(1)) + str.charAt(0);
-    }
+function reverse(str) {
+  return (str === '') ? '' : reverse(str.substr(1)) + str.charAt(0);
 }
 
-// using reverse
-function reverse(str){
-    if(!str || str.length <2){
-      return str;
-    };
-    return str.split('').reverse().join('');
+// ------------------------------------------------
+
+const reverse = function (input) {
+  return input.length === 0 ? '' : input[input.length - 1] + reverse(input.slice(0, input.length - 1));
+};
+
+
+// USING REVERSE
+
+function reverse(str) {
+  return (!str || str.length < 2) ? str : str.split('').reverse().join('');
 }
 
+// ----------------------------------------------------------------------
 
+const reverseTwo = function (string) {
+  const arr = string.split('');
+  for (let i = 0, j = string.length - 1; i = Math.floor(string.length / 2) - 1; i += 1) {
+    arr[i] = string[j];
+    arr[j] = string[i];
+    j -= 1;
+  }
+  return arr.join('');
+};
+console.log(reverseTwo('strin'))
+
+
+const reverse = function(string) {
+  const arr = string.split('');
+  for (let i = 0, j = string.length - 1; i <= Math.floor(string.length / 2) - 1; i += 1) {
+    arr[i] = string[j];
+    arr[j] = string[i];
+    j -= 1;
+  }
+  return arr.join('');
+};
--- test.js
+++ test.js
@@ -301,23 +301,25 @@
 // }
 
 
-function diffArray(arr1, arr2) {
-  var newArr = [],
-      map = {};
-  for(var i = 0; i < arr1.length; i++) {
-    map[arr1[i]] = true;
-  }
-  for (var j = 0; j < arr2.length; j += 1) {
-    map[arr2[j]] ? map[arr2[j]] = false : map[arr2[j]] = true;
-  }
-  for (var k in map) {
-    if (map[k]) {
-      Number(k) ? newArr.push(Number(k)) : newArr.push(k);
-    }
-  }
-  // Same, same; but different.
-  return newArr;
-}
+// function diffArray(arr1, arr2) {
+//   var newArr = [],
+//       map = {};
+//   for(var i = 0; i < arr1.length; i++) {
+//     map[arr1[i]] = true;
+//   }
+//   for (var j = 0; j < arr2.length; j += 1) {
+//     map[arr2[j]] ? map[arr2[j]] = false : map[arr2[j]] = true;
+//   }
+//   for (var k in map) {
+//     if (map[k]) {
+//       Number(k) ? newArr.push(Number(k)) : newArr.push(k);
+//     }
+//   }
+//   // Same, same; but different.
+//   return newArr;
+// }
 
-console.log(diffArray([1, "calf", 3, "piglet"], [1, "calf", 3, 4]));
+// console.log(diffArray([1, "calf", 3, "piglet"], [1, "calf", 3, 4]));
+
+
 
