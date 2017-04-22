3d806dcd9bfb3fc3ceea98404f3c6499e7e5c2a4
Arturo Perez <VrturoPerez@gmail.com>
2017-04-21 19:41:33 -0700

    add soltion

--- Algorithms/JS/strings/reverseWordsinStringThree.js
+++ Algorithms/JS/strings/reverseWordsinStringThree.js
@@ -6,20 +6,20 @@
 // Output: "s'teL ekat edoCteeL tsetnoc"
 // Note: In the string, each word is separated by single space and there will not be any extra space in the string.
 
-var reverseWords = function(s) {
-    const words = s.split(' ');
-    let result = '';
-    
-    function reverse(word) {
-        let result = '';
-        for (let i = word.length; i--;) {
-            result += word[i];
-        }
-        return result;
+const reverseWords = function(s) {
+  const words = s.split(' ');
+  let result = '';
+
+  function reverse(word) {
+    let temp = '';
+    for (let i = word.length; i -= 1;) {
+      temp += word[i];
     }
-    for (let i = 0; i < words.length; i += 1) {
-        result += reverse(words[i]) + ' ';
-    }
-    result = result.slice(0, result.length - 1);
-    return result;
-};
+    return temp;
+  }
+  for (let i = 0; i < words.length; i += 1) {
+    result += reverse(`${words[i]} `);
+  }
+  result = result.slice(0, result.length - 1);
+  return result;
+}
