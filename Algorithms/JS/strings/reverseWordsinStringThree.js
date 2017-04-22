224dc6ded492c0ffe35aafe5e037fd057c18c1bc
Arturo Perez <VrturoPerez@gmail.com>
2017-04-21 19:37:29 -0700

    add solution

--- Algorithms/JS/strings/reverseWordsinStringThree.js
+++ Algorithms/JS/strings/reverseWordsinStringThree.js
@@ -5,3 +5,21 @@
 // Input: "Let's take LeetCode contest"
 // Output: "s'teL ekat edoCteeL tsetnoc"
 // Note: In the string, each word is separated by single space and there will not be any extra space in the string.
+
+var reverseWords = function(s) {
+    const words = s.split(' ');
+    let result = '';
+    
+    function reverse(word) {
+        let result = '';
+        for (let i = word.length; i--;) {
+            result += word[i];
+        }
+        return result;
+    }
+    for (let i = 0; i < words.length; i += 1) {
+        result += reverse(words[i]) + ' ';
+    }
+    result = result.slice(0, result.length - 1);
+    return result;
+};
